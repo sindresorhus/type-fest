58005a45b1d492a1651c0201b2d127dfa07f1481
Dimitri Benin <dimitri.benin@gmail.com>
2019-03-13 23:54:12 +0100

    Fix lint issue

--- index.test-d.ts
+++ index.test-d.ts
@@ -1,6 +1,6 @@
 /* eslint-disable import/no-unassigned-import */
 import './test/omit';
 import './test/merge';
-import './test/mergeExclusive';
+import './test/merge-exclusive';
 
 // TODO: Add negative tests. Blocked by: https://github.com/SamVerschueren/tsd-check/issues/2
