3c54ba138cd15143cc80c0a11fef26114ce1bdf9
Sindre Sorhus <sindresorhus@gmail.com>
2019-03-18 12:38:00 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -136,7 +136,7 @@ export type MergeExclusive<FirstType, SecondType> =
 		(Without<FirstType, SecondType> & SecondType) | (Without<SecondType, FirstType> & FirstType) :
 		FirstType | SecondType;
 
-/*
+/**
 Allows creating a union type by combining primitive types and literal types without sacrificing auto-completion in IDEs for the literal type part of the union.
 
 Currently, when a union type of a primitive type is combined with literal types, TypeScript loses all information about the combined literals. Thus, when such type is used in an IDE with autocompletion, no suggestions are made for the declared literals.
