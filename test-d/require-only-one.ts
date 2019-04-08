ff340bf4544f3cac5bc46a73809ddfa6fd69ba85
Jarek Radosz <jradosz@gmail.com>
2019-04-08 18:02:22 +0200

    Add negative tests

--- test-d/require-only-one.ts
+++ test-d/require-only-one.ts
@@ -1,22 +1,28 @@
-import {expectType} from 'tsd';
+import {expectType, expectError} from 'tsd';
 import {RequireOnlyOne} from '..';
 
 type SystemMessages = {
 	default: string;
 
-	macos: string;
+	macos?: string;
 	linux: string;
 	windows: string;
+
+	optional?: string;
 };
 
 type ValidMessages = RequireOnlyOne<SystemMessages, 'macos' | 'linux' | 'windows'>;
-const test = (_: ValidMessages[]): void => {};
+const test = (_: ValidMessages): void => {};
 
-test([
-	{macos: 'hey', default: 'hello'},
-	{linux: 'sup', default: 'hello'},
-	{windows: 'hi', default: 'hello'}
-]);
+test({macos: 'hey', default: 'hello'});
+test({linux: 'sup', default: 'hello'});
+test({windows: 'hi', default: 'hello'});
+test({windows: 'hi', default: 'hello', optional: 'howdy'});
+
+expectError(test({}));
+expectError(test({macos: 'hey'}));
+expectError(test({default: 'hello'}));
+expectError(test({macos: 'hey', linux: 'sup', default: 'hello'}));
 
 declare const onlyOneWithoutKeys: RequireOnlyOne<{a: number; b: number}>;
 expectType<{a: number} | {b: number}>(onlyOneWithoutKeys);
