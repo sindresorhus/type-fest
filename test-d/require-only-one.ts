bacce8e21ee10e42c7503ac67c6081e3cefae6c5
izayl <izayl@163.com>
2019-04-09 13:41:55 +0800

    update test

--- test-d/require-only-one.ts
+++ test-d/require-only-one.ts
@@ -13,19 +13,18 @@ type SystemMessages = {
 
 type ValidMessages = RequireOnlyOne<SystemMessages, 'macos' | 'linux' | 'windows'>;
 const test = (_: ValidMessages): void => {};
+const system = Math.random() > 0.5 ? {macos: 'hey'} : {linux: 'sup'};
 
 test({macos: 'hey', default: 'hello'});
 test({linux: 'sup', default: 'hello'});
 test({windows: 'hi', default: 'hello'});
 test({windows: 'hi', default: 'hello', optional: 'howdy'});
-
-const system = {macos: 'hey'} || {linux: 'sup'};
+test({default: 'hello', ...system});
 
 expectError(test({}));
 expectError(test({macos: 'hey'}));
 expectError(test({default: 'hello'}));
 expectError(test({macos: 'hey', linux: 'sup', default: 'hello'}));
-expectError(test({default: 'hello', ...system}));
 
 declare const onlyOneWithoutKeys: RequireOnlyOne<{a: number; b: number}>;
 expectType<{a: number} | {b: number}>(onlyOneWithoutKeys);
