432cdf356c6f4993b4fb810e5216555aeecbf96b
Tiger Oakes <contact@tigeroakes.com>
2019-04-16 10:55:38 -0700

    Add additional comments for Mutable
    Details potential ways to use the type and how it works

--- index.d.ts
+++ index.d.ts
@@ -206,7 +206,11 @@ export type RequireAtLeastOne<ObjectType, KeysType extends keyof ObjectType = ke
 	& Omit<ObjectType, KeysType>;
 
 /*
-Converts a readonly object into a mutable object. Inverse of Readonly<T>.
+Convert an object with readonly properties into a mutable object. Inverse of `Readonly<T>`.
+
+This can be used to [store and mutate options within a class](https://github.com/sindresorhus/pageres/blob/4a5d05fca19a5fbd2f53842cbf3eb7b1b63bddd2/source/index.ts#L72),
+[edit readonly objects within tests](https://stackoverflow.com/questions/50703834),
+and [construct a readonly object within a function](https://github.com/Microsoft/TypeScript/issues/24509).
 
 @example
 ```
@@ -222,5 +226,6 @@ mutableFoo.a = 3;
 ```
 */
 export type Mutable<ObjectType> = {
+	// For each Key in the keys of ObjectType, make a mapped type by removing the readonly modifier from the property.
 	-readonly [KeyType in keyof ObjectType]: ObjectType[KeyType];
 }
