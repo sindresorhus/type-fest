aa1ae3b1c067d7336cd66390a8f929d2a026a7de
Sindre Sorhus <sindresorhus@gmail.com>
2019-07-05 16:31:21 +0700

    Document how to use `JsonObject` (#46)
    Fixes #15

--- source/basic.d.ts
+++ source/basic.d.ts
@@ -37,6 +37,8 @@ export type TypedArray =
 
 /**
 Matches a JSON object.
+
+This type can be useful to enforce some input to be JSON-compatible or as a super-type to be extended from. Don't use this as a direct return type as the user would have to double-cast it: `jsonObject as unknown as CustomResponse`. Instead, you could extend your CustomResponse type from it to ensure your type only uses JSON-compatible types: `interface CustomResponse extends JsonObject { … }`.
 */
 export type JsonObject = {[key: string]: JsonValue};
 
