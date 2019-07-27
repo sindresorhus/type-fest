892ea1a4a2fe6889fc45b5322ffc3d15ebc997b9
Will Hoskings <49915996+resynth1943@users.noreply.github.com>
2019-07-27 14:44:47 +0100

    Fix typo in tests
    Co-Authored-By: Dimitri Benin <BendingBender@users.noreply.github.com>

--- test-d/index.ts
+++ test-d/index.ts
@@ -12,7 +12,7 @@ observable.subscribe(value => expectType<unknown>(value));
 // In here, we test opaque types. If you don't understand them, this code should help you understand the concepts behind using an opaque type.
 type Value = Opaque<number>;
 
-// We make an explicit cast here, so we can test the value/
+// We make an explicit cast here, so we can test the value.
 const value: Value = 2 as Value;
 
 // Every opaque type should have a private symbol member, so the compiler can differentiate separate opaque types.
