cd7c3e6efdca01439828eed8ef1f199aaf2af4b3
Will Hoskings <resynth1943@outlook.com>
2019-07-27 14:47:25 +0100

    Move tests to a separate file

---
+++ test-d/opaque.ts
@@ -0,0 +1,14 @@
+import { Opaque } from '..';
+import { expectType } from 'tsd';
+
+// In here, we test opaque types. If you don't understand them, this code should help you understand the concepts behind using an opaque type.
+type Value = Opaque<number>;
+
+// We make an explicit cast here, so we can test the value.
+const value: Value = 2 as Value;
+
+// Every opaque type should have a private symbol member, so the compiler can differentiate separate opaque types.
+expectType<symbol>(value.__opaque__);
+
+// The underlying type of the value is still a number.
+expectType<number>(value);
