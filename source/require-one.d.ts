02d6e80c1333af85170d8eaabec1d19895a81f38
Kai Niedziela <kai.niedziela@outlook.com>
2019-07-28 20:30:47 +0200

    provide use cases for RequireOne type

--- source/require-one.d.ts
+++ source/require-one.d.ts
@@ -1,6 +1,12 @@
 /**
 Create a type that requires only one of the given properties, but not more. The remaining properties are kept as is.
 
+Use cases:
+- Creating interfaces for components that only need one of the properties to display properly.
+- Declaring generic properties in a single place for a single use case that get narrowed down via `RequireOne`.
+
+The caveat with `RequireOne` is that TypeScript doesn't always know at compile time every property that will exist at runtime. Therefore `RequireOne` can't do anything to prevent extra properties it doesn't know about.
+
 @example
 ```
 import {RequireOne} from 'type-fest';
@@ -10,6 +16,7 @@ type Responder = {
 	json: () => string;
 	secure: boolean;
 };
+
 const responder: RequireOne<Responder, 'text' | 'json'> = {
 	json: () => '{"message": "ok"}',
 	secure: true
