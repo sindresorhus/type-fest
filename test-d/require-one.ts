a4f748be73c8fbf38c43609e81eccf975ea561d3
Kai Niedziela <kai.niedziela@outlook.com>
2019-07-28 20:06:47 +0200

    add test cases for RequireOne type

---
+++ test-d/require-one.ts
@@ -0,0 +1,24 @@
+import {expectType, expectError} from 'tsd';
+import {RequireOne} from '..';
+
+type SystemMessages = {
+    default: string;
+
+	macos: string;
+	linux: string;
+
+	optional?: string;
+};
+
+type ValidMessages = RequireOne<SystemMessages, 'macos' | 'linux'>;
+const test = (_: ValidMessages): void => {};
+
+test({macos: 'hey', default: 'hello'});
+test({linux: 'sup', optional: 'howdy', default: 'hello'});
+
+expectError(test({}));
+expectError(test({macos: 'hey', linux: 'sup', default: 'hello'}));
+
+declare const oneWithoutKeys: RequireOne<{a: number; b: number}>;
+expectType<{a: number} | {b: number}>(oneWithoutKeys);
+expectError(expectType<{a: number; b: number}>(oneWithoutKeys));
