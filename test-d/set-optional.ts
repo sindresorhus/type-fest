05457b917cfbcb151efdc22fae6424626b454b02
Crhistian Ramirez <cramirez@four51.com>
2019-08-24 09:35:43 -0500

    add negative type assertions

--- test-d/set-optional.ts
+++ test-d/set-optional.ts
@@ -1,4 +1,4 @@
-import {expectType} from 'tsd';
+import {expectType, expectError} from 'tsd';
 import {SetOptional} from '..';
 
 // Update one required and one optional to optional
@@ -12,3 +12,7 @@ expectType<{a?: number; b?: string; c: boolean}>(variation2);
 // Three optional remain optional
 declare const variation3: SetOptional<{a?: number; b?: string; c?: boolean}, 'a' | 'b' | 'c'>;
 expectType<{a?: number; b?: string; c?: boolean}>(variation3);
+
+// Fail if type changes even if optional is right
+declare const variation4: SetOptional<{a: number; b?: string; c: boolean}, 'b' | 'c'>;
+expectError<{a: boolean; b?: string; c?: boolean}>(variation4);
--- test-d/set-required.ts
+++ test-d/set-required.ts
@@ -1,4 +1,4 @@
-import {expectType} from 'tsd';
+import {expectType, expectError} from 'tsd';
 import {SetRequired} from '..';
 
 // Update one required and one optional to required
@@ -12,3 +12,7 @@ expectType<{a: number; b: string; c?: boolean}>(variation2);
 // Three required remain required
 declare const variation3: SetRequired<{a: number; b: string; c: boolean}, 'a' | 'b' | 'c'>;
 expectType<{a: number; b: string; c: boolean}>(variation3);
+
+// Fail if type changes even if optional is right
+declare const variation4: SetRequired<{a?: number; b: string; c?: boolean}, 'b' | 'c'>;
+expectError<{a?: boolean; b: string; c: boolean}>(variation4);
