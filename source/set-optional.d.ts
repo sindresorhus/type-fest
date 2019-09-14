a51d082e4ad1ff25649f91cc4cde8662eaf5f426
Sindre Sorhus <sindresorhus@gmail.com>
2019-09-14 12:40:36 +0700

    Update set-optional.d.ts

--- source/set-optional.d.ts
+++ source/set-optional.d.ts
@@ -1,47 +1,32 @@
 /**
-Create a type that makes optional the defined properties. The remaining properties are kept as is. The sister of `SetRequired` type.
+Create a type that makes the given keys optional. The remaining keys are kept as is. The sister of the `SetRequired` type.
 
-Use-case: You want to define a single model where the only thing that changes is whether or not some of the properties are optional.
+Use-case: You want to define a single model where the only thing that changes is whether or not some of the keys are optional.
 
 @example
 ```
 import {SetOptional} from 'type-fest';
 
 type Foo = {
-  a: number;
-  b?: string;
-  c: boolean;
+	a: number;
+	b?: string;
+	c: boolean;
 }
 
 type SomeOptional = SetOptional<Foo, 'b' | 'c'>;
-//  type SomeOptional = {
-//    a: number;
-//    b?: string; // was already optional, still is
-//    c?: boolean; // is now optional
-//  }
+// type SomeOptional = {
+// 	a: number;
+// 	b?: string; // Was already optional and still is
+// 	c?: boolean; // Is now optional
+// }
 ```
 */
-
 export type SetOptional<BaseType, Keys extends keyof BaseType = keyof BaseType> =
-
-// Pick from the base type just the keys that are not optional
-Pick<
-  BaseType,
-  Exclude<keyof BaseType, Keys>
->
-
-// Intersect that type with
-&
-
-// Pick from the base type the keys that should be optional and make them optional
-Partial<
-  Pick<
-    BaseType,
-    Keys
-  >
-> extends
-
-// If InferredType extends the previous then for each property use the inferred type property
-infer InferredType
-    ? { [Property in keyof InferredType]: InferredType[Property] }
-    : never;
+	// Pick just the keys that are not optional from the base type.
+	Pick<BaseType, Exclude<keyof BaseType, Keys>>	&
+	// Pick the keys that should be optional from the base type and make them optional.
+	Partial<Pick<BaseType, Keys>> extends
+	// If `InferredType` extends the previous, then for each key, use the inferred type key.
+	infer InferredType
+			? {[Property in keyof InferredType]: InferredType[Property]}
+			: never;
