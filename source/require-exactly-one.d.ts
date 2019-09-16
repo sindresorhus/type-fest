6af4e7e648621b0d757f49a755e9a1ffbb414304
Sindre Sorhus <sindresorhus@gmail.com>
2019-09-16 13:19:36 +0700

    Update require-exactly-one.d.ts

--- source/require-exactly-one.d.ts
+++ source/require-exactly-one.d.ts
@@ -1,9 +1,9 @@
 /**
 Create a type that requires exactly one of the given properties and disallows more. The remaining properties are kept as is.
 
-Use cases:
+Use-cases:
 - Creating interfaces for components that only need one of the properties to display properly.
-- Declaring generic properties in a single place for a single use case that get narrowed down via `RequireExactlyOne`.
+- Declaring generic properties in a single place for a single use-case that gets narrowed down via `RequireExactlyOne`.
 
 The caveat with `RequireExactlyOne` is that TypeScript doesn't always know at compile time every property that will exist at runtime. Therefore `RequireExactlyOne` can't do anything to prevent extra properties it doesn't know about.
 
@@ -18,6 +18,8 @@ type Responder = {
 };
 
 const responder: RequireExactlyOne<Responder, 'text' | 'json'> = {
+	// Adding a `text` property here would cause a compile error.
+
 	json: () => '{"message": "ok"}',
 	secure: true
 };
