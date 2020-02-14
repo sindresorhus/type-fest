ce6f229b148f793d4c2bff963ddc6c416d21400c
Will Hoskings <resynth1943@outlook.com>
2020-02-14 19:53:41 +0000

    More tests

--- test-d/optional.ts
+++ test-d/optional.ts
@@ -1,5 +1,7 @@
-import {expectType} from 'tsd';
+import {expectType, expectError} from 'tsd';
 import {Optional} from '..';
 
 expectType<Optional<number>>(2);
 expectType<Optional<number>>(undefined);
+expectError<Optional<null>>(null);
+expectError<Optional<string>>(2);
