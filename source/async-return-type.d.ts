7ee7e7acef79fe3e1cb975cab76dcbc2401c5f1b
Sindre Sorhus <sindresorhus@gmail.com>
2020-02-14 23:34:36 +0700

    Update async-return-type.d.ts

--- source/async-return-type.d.ts
+++ source/async-return-type.d.ts
@@ -3,7 +3,7 @@ import {PromiseValue} from './promise-value';
 type AsyncFunction = (...args: unknown[]) => Promise<unknown>;
 
 /**
-Unpack the return type of a function that returns a `Promise`.
+Unwrap the return type of a function that returns a `Promise`.
 
 There has been [discussion](https://github.com/microsoft/TypeScript/pull/35998) about implementing this type in TypeScript.
 
@@ -12,7 +12,7 @@ There has been [discussion](https://github.com/microsoft/TypeScript/pull/35998) 
 import {AsyncReturnType} from 'type-fest';
 import {asyncFunction} from 'api';
 
-// This type resolves to the unpacked return type of `asyncFunction`.
+// This type resolves to the unwrapped return type of `asyncFunction`.
 type Value = AsyncReturnType<typeof asyncFunction>;
 
 async function doSomething(value: Value) {}
