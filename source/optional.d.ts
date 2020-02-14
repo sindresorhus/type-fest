78c026f813e92cff1a95b40ae58b00be5a745ec2
Sindre Sorhus <sindresorhus@gmail.com>
2020-02-15 02:39:30 +0700

    Update optional.d.ts

--- source/optional.d.ts
+++ source/optional.d.ts
@@ -2,10 +2,13 @@
 Makes a type optional. Allows a type to be `undefined`, but not `null`.
 
 @example
+```
 import {Optional} from 'type-fest';
 
 type Value = number;
 type OptionalValue = Optional<Value>;
+
 const values: [Value, OptionalValue] = [2, undefined];
+```
 */
 export type Optional<Value> = Value extends null ? never : (Value | undefined);
