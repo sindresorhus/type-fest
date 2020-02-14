62897845228d5bbfe043b2b0d9a9bc9e28794ced
Sindre Sorhus <sindresorhus@gmail.com>
2020-02-15 02:45:24 +0700

    Update one-or-more.d.ts

--- source/one-or-more.d.ts
+++ source/one-or-more.d.ts
@@ -3,6 +3,7 @@
 Either matches an array containing only `Value`, or `Value` itself, not in an array.
 
 @example
+```
 import {OneOrMore} from 'type-fest';
 
 type ID = number;
@@ -10,6 +11,7 @@ type OneOrMoreIDs = OneOrMore<ID>;
 
 const multipleIDs: OneOrMoreIDs = [1234, 2468];
 const oneID: OneOrMoreIDs = 1234;
+```
 */
 export type OneOrMore<Value> = Value | ReadonlyArray<Value>;
 /* eslint-enable @typescript-eslint/array-type */
