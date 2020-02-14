6c105fb81876635c73316642d92c481ab86d42ea
Xiao Liang <yxliang01@users.noreply.github.com>
2020-02-15 03:50:07 +0800

    Allow all keys to be optional in the `JsonObject` type (#65)

--- source/basic.d.ts
+++ source/basic.d.ts
@@ -40,7 +40,7 @@ Matches a JSON object.
 
 This type can be useful to enforce some input to be JSON-compatible or as a super-type to be extended from. Don't use this as a direct return type as the user would have to double-cast it: `jsonObject as unknown as CustomResponse`. Instead, you could extend your CustomResponse type from it to ensure your type only uses JSON-compatible types: `interface CustomResponse extends JsonObject { … }`.
 */
-export type JsonObject = {[key: string]: JsonValue};
+export type JsonObject = {[Key in string]?: JsonValue};
 
 /**
 Matches a JSON array.
