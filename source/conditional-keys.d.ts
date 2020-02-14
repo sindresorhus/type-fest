f06ec2d6eceadbb2d5e05e70e6d4b245d4a68740
Sindre Sorhus <sindresorhus@gmail.com>
2020-02-15 02:23:04 +0700

    Update conditional-keys.d.ts

--- source/conditional-keys.d.ts
+++ source/conditional-keys.d.ts
@@ -1,5 +1,5 @@
 /**
-Extract the keys from a type where the value type of the key extends the provided condition.
+Extract the keys from a type where the value type of the key extends the given `Condition`.
 
 Internally this is used for the `ConditionalPick` and `ConditionalExcept` types.
 
@@ -11,33 +11,33 @@ interface Example {
 	a: string;
 	b: string | number;
 	c?: string;
-	d: {}
+	d: {};
 }
 
 type StringKeysOnly = ConditionalKeys<Example, string>;
-// => 'a'
+//=> 'a'
 ```
 
-To support partial types make sure your `Condition` is a union of undefined `string | undefined` as demonstrated below.
+To support partial types, make sure your `Condition` is a union of undefined (for example, `string | undefined`) as demonstrated below.
 
 @example
 ```
 type StringKeysAndUndefined = ConditionalKeys<Example, string | undefined>;
-// => 'a' | 'c'
+//=> 'a' | 'c'
 ```
 */
 export type ConditionalKeys<Base, Condition> = NonNullable<
-	// Wrap in NonNullable to strip away the `undefined` type from the produced union.
+	// Wrap in `NonNullable` to strip away the `undefined` type from the produced union.
 	{
-		// Map through all the keys of the provided base type.
+		// Map through all the keys of the given base type.
 		[Key in keyof Base]:
-			// Pick only keys with types extending the `Condition` type provided.
+			// Pick only keys with types extending the given `Condition` type.
 			Base[Key] extends Condition
 				// Retain this key since the condition passes.
 				? Key
 				// Discard this key since the condition fails.
 				: never;
 
-	// Convert the produced object into a union type of the keys which pass the conditional test.
+	// Convert the produced object into a union type of the keys which passed the conditional test.
 	}[keyof Base]
 >;
