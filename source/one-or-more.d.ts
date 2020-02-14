87348b1fafd79f6b55bc8b052383d309f4ac3ab4
Will Hoskings <resynth1943@outlook.com>
2020-02-14 20:05:21 +0000

    Make some changes

--- source/one-or-more.d.ts
+++ source/one-or-more.d.ts
@@ -1,6 +1,8 @@
 /**
 Either matches an array containing only `Value`, or `Value` itself, not in an array.
 
+This can prove to be useful when you're not sure if you're going to get a type as an array, or on it's own.
+
 @example
 ```
 import {OneOrMore} from 'type-fest';
@@ -8,8 +10,13 @@ import {OneOrMore} from 'type-fest';
 type ID = number;
 type OneOrMoreIDs = OneOrMore<ID>;
 
-const multipleIDs: OneOrMoreIDs = [1234, 2468];
-const oneID: OneOrMoreIDs = 1234;
+function runOnIDs (ids: OneOrMoreIDs) {
+    if (Array.isArray(ids)) {
+        ids.forEach(id => console.log(id));
+    } else {
+        console.log(ids);
+    }
+}
 ```
 */
 export type OneOrMore<Value> = Value | readonly Value[];
