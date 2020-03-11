fd9fd35cc829ecdffb734a87cf8ae1d66b762e57
Richie Bendall <richiebendall@gmail.com>
2020-03-11 21:36:56 +1300

    docs: Add example for second parameter
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- source/value-of.d.ts
+++ source/value-of.d.ts
@@ -6,16 +6,33 @@ Create a union of the object's values, and optionally specify which keys to get 
 // data.json
 {
 	"foo": 1,
-	"bar": 2
+	"bar": 2,
+	"biz": 3
 }
 
 // main.ts
 import {ValueOf} from 'type-fest';
 import data = require('./data.json');
 
-export default (name: string): ValueOf<typeof data> => {
+export function getData(name: string): ValueOf<typeof data> {
 	return data[name]
 }
+
+export function onlyBar(name: string): ValueOf<typeof data, 2> {
+	return data[name]
+}
+
+// file.ts
+import {getData, onlyBar} from "./main"
+
+getData("foo")
+//=> 1
+
+onlyBar("foo")
+//=> TypeError ...
+
+onlyBar("bar")
+//=> 2
 ```
 */
 export type ValueOf<ObjectType, ValueType extends keyof ObjectType = keyof ObjectType> = ObjectType[ValueType];
