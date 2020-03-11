4208ece12aa28b8f42fcbbc68a36a59597a89f08
Richie Bendall <richiebendall@gmail.com>
2020-03-11 21:32:22 +1300

    docs: Add more realistic example
    Signed-off-by: Richie Bendall <richiebendall@gmail.com>

--- source/value-of.d.ts
+++ source/value-of.d.ts
@@ -3,11 +3,19 @@ Create a union of the object's values, and optionally specify which keys to get 
 
 @example
 ```
+// data.json
+{
+	"foo": 1,
+	"bar": 2
+}
+
+// main.ts
 import {ValueOf} from 'type-fest';
+import data = require('./data.json');
 
-const value: ValueOf<{a: 1; b: 2; c: 3}> = 3;
-
-expectType<1 | 2 | 3>(value);
+export default (name: string): ValueOf<typeof data> => {
+	return data[name]
+}
 ```
 */
 export type ValueOf<ObjectType, ValueType extends keyof ObjectType = keyof ObjectType> = ObjectType[ValueType];
