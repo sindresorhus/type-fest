713033237775c26862193ce6fbf9b7352f148bad
Sindre Sorhus <sindresorhus@gmail.com>
2020-04-05 22:40:52 +0800

    Upgrade XO

--- test-d/value-of.ts
+++ test-d/value-of.ts
@@ -1,10 +1,10 @@
-import {expectType, expectError} from 'tsd';
+import {expectType, expectError, expectAssignable} from 'tsd';
 import {ValueOf} from '..';
 
 const value: ValueOf<{a: 1; b: 2; c: 3}> = 3;
 const valueRestricted: ValueOf<{a: 1; b: 2; c: 3}, 'a'> = 1;
 
-expectType<1 | 2 | 3>(value);
+expectAssignable<1 | 2 | 3>(value);
 expectError<4>(value);
 
 expectType<1>(valueRestricted);
