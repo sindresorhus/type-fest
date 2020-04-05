c54d20a8c1527aadb9344b1a35fdfe65f0683f77
Sindre Sorhus <sindresorhus@gmail.com>
2020-04-05 22:28:44 +0800

    Update value-of.ts

--- test-d/value-of.ts
+++ test-d/value-of.ts
@@ -1,8 +1,8 @@
 import {expectType, expectError} from 'tsd';
 import {ValueOf} from '..';
 
-const value: ValueOf<{ a: 1; b: 2; c: 3 }> = 3;
-const valueRestricted: ValueOf<{ a: 1; b: 2; c: 3 }, 'a'> = 1;
+const value: ValueOf<{a: 1; b: 2; c: 3}> = 3;
+const valueRestricted: ValueOf<{a: 1; b: 2; c: 3}, 'a'> = 1;
 
 expectType<1 | 2 | 3>(value);
 expectError<4>(value);
