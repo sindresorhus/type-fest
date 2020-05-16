93f67c639517a0f8ccd083ab635abc58e723293a
Kai Niedziela <kai.niedziela@outlook.com>
2020-05-16 19:54:16 +0200

    Add use cases

--- source/fixed-length-array.d.ts
+++ source/fixed-length-array.d.ts
@@ -1,6 +1,13 @@
 /**
 Create a type that represents an array of a provided type and of a provided fixed length. The arrays length and Array prototype mutations that manipulate its length are excluded in the resulting type.
 
+Please participate in [this issue](https://github.com/microsoft/TypeScript/issues/26223) if you want to have a similiar type built-in in TypeScript.
+
+Use-cases:
+- Declaring fixed-length tuples or arrays with a large number of items
+- Creating a Range union (e.g. `0 | 1 | 2 | 3 | 4` from the keys of such a type) without having to resort to recursive types
+- Creating an array of coordinates with a static length, of for example 3 for a 3D vector
+
 @example
 ```
 import {FixedLengthArray} from 'type-fest';
