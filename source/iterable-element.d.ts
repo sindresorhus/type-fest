6309172f382987acffab6439ca9a80ce8405ac6f
Elmer Bulthuis <elmerbulthuis@gmail.com>
2020-06-01 20:21:50 +0200

    formatting comments

--- source/iterable-element.d.ts
+++ source/iterable-element.d.ts
@@ -1,37 +1,35 @@
 /**
- * Get the element type of an Iterable
- *
- * Quite usefull if combined with generators
- *
- * @example
- * ```
- * function * iAmGenerator() {
- *   yield 1;
- *   yield 2;
- * }
- * type MeNumber = IterableElement<ReturnType<typeof iAmGenerator>>
- * ```
- * Or, async iterators!
- *
- * @example
- * ```
- * async function * iAmGeneratorAsync() {
- *   yield "hi";
- *   yield true;
- * }
- * type MeStringOrBoolean = IterableElement<ReturnType<typeof iAmGeneratorAsync>>
- * ```
- *
- * an Array is also an Iterable! so we can do this:
- *
- * @example
- * ```
- * type MeString = IterableElement<Array<string>>
- * ```
- */
+Get the element type of an Iterable or AsyncIterable.
+
+Quite usefull if combined with generators:
+@example
+```
+function * iAmGenerator() {
+	yield 1;
+	yield 2;
+}
+type MeNumber = IterableElement<ReturnType<typeof iAmGenerator>>
+```
+
+Or, async iterators!
+@example
+```
+async function * iAmGeneratorAsync() {
+	yield "hi";
+	yield true;
+}
+type MeStringOrBoolean = IterableElement<ReturnType<typeof iAmGeneratorAsync>>
+```
+
+An Array is also an Iterable! so we can do this:
+@example
+```
+type MeString = IterableElement<Array<string>>
+```
+*/
 export type IterableElement<TargetIterable> =
-	TargetIterable extends Iterable<infer ElementType> ?
-	ElementType :
-	TargetIterable extends AsyncIterable<infer ElementType> ?
-	ElementType :
-	never;
+    TargetIterable extends Iterable<infer ElementType> ?
+    ElementType :
+    TargetIterable extends AsyncIterable<infer ElementType> ?
+    ElementType :
+    never;
