8e79a91bf8870cdb01f8a04663e2e3834b75e433
onichandame <zxinmyth@gmail.com>
2020-06-13 17:01:55 +0000

    added description

--- source/promise-value.d.ts
+++ source/promise-value.d.ts
@@ -1,5 +1,6 @@
 /**
 Returns the type that is wrapped inside a `Promise` type.
+If the type is a nested Promise, it is unwrapped recursively until a non-Promise type is obtained.
 If the type is not a `Promise`, the type itself is returned.
 
 @example
@@ -21,6 +22,11 @@ type RecursiveAsyncData = Promise<Promise<string> >;
 let recursiveAsyncData: PromiseValue<RecursiveAsyncData> = Promise.resolve(Promise.resolve('ABC'));
 ```
 */
-export type PromiseValue<PromiseType, Otherwise = PromiseType> = PromiseType extends Promise<infer Value>
-	? {0: PromiseValue<Value>; 1: Value}[PromiseType extends Promise<any> ? 0 : 1]
+export type PromiseValue<
+	PromiseType,
+	Otherwise = PromiseType
+> = PromiseType extends Promise<infer Value>
+	? { 0: PromiseValue<Value>; 1: Value }[PromiseType extends Promise<any>
+			? 0
+			: 1]
 	: Otherwise;
