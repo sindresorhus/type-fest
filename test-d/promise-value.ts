577c2a24f524687b8bce42f3dc8ce90b1c5c8d7b
onichandame <zxinmyth@gmail.com>
2020-06-28 04:53:28 +0800

    Make the `PromiseValue` type handle nested promises (#112)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- test-d/promise-value.ts
+++ test-d/promise-value.ts
@@ -2,10 +2,14 @@ import {expectAssignable} from 'tsd';
 import {PromiseValue} from '..';
 
 type NumberPromise = Promise<number>;
+type NestedPromise = Promise<NumberPromise>;
 type Otherwise = object;
 
 // Test the normal behaviour.
 expectAssignable<PromiseValue<NumberPromise>>(2);
+
+// Test the nested behaviour.
+expectAssignable<PromiseValue<NestedPromise>>(2);
 
 // Test what happens when the `PromiseValue` type is not handed a `Promise` type.
 expectAssignable<PromiseValue<number>>(2);
