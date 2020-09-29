976eccebfde48b2cccce253b2501a9df1ced32bd
Sindre Sorhus <sindresorhus@gmail.com>
2020-09-29 14:42:06 +0200

    Update iterable-element.d.ts

--- source/iterable-element.d.ts
+++ source/iterable-element.d.ts
@@ -1,7 +1,9 @@
 /**
-Get the element type of an `Iterable` or `AsyncIterable`.
+Get the element type of an `Iterable`/`AsyncIterable`. For example, an array or a generator.
 
-This can be useful if you want to get the type that is yielded in a generator function. Often the return type of those functions are not specified. This type works with both `Iterable`s and `AsyncIterable`s so it can be use with synchronous and asynchronous generators.
+This can be useful, for example, if you want to get the type that is yielded in a generator function. Often the return type of those functions are not specified.
+
+This type works with both `Iterable`s and `AsyncIterable`s, so it can be use with synchronous and asynchronous generators.
 
 Here is an example of `IterableElement` in action with a generator function:
 
