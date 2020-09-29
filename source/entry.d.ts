8bb9e9118a889edffec6447610507bea8ac5057e
Sindre Sorhus <sindresorhus@gmail.com>
2020-09-29 14:54:05 +0200

    Update entry.d.ts

--- source/entry.d.ts
+++ source/entry.d.ts
@@ -7,30 +7,29 @@ type ObjectEntry<BaseType> = [keyof BaseType, BaseType[keyof BaseType]];
 type SetEntry<BaseType> = BaseType extends Set<infer ItemType> ? [ItemType, ItemType] : never;
 
 /**
-Many collections have a `.entries()` method that will return an enumerable array of that structure's keys and values. The `Entry` type will return the type of that collection's entry given the type itself.
+Many collections have an `.entries()` method that will return an iterable of that structure's keys and values. The `Entry` type will return the type of that collection's entry given the type itself.
 
-For example the {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries|`Object`}, {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/entries|`Map`}, {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/entries|`Array`}, and {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/entries|`Set`}
-collections all have this method. Note that since `WeakMap` and `WeakSet` do not have this method since their entries are not enumerable.
+For example the {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Object/entries|`Object`}, {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Map/entries|`Map`}, {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/entries|`Array`}, and {@link https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Set/entries|`Set`} collections all have this method. Note that `WeakMap` and `WeakSet` do not have this method since their entries are not enumerable.
 
 @see `Entries` if you want to just access the type of the array of entries (which is the return of the `.entries()` method).
 
 @example
 ```
+import {Entry} from 'type-fest';
+
 interface Example {
 	someKey: number;
 }
 
-const manipulatesEntry = (example: Entry<Example>) => {
-	return [
-		// does some arbitrary processing on the key (with type information available)
-		example[0].toUpperCase(),
+const manipulatesEntry = (example: Entry<Example>) => [
+	// Does some arbitrary processing on the key (with type information available)
+	example[0].toUpperCase(),
 
-		// does some arbitrary processing on the value (with type information available)
-		example[1].toFixed(),
-	];
-};
+	// Does some arbitrary processing on the value (with type information available)
+	example[1].toFixed(),
+];
 
-const example: Example = { someKey: 1 };
+const example: Example = {someKey: 1};
 const entry = Object.entries(example)[0] as Entry<Example>;
 const output = manipulatesEntry(entry);
 
@@ -38,14 +37,14 @@ const output = manipulatesEntry(entry);
 const objectExample = {a: 1};
 const objectEntry: Entry<typeof objectExample> = ['a', 1];
 
-// Maps
-const mapExample = new Map([['a', 1]]);
-const mapEntry: Entry<typeof map> = ['a', 1];
-
 // Arrays
 const arrayExample = ['a', 1];
 const arrayEntryString: Entry<typeof arrayExample> = [0, 'a'];
 const arrayEntryNumber: Entry<typeof arrayExample> = [1, 1];
+
+// Maps
+const mapExample = new Map([['a', 1]]);
+const mapEntry: Entry<typeof map> = ['a', 1];
 
 // Sets
 const setExample = new Set(['a', 1]);
@@ -54,8 +53,8 @@ const setEntryNumber: Entry<typeof setExample> = [1, 1];
 ```
 */
 export type Entry<BaseType> =
-    BaseType extends Map<unknown, unknown> ? MapEntry<BaseType>
-  : BaseType extends Set<unknown> ? SetEntry<BaseType>
-  : BaseType extends unknown[] ? ArrayEntry<BaseType>
-  : BaseType extends object ? ObjectEntry<BaseType>
-	: never;
+	BaseType extends Map<unknown, unknown> ? MapEntry<BaseType>
+		: BaseType extends Set<unknown> ? SetEntry<BaseType>
+		: BaseType extends unknown[] ? ArrayEntry<BaseType>
+		: BaseType extends object ? ObjectEntry<BaseType>
+		: never;
