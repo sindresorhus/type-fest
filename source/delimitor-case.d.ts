3f5c159f1db579c0df8174e540ad0190a0498adb
Pelle Wessman <pelle@kodfabrik.se>
2020-11-16 16:33:28 +0100

    Add DelimitorCase<> and SnakeCase<>

---
+++ source/delimitor-case.d.ts
@@ -0,0 +1,70 @@
+type UpperCaseChars = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'X' | 'Y' | 'Z';
+type WordSeparators = '-'|'_'|' ';
+
+type SplitIncludingDelimitors<Source extends string, Delimitor extends string> =
+	Source extends '' ? [] :
+	Source extends `${infer FirstPart}${Delimitor}${infer SecondPart}` ?
+	(
+		Source extends `${FirstPart}${infer UsedDelimitor}${SecondPart}`
+			? [FirstPart, UsedDelimitor, ...SplitIncludingDelimitors<SecondPart, Delimitor>]
+			: never
+	) :
+	[Source];
+
+type StringPartToDelimitorCase<StringPart extends string, UsedWordSeparators extends string, UsedUpperCaseChars extends string, Delimitor extends string> =
+	StringPart extends UsedWordSeparators ? Delimitor :
+	StringPart extends UsedUpperCaseChars ? `${Delimitor}${Lowercase<StringPart>}` :
+	StringPart;
+
+type StringArrayToDelimitorCase<Parts extends any[], UsedWordSeparators extends string, UsedUpperCaseChars extends string, Delimitor extends string> =
+	Parts extends [`${infer FirstPart}`, ...infer RemainingParts]
+		? `${StringPartToDelimitorCase<FirstPart, UsedWordSeparators, UsedUpperCaseChars, Delimitor>}${StringArrayToDelimitorCase<RemainingParts, UsedWordSeparators, UsedUpperCaseChars, Delimitor>}`
+		: '';
+
+/**
+Converts a string literal that may use another casing than the desired delimitor based casing to the desired casing
+
+This can be useful when eg. converting a camel cased object property to eg. a kebab cased CSS class name or CLI option.
+
+@example
+```
+import {DelimitorCase, KebabCase, SnakeCase} from 'type-fest';
+
+type KebabCasedProps<T> = {
+	[K in keyof T as DelimitorCase<K, '-'>]: T[K]
+};
+
+type AlternativeKebabCasedProps<T> = {
+	[K in keyof T as KebabCase<K>]: T[K]
+};
+
+type SnakeCasedProps<T> = {
+	[K in keyof T as SnakeCase<K>]: T[K]
+};
+
+interface CliOptions {
+	dryRun: boolean;
+	includeFile: string;
+	foo: number;
+}
+
+const rawCliOptions: KebabCasedProps<CliOptions> = {
+	'dry-run': true,
+	'include-file': 'bar.js',
+	foo: 123
+};
+```
+*/
+
+export type DelimitorCase<Value, Delimitor extends string> = Value extends string
+	? StringArrayToDelimitorCase<
+		SplitIncludingDelimitors<Value, WordSeparators | UpperCaseChars>,
+		WordSeparators,
+		UpperCaseChars,
+		Delimitor
+	>
+	: Value;
+
+export type KebabCase<Value> = DelimitorCase<value, '-'>;
+
+export type SnakeCase<Value> = DelimitorCase<value, '_'>;
---
+++ test-d/delimitor-case.ts
@@ -0,0 +1,91 @@
+import {
+	DelimitorCase,
+	KebabCase,
+	SnakeCase
+} from '../source/delimitor-case';
+import {expectType, expectAssignable} from 'tsd';
+
+// DelimitorCase
+const delimitorFromCamel: DelimitorCase<'fooBar', '#'> = 'foo#bar';
+expectType<'foo#bar'>(delimitorFromCamel);
+
+const delimitorFromKebab: DelimitorCase<'foo-bar', '#'> = 'foo#bar';
+expectType<'foo#bar'>(delimitorFromKebab);
+
+const delimitorFromSpace: DelimitorCase<'foo bar', '#'> = 'foo#bar';
+expectType<'foo#bar'>(delimitorFromSpace);
+
+const delimitorFromSnake: DelimitorCase<'foo_bar', '#'> = 'foo#bar';
+expectType<'foo#bar'>(delimitorFromSnake);
+
+const noDelimitorFromMono: DelimitorCase<'foobar', '#'> = 'foobar';
+expectType<'foobar'>(noDelimitorFromMono);
+
+// KebabCase
+const kebabFromCamel: KebabCase<'fooBar'> = 'foo-bar';
+expectType<'foo-bar'>(kebabFromCamel);
+
+const kebabFromKebab: KebabCase<'foo-bar'> = 'foo-bar';
+expectType<'foo-bar'>(kebabFromKebab);
+
+const kebabFromSpace: KebabCase<'foo bar'> = 'foo-bar';
+expectType<'foo-bar'>(kebabFromSpace);
+
+const kebabFromSnake: KebabCase<'foo_bar'> = 'foo-bar';
+expectType<'foo-bar'>(kebabFromSnake);
+
+const noKebabFromMono: KebabCase<'foobar'> = 'foobar';
+expectType<'foobar'>(noKebabFromMono);
+
+// SnakeCase
+const snakeFromCamel: SnakeCase<'fooBar'> = 'foo_bar';
+expectType<'foo_bar'>(snakeFromCamel);
+
+const snakeFromKebab: SnakeCase<'foo-bar'> = 'foo-bar';
+expectType<'foo-bar'>(snakeFromKebab);
+
+const snakeFromSpace: SnakeCase<'foo bar'> = 'foo-bar';
+expectType<'foo-bar'>(snakeFromSpace);
+
+const snakeFromSnake: SnakeCase<'foo_bar'> = 'foo-bar';
+expectType<'foo-bar'>(snakeFromSnake);
+
+const noSnakeFromMono: SnakeCase<'foobar'> = 'foobar';
+expectType<'foobar'>(noSnakeFromMono);
+
+// Verifying example
+type KebabCasedProps<T> = {
+	[K in keyof T as DelimitorCase<K, '-'>]: T[K]
+};
+
+type AlternativeKebabCasedProps<T> = {
+	[K in keyof T as KebabCase<K>]: T[K]
+};
+
+type SnakeCasedProps<T> = {
+	[K in keyof T as SnakeCase<K>]: T[K]
+};
+
+interface CliOptions {
+	dryRun: boolean;
+	includeFile: string;
+	foo: number;
+}
+
+expectAssignable<KebabCasedProps<CliOptions>>({
+	'dry-run': true,
+	'include-file': 'bar.js',
+	foo: 123
+});
+
+expectAssignable<AlternativeKebabCasedProps<CliOptions>>({
+	'dry-run': true,
+	'include-file': 'bar.js',
+	foo: 123
+});
+
+expectAssignable<SnakeCasedProps<CliOptions>>({
+	'dry_run': true,
+	'include_file': 'bar.js',
+	foo: 123
+});
