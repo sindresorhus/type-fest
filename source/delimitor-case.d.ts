ec6170d5a1fe1b8e8e57ddc777b00b034b8b4f55
Sindre Sorhus <sindresorhus@gmail.com>
2020-11-20 19:44:01 +0700

    Update delimitor-case.d.ts

--- source/delimitor-case.d.ts
+++ source/delimitor-case.d.ts
@@ -1,5 +1,5 @@
 export type UpperCaseChars = 'A' | 'B' | 'C' | 'D' | 'E' | 'F' | 'G' | 'H' | 'I' | 'J' | 'K' | 'L' | 'M' | 'N' | 'O' | 'P' | 'Q' | 'R' | 'S' | 'T' | 'U' | 'V' | 'X' | 'Y' | 'Z';
-export type WordSeparators = '-'|'_'|' ';
+export type WordSeparators = '-' | '_' | ' ';
 
 export type SplitIncludingDelimitors<Source extends string, Delimitor extends string> =
 	Source extends '' ? [] :
