a36eed2ca74ffbf7a7b29f682b061a8ef143db66
Pelle Wessman <pelle@kodfabrik.se>
2020-11-24 17:51:53 +0100

    Improve documentation of camel case

--- source/camel-case.d.ts
+++ source/camel-case.d.ts
@@ -1,11 +1,19 @@
 import {WordSeparators} from './delimiter-case';
 
+/**
+Recursively splits a string literal into two parts on the first occurence
+of a specified string, returning an array literal of all the separate parts
+*/
 export type Split<S extends string, D extends string> =
 	string extends S ? string[] :
 	S extends '' ? [] :
 	S extends `${infer T}${D}${infer U}` ? [T, ...Split<U, D>] :
 	[S];
 
+/**
+Step by step takes the first item in an array literal, formats it and adds it
+to a string literal, then recursively appending the remainder.
+*/
 type InnerCamelCaseStringArray<Parts extends any[], PreviousPart> =
 	Parts extends [`${infer FirstPart}`, ...infer RemainingParts]
 		? FirstPart extends undefined
@@ -15,6 +23,11 @@ type InnerCamelCaseStringArray<Parts extends any[], PreviousPart> =
 					: `${PreviousPart extends '' ? FirstPart : Capitalize<FirstPart>}${InnerCamelCaseStringArray<RemainingParts, FirstPart>}`
 		: '';
 
+/**
+Starts the concatenating the output of Split<>, an array literal of strings, into a camel cased string literal.
+
+Separate from InnerCamelCaseStringArray<> to keep a clean API outwards to the rest of the code
+*/
 type CamelCaseStringArray<Parts extends string[]> =
 	Parts extends [`${infer FirstPart}`, ...infer RemainingParts]
 		? `${FirstPart}${InnerCamelCaseStringArray<RemainingParts, FirstPart>}`
@@ -28,6 +41,12 @@ This can be useful when, for example, converting some kebab-cased command-line f
 @example
 ```
 import {CamelCase} from 'type-fest';
+
+// Simple
+
+const someVariable: CamelCase<'foo-bar'> = 'fooBar';
+
+// Advanced
 
 type CamelCasedProps<T> = {
 	[K in keyof T as CamelCase<K>]: T[K]
