536ef2e5693e0a3922a40d19a97be665bbd9d581
Sindre Sorhus <sindresorhus@gmail.com>
2020-11-25 15:08:07 +0700

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,2 @@
-// These are all the basic types that's compatible with all supported TypeScript versions
+// These are all the basic types that's compatible with all supported TypeScript versions.
 export * from './base';
