59e15485c94dddc0de801f95b4d9bf6450df011c
mmkal <misha.kaletsky@gmail.com>
2020-11-26 11:16:31 -0500

    Improve docs/naming

--- ts41/get.d.ts
+++ ts41/get.d.ts
@@ -2,43 +2,9 @@ import {Split} from './utilities';
 import {Integers} from '../source/utilities';
 
 /**
- * Gets a deeply-nested property from an object, like lodash's `get` method.
- *
- * Use-case: retrieve a property from deep inside an API response or other complex object.
- *
- * @example
- * import { Get } from 'type-fest'
- *
- * interface ApiResponse {
- *   hits: {
- *     hits: Array<{
- *       _id: string
- *       _source: {
- *         name: Array<{
- *           given: string[]
- *           family: string
- *         }>
- *         birthDate: string
- *       }
- *     }>
- *   }
- * }
- *
- * type Name = Get<ApiResponse, 'hits.hits[0]._source.name'> // Array<{ given: string[]; family: string }>
- *
- * @explanation
- *
- * This works by first splitting the path based on `.` and `[...]` characters into a tuple of string keys.
- * Then it recursively uses the head key to get the next property of the current object, until there are no keys
- * left. Number keys extract the item type from arrays, or are converted to strings to extract types from tuples
- * and dictionaries with number keys.
+Like @see Get but receives an array of strings as a path parameter.
  */
-export type Get<Object, Path extends string> = GetWithPath<Object, ToPath<Path>>;
-
-/**
- * Like @see Get but receives an array of strings as a path parameter.
- */
-type GetWithPath<T, Keys extends string[]> =
+type GetWithPath<T, Keys extends readonly string[]> =
   Keys extends []
   ? T
   : Keys extends [infer Head, ...infer Tail]
@@ -61,22 +27,69 @@ type ConsistsOnlyOf<S extends string, C extends string> =
 
 type IsInteger<S extends string> = ConsistsOnlyOf<S, Integers>;
 
+/**
+Convert a type which may have number keys to one with string keys, making it possible to index
+using strings retrieved from template types.
+
+@example
+```
+type WithNumbers = { foo: string; 0: boolean }
+type WithStrings = WithStringKeys<WithNumbers>
+
+type WithNumbersKeys = keyof WithNumbers // evaluates to 'foo' | 0
+type WithStringsKeys = keyof WithStrings // evaluates to 'foo' | '0'
+```
+ */
 type WithStringKeys<T extends Record<string | number, any>> = {
   [K in `${Extract<keyof T, string | number>}`]: T[K]
 };
 
 /**
- * Get a property of an object or array. Works when indexing arrays using number-literal-strings, e.g. `PropertyOf<number[], '0'>  = number`,
- * and when indexing objects with number keys.
- * Returns `never` if `Key` is not a property of `Object`,
+Get a property of an object or array. Works when indexing arrays using number-literal-strings, e.g. `PropertyOf<number[], '0'>  = number`,
+and when indexing objects with number keys.
+Returns `never` if `Key` is not a property of `Object`,
  */
-type PropertyOf<Object, Key extends string> =
-  Key extends keyof Object
-  ? Object[Key]
-  : Object extends Array<infer Item>
+type PropertyOf<ObjectType, Key extends string> =
+  Key extends keyof ObjectType
+  ? ObjectType[Key]
+  : ObjectType extends Array<infer Item>
   ? IsInteger<Key> extends true
   ? Item
   : never
-  : Key extends keyof WithStringKeys<Object>
-  ? WithStringKeys<Object>[Key]
+  : Key extends keyof WithStringKeys<ObjectType>
+  ? WithStringKeys<ObjectType>[Key]
   : never;
+
+/**
+Gets a deeply-nested property from an object, like lodash's `get` method.
+
+Use-case: retrieve a property from deep inside an API response or other complex object.
+
+@example
+```
+import { Get } from 'type-fest'
+
+interface ApiResponse {
+  hits: {
+    hits: Array<{
+      _id: string
+      _source: {
+        name: Array<{
+          given: string[]
+          family: string
+        }>
+        birthDate: string
+      }
+    }>
+  }
+}
+
+type Name = Get<ApiResponse, 'hits.hits[0]._source.name'> // Array<{ given: string[]; family: string }>
+```
+
+This works by first splitting the path based on `.` and `[...]` characters into a tuple of string keys.
+Then it recursively uses the head key to get the next property of the current object, until there are no keys
+left. Number keys extract the item type from arrays, or are converted to strings to extract types from tuples
+and dictionaries with number keys.
+ */
+export type Get<Object, Path extends string> = GetWithPath<Object, ToPath<Path>>;
