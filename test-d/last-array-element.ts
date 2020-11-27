cc359f1a2502b113cefac178caa60a4caebbf889
Kai Niedziela <kai.niedziela@outlook.com>
2020-11-27 17:48:35 +0100

    Update test-d/last-array-element.ts
    Co-authored-by: Misha Kaletsky <15040698+mmkal@users.noreply.github.com>

--- test-d/last-array-element.ts
+++ test-d/last-array-element.ts
@@ -1,7 +1,7 @@
 import {expectType} from 'tsd';
 import {LastArrayElement} from '../ts41';
 
-declare function lastOf<V extends any[], L extends LastArrayElement<V>>(array: V): L;
+declare function lastOf<V extends any[]>(array: V): LastArrayElement<V>;
 const array = ['foo', 2];
 
 expectType<number>(lastOf(array));
