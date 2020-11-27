5eb73149cb1e7f177469dff86f296b059c019c71
Kai Niedziela <kai.niedziela@outlook.com>
2020-11-27 23:43:14 +0100

    Improve `LastArrayElement` test cases

--- test-d/last-array-element.ts
+++ test-d/last-array-element.ts
@@ -1,8 +1,9 @@
 import {expectType} from 'tsd';
 import {LastArrayElement} from '../ts41';
 
-declare function lastOf<V extends any[]>(array: V): LastArrayElement<V>;
-const array = ['foo', 2];
+declare function lastOf<V extends [any, ...any]>(array: V): LastArrayElement<V>;
+const array: ['foo', 2, 'bar'] = ['foo', 2, 'bar'];
+const mixedArray: ['bar', 'foo', 2] = ['bar', 'foo', 2];
 
-expectType<number>(lastOf(array));
-expectType<string>(lastOf(array.reverse()));
+expectType<'bar'>(lastOf(array));
+expectType<number>(lastOf(mixedArray));
