0804a7516888aea9a8824045ab9424bcc47ee526
mmkal <misha.kaletsky@gmail.com>
2020-11-29 10:38:33 -0500

    Tab indentation

--- test-d/get.ts
+++ test-d/get.ts
@@ -2,18 +2,18 @@ import {Get} from '../ts41/get';
 import {expectTypeOf} from 'expect-type';
 
 interface ApiResponse {
-  hits: {
-    hits: Array<{
-      _id: string;
-      _source: {
-        name: Array<{
-          given: string[];
-          family: string;
-        }>;
-        birthDate: string;
-      };
-    }>;
-  };
+	hits: {
+		hits: Array<{
+			_id: string;
+			_source: {
+				name: Array<{
+					given: string[];
+					family: string;
+				}>;
+				birthDate: string;
+			};
+		}>;
+	};
 }
 
 expectTypeOf<Get<ApiResponse, 'hits.hits[0]._source.name'>>().toEqualTypeOf<Array<{given: string[]; family: string}>>();
@@ -23,14 +23,14 @@ expectTypeOf<Get<ApiResponse, 'hits.hits.0._source.name'>>().toEqualTypeOf<Array
 expectTypeOf<Get<ApiResponse, 'hits.someNonsense.notTheRightPath'>>().toBeUnknown();
 
 interface WithTuples {
-  foo: [
-    {
+	foo: [
+		{
 			bar: number;
 		},
-    {
+		{
 			baz: boolean;
 		}
-  ];
+	];
 }
 
 expectTypeOf<Get<WithTuples, 'foo[0].bar'>>().toBeNumber();
@@ -40,11 +40,11 @@ expectTypeOf<Get<WithTuples, 'foo[1].bar'>>().toBeUnknown();
 expectTypeOf<Get<WithTuples, 'foo.1.bar'>>().toBeUnknown();
 
 interface WithNumberKeys {
-  foo: {
-    1: {
-      bar: number;
-    };
-  };
+	foo: {
+		1: {
+			bar: number;
+		};
+	};
 }
 
 expectTypeOf<Get<WithNumberKeys, 'foo[1].bar'>>().toBeNumber();
@@ -53,21 +53,21 @@ expectTypeOf<Get<WithNumberKeys, 'foo.1.bar'>>().toBeNumber();
 expectTypeOf<Get<WithNumberKeys, 'foo[2].bar'>>().toBeUnknown();
 expectTypeOf<Get<WithNumberKeys, 'foo.2.bar'>>().toBeUnknown();
 
-// test readonly, ReadonlyArray, optional props, unions with null
+// Test readonly, ReadonlyArray, optional props, unions with null
 
 interface WithModifiers {
-  foo: ReadonlyArray<{
-    bar?: {
-      readonly baz: {
-        qux: number;
-      };
-    };
-    abc: {
-      def: {
-        ghi: string;
-      };
-    } | null;
-  }>;
+	foo: ReadonlyArray<{
+		bar?: {
+			readonly baz: {
+				qux: number;
+			};
+		};
+		abc: {
+			def: {
+				ghi: string;
+			};
+		} | null;
+	}>;
 }
 
 expectTypeOf<Get<WithModifiers, 'foo[0].bar.baz'>>().toEqualTypeOf<{ qux: number } | undefined>();
--- ts41/get.d.ts
+++ ts41/get.d.ts
@@ -5,11 +5,11 @@ import {StringDigit} from '../source/utilities';
 Like @see Get but receives an array of strings as a path parameter.
 */
 type GetWithPath<T, Keys extends readonly string[]> =
-  Keys extends []
-  ? T
-  : Keys extends [infer Head, ...infer Tail]
-  ? GetWithPath<PropertyOf<T, Extract<Head, string>>, Extract<Tail, string[]>>
-  : never;
+	Keys extends []
+	? T
+	: Keys extends [infer Head, ...infer Tail]
+	? GetWithPath<PropertyOf<T, Extract<Head, string>>, Extract<Tail, string[]>>
+	: never;
 
 /**
 Splits a dot-prop style path into a tuple comprised of the properties in the path. Handles square-bracket notation.
@@ -23,9 +23,9 @@ ToPath<'foo[0].bar.baz'> // ['foo', '0', 'bar', 'baz']
 type ToPath<S extends string> = Split<FixPathSquareBrackets<S>, '.'>;
 
 type FixPathSquareBrackets<S extends string> =
-  S extends `${infer T}[${infer U}]${infer V}`
-  ? `${T}.${U}${FixPathSquareBrackets<V>}`
-  : S;
+	S extends `${infer T}[${infer U}]${infer V}`
+	? `${T}.${U}${FixPathSquareBrackets<V>}`
+	: S;
 
 /**
 Returns true if S is made up out of C repeated 0 or more times
@@ -38,11 +38,11 @@ ConsistsOnlyOf<'', 'a'> // true
 ```
 */
 type ConsistsOnlyOf<S extends string, C extends string> =
-  S extends ''
-  ? true
-  : S extends `${C}${infer Tail}`
-  ? ConsistsOnlyOf<Tail, C>
-  : false;
+	S extends ''
+	? true
+	: S extends `${C}${infer Tail}`
+	? ConsistsOnlyOf<Tail, C>
+	: false;
 
 /**
 Convert a type which may have number keys to one with string keys, making it possible to index using strings retrieved from template types.
@@ -59,25 +59,25 @@ type WithStringsKeys = keyof WithStrings;
 ```
 */
 type WithStringKeys<T extends Record<string | number, any>> = {
-  [K in `${Extract<keyof T, string | number>}`]: T[K]
+	[K in `${Extract<keyof T, string | number>}`]: T[K]
 };
 
 /**
-Get a property of an object or array. Works when indexing arrays using number-literal-strings, e.g. `PropertyOf<number[], '0'>  = number`, and when indexing objects with number keys.
+Get a property of an object or array. Works when indexing arrays using number-literal-strings, e.g. `PropertyOf<number[], '0'>	= number`, and when indexing objects with number keys.
 Returns `unknown` if `Key` is not a property of `ObjectType`,
  */
 type PropertyOf<ObjectType, Key extends string> =
-  ObjectType extends null | undefined
-  ? undefined
-  : Key extends keyof ObjectType
-  ? ObjectType[Key]
-  : ObjectType extends ArrayLike<infer Item>
-  ? ConsistsOnlyOf<Key, StringDigit> extends true
-  ? Item
-  : unknown
-  : Key extends keyof WithStringKeys<ObjectType>
-  ? WithStringKeys<ObjectType>[Key]
-  : unknown;
+	ObjectType extends null | undefined
+	? undefined
+	: Key extends keyof ObjectType
+	? ObjectType[Key]
+	: ObjectType extends ArrayLike<infer Item>
+	? ConsistsOnlyOf<Key, StringDigit> extends true
+	? Item
+	: unknown
+	: Key extends keyof WithStringKeys<ObjectType>
+	? WithStringKeys<ObjectType>[Key]
+	: unknown;
 
 // This works by first splitting the path based on `.` and `[...]` characters into a tuple of string keys. Then it recursively uses the head key to get the next property of the current object, until there are no keys left. Number keys extract the item type from arrays, or are converted to strings to extract types from tuples and dictionaries with number keys.
 /**
@@ -91,25 +91,25 @@ import { Get } from 'type-fest';
 import * as lodash from 'lodash';
 
 const get = <ObjectType, Path extends string>(object: ObjectType, path: Path): Get<ObjectType, Path> =>
-  lodash.get(object, path);
+	lodash.get(object, path);
 
 interface ApiResponse {
-  hits: {
-    hits: Array<{
-      _id: string
-      _source: {
-        name: Array<{
-          given: string[]
-          family: string
-        }>
-        birthDate: string
-      }
-    }>
-  }
+	hits: {
+		hits: Array<{
+			_id: string
+			_source: {
+				name: Array<{
+					given: string[]
+					family: string
+				}>
+				birthDate: string
+			}
+		}>
+	}
 }
 
 const getName = (apiResponse: ApiResponse) =>
-  get(apiResponse, 'hits.hits[0]._source.name'); // returns Array<{ given: string[]; family: string }>
+	get(apiResponse, 'hits.hits[0]._source.name'); // returns Array<{ given: string[]; family: string }>
 ```
 */
 export type Get<Object, Path extends string> = GetWithPath<Object, ToPath<Path>>;
