f8c62ec63723d16ff06db6e09bd5d099373463ea
Kid Commit <26439946+ferdicus@users.noreply.github.com>
2021-01-06 06:45:10 +0100

    Update except.d.ts
    Just add some newlines. No need to scroll all the way to Saturn to read the rest of the sentence

--- source/except.d.ts
+++ source/except.d.ts
@@ -1,7 +1,9 @@
 /**
 Create a type from an object type without certain keys.
 
-This type is a stricter version of [`Omit`](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-5.html#the-omit-helper-type). The `Omit` type does not restrict the omitted keys to be keys present on the given type, while `Except` does. The benefits of a stricter type are avoiding typos and allowing the compiler to pick up on rename refactors automatically.
+This type is a stricter version of [`Omit`](https://www.typescriptlang.org/docs/handbook/release-notes/typescript-3-5.html#the-omit-helper-type). 
+The `Omit` type does not restrict the omitted keys to be keys present on the given type, while `Except` does. 
+The benefits of a stricter type are avoiding typos and allowing the compiler to pick up on rename refactors automatically.
 
 Please upvote [this issue](https://github.com/microsoft/TypeScript/issues/30825) if you want to have the stricter version as a built-in in TypeScript.
 
