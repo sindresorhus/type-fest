3bace1e748dadc606be06f6d1d98e209b1531bc9
Sindre Sorhus <sindresorhus@gmail.com>
2021-03-10 17:59:01 +0700

    Update trim.d.ts

--- ts41/trim.d.ts
+++ ts41/trim.d.ts
@@ -1,17 +1,22 @@
 /**
-Removes spaces from the edges of a string.
+Remove spaces from the left side.
+*/
+type TrimLeft<V extends string> = V extends ` ${infer R}` ? TrimLeft<R> : V;
+
+/**
+Remove spaces from the right side.
+*/
+type TrimRight<V extends string> = V extends `${infer R} ` ? TrimRight<R> : V;
+
+/**
+Remove leading and trailing spaces from a string.
 
 @example
 ```
 import {Trim} from 'type-fest';
 
-Trim<' foo '> // -> 'foo';
+Trim<' foo '>
+//=> 'foo'
 ```
 */
 export type Trim<V extends string> = TrimLeft<TrimRight<V>>;
-
-/** Removes spaces from the left side. */
-type TrimLeft<V extends string> = V extends ` ${infer R}` ? TrimLeft<R> : V;
-
-/** Removes spaces from the right side. */
-type TrimRight<V extends string> = V extends `${infer R} ` ? TrimRight<R> : V;
