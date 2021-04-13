73e9aa7f9ab759cb14e47202b8873039a95853f6
Armedi <armedi@outlook.com>
2021-04-14 00:01:23 +0700

    Add missing exports for some of the TypeScript 4.1-only types (#196)

--- ts41/index.d.ts
+++ ts41/index.d.ts
@@ -3,10 +3,20 @@ export * from '../base';
 
 // These are special types that require at least TypeScript 4.1.
 export {CamelCase} from './camel-case';
+export {CamelCasedProperties} from './camel-cased-properties';
+export {CamelCasedPropertiesDeep} from './camel-cased-properties-deep';
 export {KebabCase} from './kebab-case';
+export {KebabCasedProperties} from './kebab-cased-properties';
+export {KebabCasedPropertiesDeep} from './kebab-cased-properties-deep';
 export {PascalCase} from './pascal-case';
+export {PascalCasedProperties} from './pascal-cased-properties';
+export {PascalCasedPropertiesDeep} from './pascal-cased-properties-deep';
 export {SnakeCase} from './snake-case';
+export {SnakeCasedProperties} from './snake-cased-properties';
+export {SnakeCasedPropertiesDeep} from './snake-cased-properties-deep';
 export {DelimiterCase} from './delimiter-case';
+export {DelimiterCasedProperties} from './delimiter-cased-properties';
+export {DelimiterCasedPropertiesDeep} from './delimiter-cased-properties-deep';
 export {Split} from './split';
 export {Trim} from './trim';
 export {Get} from './get';
