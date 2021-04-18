563dcac39b8c8cdcfe3a408c8a27260a8bda772d
Sindre Sorhus <sindresorhus@gmail.com>
2021-04-18 23:27:42 +0700

    Update set-non-nullable.ts

--- test-d/set-non-nullable.ts
+++ test-d/set-non-nullable.ts
@@ -1,7 +1,7 @@
 import {expectType, expectError} from 'tsd';
 import {SetNonNullable} from '..';
 
-// Update nullish fields to be non-null, leave one unchanged.
+// Update nullish fields to be non-null and leave one unchanged.
 declare const variation1: SetNonNullable<
 	{
 		a?: string;
@@ -12,20 +12,20 @@ declare const variation1: SetNonNullable<
 	},
 	'b' | 'c' | 'd' | 'e'
 >;
-expectType<{ a?: string; b: string; c: string; d: string; e: string }>(
+expectType<{a?: string; b: string; c: string; d: string; e: string}>(
 	variation1
 );
 
-// Remove optional, undefined and null, leave one unchanged.
+// Remove optional, undefined, and null. Leave one unchanged.
 declare const variation2: SetNonNullable<
-	{ a?: number | null | undefined; b?: number | null | undefined },
+	{a?: number | null | undefined; b?: number | null | undefined},
 	'a'
 >;
 expectType<{ a: number; b?: number | null | undefined }>(variation2);
 
 // Fail if type changes even if optional is right.
 declare const variation3: SetNonNullable<
-	{ a?: number; b: string; c?: boolean | null | undefined },
+	{a?: number; b: string; c?: boolean | null | undefined},
 	'b' | 'c'
 >;
-expectError<{ a?: boolean; b: string; c: boolean }>(variation3);
+expectError<{a?: boolean; b: string; c: boolean}>(variation3);
