00fd04b9f2b2c1838cd62300287a2c29988e2e7d
Sindre Sorhus <sindresorhus@gmail.com>
2021-06-01 16:33:20 +0700

    Update screaming-snake-case.d.ts

--- ts41/screaming-snake-case.d.ts
+++ ts41/screaming-snake-case.d.ts
@@ -2,7 +2,7 @@ import {SplitIncludingDelimiters} from './delimiter-case';
 import {SnakeCase} from './snake-case';
 
 /**
-Returns a boolean on whether the given array contains a certain item.
+Returns a boolean for whether the given array includes the given item.
 */
 type Includes<Value extends any[], Item> = {
 	[P in keyof Value & number as Value[P]]: true;
@@ -28,27 +28,7 @@ This can be useful when, for example, converting a camel-cased object property t
 ```
 import {ScreamingSnakeCase} from 'type-fest';
 
-// Simple
-
 const someVariable: ScreamingSnakeCase<'fooBar'> = 'FOO_BAR';
-
-// Advanced
-
-type ScreamingSnakeCasedProperties<T> = {
-	[K in keyof T as ScreamingSnakeCase<K>]: T[K]
-};
-
-interface ModelProps {
-	isHappy: boolean;
-	fullFamilyName: string;
-	foo: number;
-}
-
-const dbResult: ScreamingSnakeCasedProperties<ModelProps> = {
-	'is_happy': true,
-	'full_family_name': 'Carla Smith',
-	foo: 123
-};
 ```
 
 @category Template Literals
