c47674d137486ee39d70204409fa5bce5f58ffe3
replygirl <moon@moon.fyi>
2021-06-05 16:46:55 -0400

    add aliases for template literal case types

--- ts41/index.d.ts
+++ ts41/index.d.ts
@@ -2,19 +2,68 @@
 export * from '../base';
 
 // These are special types that require at least TypeScript 4.1.
-export {CamelCase} from './camel-case';
-export {CamelCasedProperties} from './camel-cased-properties';
-export {CamelCasedPropertiesDeep} from './camel-cased-properties-deep';
-export {KebabCase} from './kebab-case';
-export {KebabCasedProperties} from './kebab-cased-properties';
-export {KebabCasedPropertiesDeep} from './kebab-cased-properties-deep';
-export {PascalCase} from './pascal-case';
-export {PascalCasedProperties} from './pascal-cased-properties';
-export {PascalCasedPropertiesDeep} from './pascal-cased-properties-deep';
-export {SnakeCase} from './snake-case';
-export {SnakeCasedProperties} from './snake-cased-properties';
-export {SnakeCasedPropertiesDeep} from './snake-cased-properties-deep';
-export {ScreamingSnakeCase} from './screaming-snake-case';
+export {
+	CamelCase,
+	CamelCase as DromedaryCase,
+} from './camel-case';
+export {
+	CamelCasedProperties,
+	CamelCasedProperties as DromedaryCasedProperties,
+} from './camel-cased-properties';
+export {
+	CamelCasedPropertiesDeep,
+	CamelCasedPropertiesDeep as DromedaryCasedPropertiesDeep,
+} from './camel-cased-properties-deep';
+export {
+	KebabCase,
+	KebabCase as DashCase,
+	KebabCase as LispCase,
+	KebabCase as ParamCase,
+} from './kebab-case';
+export {
+	KebabCasedProperties,
+	KebabCasedProperties as DashCasedProperties,
+	KebabCasedProperties as LispCasedProperties,
+	KebabCasedProperties as ParamCasedProperties,
+} from './kebab-cased-properties';
+export {
+	KebabCasedPropertiesDeep,
+	KebabCasedPropertiesDeep as DashCasedPropertiesDeep,
+	KebabCasedPropertiesDeep as LispCasedPropertiesDeep,
+	KebabCasedPropertiesDeep as ParamCasedPropertiesDeep,
+} from './kebab-cased-properties-deep';
+export {
+	PascalCase,
+	PascalCase as StudlyCase,
+	PascalCase as UpperCamelCase,
+} from './pascal-case';
+export {
+	PascalCasedProperties,
+	PascalCasedProperties as StudlyCasedProperties,
+	PascalCasedProperties as UpperCamelCasedProperties,
+} from './pascal-cased-properties';
+export {
+	PascalCasedPropertiesDeep,
+	PascalCasedPropertiesDeep as StudlyCasedPropertiesDeep,
+	PascalCasedPropertiesDeep as UpperCamelCasedPropertiesDeep,
+} from './pascal-cased-properties-deep';
+export {
+	SnakeCase,
+	SnakeCase as PotholeCase,
+} from './snake-case';
+export {
+	SnakeCasedProperties,
+	SnakeCasedProperties as PotholeCasedProperties,
+} from './snake-cased-properties';
+export {
+	SnakeCasedPropertiesDeep,
+	SnakeCasedPropertiesDeep as PotholeCasedPropertiesDeep
+} from './snake-cased-properties-deep';
+export {
+	ScreamingSnakeCase,
+	ScreamingSnakeCase as ConstantCase,
+	ScreamingSnakeCase as MacroCase
+} from './screaming-snake-case';
 export {DelimiterCase} from './delimiter-case';
 export {DelimiterCasedProperties} from './delimiter-cased-properties';
 export {DelimiterCasedPropertiesDeep} from './delimiter-cased-properties-deep';
