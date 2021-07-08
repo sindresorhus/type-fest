a7fa37a6aeeae590516b2c8c2b6a616395b8c35c
Hunter Kohler <jhunterkohler@gmail.com>
2021-07-08 07:49:36 -0400

    Fix `Split` type (#226)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- test-d/split.ts
+++ test-d/split.ts
@@ -1,12 +1,30 @@
 import {expectType} from 'tsd';
 import {Split} from '../ts41';
 
-declare function split<S extends string, D extends string>(string: S, separator: D): Split<S, D>;
+declare function split<
+	S extends string,
+	Delimiter extends string
+>(string: S, separator: Delimiter): Split<S, Delimiter>;
 
 const items = 'foo,bar,baz,waldo';
-const array = split(items, ',');
 
-expectType<'foo'>(array[0]);
-expectType<'bar'>(array[1]);
-expectType<'baz'>(array[2]);
-expectType<'waldo'>(array[3]);
+// General use.
+expectType<['foo', 'bar', 'baz', 'waldo']>(split(items, ','));
+
+// Missing replacement character in original string.
+expectType<['foo,bar,baz,waldo']>(split(items, ' '));
+
+// Empty string split (every character).
+expectType<[
+	'f', 'o', 'o', ',', 'b', 'a', 'r', ',',
+	'b', 'a', 'z', ',', 'w', 'a', 'l', 'd', 'o'
+]>(split(items, ''));
+
+// Split single same character.
+expectType<['', '']>(split(' ', ' '));
+
+// Split empty string by empty string.
+expectType<[]>(split('', ''));
+
+// Split empty string by any string.
+expectType<['']>(split('', ' '));
