f8d68d31f624aef3135be9e9f5c1a661c00c640b
David Smith <smith.w.da@gmail.com>
2021-08-06 10:25:16 +1000

    Improve documentation for `SetNonNullable` type

--- source/set-non-nullable.d.ts
+++ source/set-non-nullable.d.ts
@@ -2,7 +2,8 @@ import {Except} from './except';
 import {Simplify} from './simplify';
 
 /**
-Create a type removes nullish (optional, undefined and null) types from the given keys. The remaining keys are kept as is.
+Create a type that removes nullish (optional, undefined and null) types from the given keys. The remaining keys are kept as is.
+This behaves in a similar manor to `SetRequired<T,K>` by removing optionals, however also applies the `NonNullable<T>` operator to the values at the specified keys.
 
 Use-case: You want to define a single model where the only thing that changes is whether or not some of the keys are required.
 
@@ -26,7 +27,24 @@ type SomeNonNullable = SetNonNullable<Foo, 'b' | 'c' | 'd' | 'e'>;
 //  d: number; // Is now not null
 //	e: string; // Is now not undefined
 // }
+
+function hasRequiredKeys(foo: Foo): foo is SomeNonNullable;
 ```
+
+This is especially useful in cases where you want to write a type-guard that asserts one or more values of an object are not null.
+
+@example
+```
+type Person {
+	firstName?: string | null;
+	lastName?: string | null;
+}
+
+function hasFirstName(person: Person) person is SetNonNullable<Person, "firstName"> {
+	return typeof person.firstName === "string";
+}
+```
+
 */
 export type SetNonNullable<BaseType, Keys extends keyof BaseType> = Simplify<
 	// Pick just the keys that are optional from the base type.
