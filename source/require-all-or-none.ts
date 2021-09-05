076dcf1968fb8669954edbd85e550582ed85426e
Aaron Harper <aharper@duosecurity.com>
2021-09-05 18:40:37 -0400

    Use tabs

--- source/require-all-or-none.ts
+++ source/require-all-or-none.ts
@@ -30,7 +30,7 @@ const responder2: RequireAllOrNone<Responder, 'text' | 'json'> = {
 @category Utilities
 */
 export type RequireAllOrNone<ObjectType, KeysType extends keyof ObjectType = never> = (
-    | Required<Pick<ObjectType, KeysType>> // Require all of the given keys.
-    | Partial<Record<KeysType, never>> // Require none of the given keys.
+	| Required<Pick<ObjectType, KeysType>> // Require all of the given keys.
+	| Partial<Record<KeysType, never>> // Require none of the given keys.
 ) &
-    Omit<ObjectType, KeysType>; // The rest of the keys.
+	Omit<ObjectType, KeysType>; // The rest of the keys.
