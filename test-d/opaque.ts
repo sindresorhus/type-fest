70fe5242645071808bc556e8aaa0d2ba9e12991b
Sindre Sorhus <sindresorhus@gmail.com>
2021-09-10 12:08:24 +0700

    Add failing test for #108

--- test-d/opaque.ts
+++ test-d/opaque.ts
@@ -20,3 +20,20 @@ type Person = {
 	id: Opaque<number, Person>; // eslint-disable-line @typescript-eslint/no-unused-vars
 	name: string;
 };
+
+// Failing test for https://github.com/sindresorhus/type-fest/issues/108
+// Use `Opaque` value as `Record` index type.
+type UUID = Opaque<string, 'UUID'>;
+type NormalizedDictionary<T> = Record<UUID, T>;
+type Foo = {bar: string};
+
+const userEntities: NormalizedDictionary<Foo> = {
+	'7dd4a16e-d5ee-454c-b1d0-71e23d9fa70b': {bar: 'John'},
+	'6ce31270-31eb-4a72-a9bf-43192d4ab436': {bar: 'Doe'},
+};
+
+const johnsId = '7dd4a16e-d5ee-454c-b1d0-71e23d9fa70b' as UUID;
+
+// @ts-expect-error
+const userJohn = userEntities[johnsId]; // eslint-disable-line @typescript-eslint/no-unused-vars
+/// expectType<Foo>(userJohn);
