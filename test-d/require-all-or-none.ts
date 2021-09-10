28199b08206a955c78098e650d0f9f73331dd708
Aaron Harper <aaron.harper@outlook.com>
2021-09-09 23:39:53 -0400

    Add `RequireAllOrNone` type (#254)

---
+++ test-d/require-all-or-none.ts
@@ -0,0 +1,24 @@
+import {expectError, expectAssignable} from 'tsd';
+import {RequireAllOrNone} from '../index';
+
+type SystemMessages = {
+	default: string;
+
+	macos: string;
+	linux: string;
+
+	optional?: string;
+};
+
+type ValidMessages = RequireAllOrNone<SystemMessages, 'macos' | 'linux'>;
+const test = (_: ValidMessages): void => {}; // eslint-disable-line @typescript-eslint/no-empty-function
+
+test({default: 'hello'});
+test({macos: 'yo', linux: 'sup', optional: 'howdy', default: 'hello'});
+
+expectError(test({}));
+expectError(test({macos: 'hey', default: 'hello'}));
+expectError(test({linux: 'hey', default: 'hello'}));
+
+declare const oneWithoutKeys: RequireAllOrNone<{a: number; b: number}>;
+expectAssignable<{a: number; b: number}>(oneWithoutKeys);
