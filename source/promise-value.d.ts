daca32aa83af2b9167fbe8955346fa473e8e54cb
Sindre Sorhus <sindresorhus@gmail.com>
2021-09-10 11:57:33 +0700

    Simplify `PromiseValue` type
    Fixes #131

--- source/promise-value.d.ts
+++ source/promise-value.d.ts
@@ -24,6 +24,4 @@ let recursiveAsyncData: PromiseValue<RecursiveAsyncData> = Promise.resolve(Promi
 
 @category Utilities
 */
-export type PromiseValue<PromiseType, Otherwise = PromiseType> = PromiseType extends Promise<infer Value>
-	? {0: PromiseValue<Value>; 1: Value}[PromiseType extends Promise<unknown> ? 0 : 1]
-	: Otherwise;
+export type PromiseValue<PromiseType> = PromiseType extends PromiseLike<infer Value> ? PromiseValue<Value> : PromiseType;
--- test-d/promise-value.ts
+++ test-d/promise-value.ts
@@ -3,7 +3,6 @@ import {PromiseValue} from '../index';
 
 type NumberPromise = Promise<number>;
 type NestedPromise = Promise<NumberPromise>;
-type Otherwise = object;
 
 // Test the normal behaviour.
 expectAssignable<PromiseValue<NumberPromise>>(2);
@@ -13,6 +12,3 @@ expectAssignable<PromiseValue<NestedPromise>>(2);
 
 // Test what happens when the `PromiseValue` type is not handed a `Promise` type.
 expectAssignable<PromiseValue<number>>(2);
-
-// Test the `Otherwise` generic parameter.
-expectAssignable<PromiseValue<number, Otherwise>>({});
