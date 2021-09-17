8d740cc51f60748abaa2c828ca872d79f3619822
wafuwafu13 <mariobaske@i.softbank.jp>
2021-09-17 18:35:41 +0900

    hasWrongExtensionFile -> hasIncorrectFileExtension

--- script/test/source-files-extension.js
+++ script/test/source-files-extension.js
@@ -6,7 +6,7 @@ const checkSourceFilesExtension = async () => {
 	try {
 		const files = await fs.promises.readdir('source');
 
-		let hasWrongExtensionFile = false;
+		let hasIncorrectFileExtension = false;
 		for (const file of files) {
 			if (!file.endsWith('.d.ts')) {
 				hasWrongExtensionFile = true;
@@ -14,7 +14,7 @@ const checkSourceFilesExtension = async () => {
 			}
 		}
 
-		if (hasWrongExtensionFile) {
+		if (hasIncorrectFileExtension) {
 			process.exitCode = 1;
 		}
 	} catch (error) {
