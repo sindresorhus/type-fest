78ecaa9c45b22c11d83f016cddd58d024d922f99
Younho Choo <yo+github@younho9.com>
2021-10-19 23:44:48 +0900

    Update doc comments for `Simplify`

--- source/simplify.d.ts
+++ source/simplify.d.ts
@@ -19,6 +19,10 @@ type SizeProps = {
 type Props = Simplify<PositionProps & SizeProps>;
 ```
 
+Sometimes it is desired to pass a value as a function argument that has a different type. At first inspection it may seem assignable, and then you discover it is not because the `value`'s type definition was defined as an interface. In the following example, `fn` requires an argument of type `Record<string, unknown>`. If the value is defined as a literal, then it is assignable. And if the `value` is defined as type using the `Simplify` utility the value is assignable.  But if the `value` is defined as an interface, it is not assignable because the interface is not sealed and elsewhere a non-string property could be added to the interface.
+
+If the type definition must be an interface (perhaps it was defined in a 3rd party npm package), then the `value` can be defined as `const value: Simplify<SomeInterface> = ...`.  Then `value` will be assignable to the `fn` argument.  Or the `value` can be cast as `Simplify<SomeInterface>` if you can't re-declare the `value`.
+
 @example
 ```
 import {Simplify} from 'type-fest';
