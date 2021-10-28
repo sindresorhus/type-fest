113cb1f8a4ba747d5d51e8489030757735be30a7
grumpytofu <>
2021-10-28 00:22:44 -0400

    fixed formatting

--- source/title-case.d.ts
+++ source/title-case.d.ts
@@ -1,52 +1,29 @@
-import { StringDigit, UpperCaseCharacters } from "./utilities";
-import { SplitIncludingDelimiters } from "./delimiter-case";
-import { CamelCase } from "./camel-case";
-import { Join } from "./join";
+import {StringDigit, UpperCaseCharacters} from './utilities';
+import {SplitIncludingDelimiters} from './delimiter-case';
+import {CamelCase} from './camel-case';
+import {Join} from './join';
 
-export type Conjunctions = "and" | "as" | "but" | "for" | "if" | "nor" | "or" | "so" | "yet";
-export type Articles = "a" | "an" | "the";
-export type ShortPrepositions = "as" | "at" | "by" | "for" | "in" | "of" | "off" | "on" | "per" | "to" | "up" | "via" | "is" | "ok";
+export type Conjunctions = 'and' | 'as' | 'but' | 'for' | 'if' | 'nor' | 'or' | 'so' | 'yet';
+export type Articles = 'a' | 'an' | 'the';
+export type ShortPrepositions = 'as' | 'at' | 'by' | 'for' | 'in' | 'of' | 'off' | 'on' | 'per' | 'to' | 'up' | 'via' | 'is' | 'ok';
 
 export type ApaIgnoreUpperCase = Conjunctions | Articles | ShortPrepositions;
 
-type ApaTitleCase<K, I extends ApaIgnoreUpperCase> = K extends string
-	? Lowercase<K> extends I
-		? Lowercase<K>
-		: Capitalize<K>
-	: K;
+type ApaTitleCase<K, I extends ApaIgnoreUpperCase> = K extends string ? Lowercase<K> extends I ? Lowercase<K> : Capitalize<K> : K;
 
-type TitleCaseJoin<Parts extends readonly any[], I extends ApaIgnoreUpperCase> = Parts extends [
-	`${infer FirstPart}`,
-	`${infer SecondPart}`,
-	...infer Rest
-]
+type TitleCaseJoin<Parts extends readonly any[], I extends ApaIgnoreUpperCase> = Parts extends [`${infer FirstPart}`, `${infer SecondPart}`, ...infer Rest]
 	? FirstPart extends StringDigit
-		? `${ApaTitleCase<FirstPart, I>}${TitleCase<
-				Join<
-					[SecondPart, Rest extends string[] | number[] ? Join<Rest, ""> : ""],
-					""
-				>
-		  >}`
-		: `${ApaTitleCase<FirstPart, I>} ${TitleCase<
-				Join<
-					[SecondPart, Rest extends string[] | number[] ? Join<Rest, ""> : ""],
-					""
-				>
-		  >}`
+	? `${ApaTitleCase<FirstPart, I>}${TitleCase<Join<[SecondPart, Rest extends string[] | number[] ? Join<Rest, ''> : ''], ''>>}`
+	: `${ApaTitleCase<FirstPart, I>} ${TitleCase<Join<[SecondPart, Rest extends string[] | number[] ? Join<Rest, ''> : ''], ''>>}`
 	: Parts extends [`${infer FirstPart}`, ...infer Rest]
-	? `${ApaTitleCase<FirstPart, I>}${Rest extends string
-			? Rest
-			: TitleCaseJoin<Rest, I>}`
-	: Parts extends [string | number]
-	? Parts extends [string]
-		? Capitalize<Parts[0]>
-		: `${Parts[0]}`
-	: "";
+	? `${ApaTitleCase<FirstPart, I>}${Rest extends string ? Rest : TitleCaseJoin<Rest, I>}`
+	: Parts extends [string | number] ? Parts extends [string] ? Capitalize<Parts[0]> : `${Parts[0]}`
+	: '';
 
 /**
 Convert a string literal to Title Case.
 
-__This is similar to "Titleize", however does follow APA guidelines for title casing.__ 
+__This is similar to 'Titleize', however does follow APA guidelines for title casing.__
 
 - _typeOfAnimal -> Type Of Animal (Titleize)_
 - _typeOfAnimal -> Type of Animal (Title Case)_
@@ -78,17 +55,12 @@ interface CustomGridColDef extends GridColDef {
 }
 
 const columns: CustomGridColDef[] = [
-  { field: "id", headerName: "Id" },
-  { field: "name", headerName: "Name" },
-  { field: "thisIsASpecialProperty", headerName: "This is a Special Property" }
+  { field: 'id', headerName: 'Id' },
+  { field: 'name', headerName: 'Name' },
+  { field: 'thisIsASpecialProperty', headerName: 'This is a Special Property' }
 ]
 ```
 
 @category Template Literals
 */
-export type TitleCase<K, I extends ApaIgnoreUpperCase = ApaIgnoreUpperCase> = K extends string
-	? TitleCaseJoin<
-			SplitIncludingDelimiters<CamelCase<K>, UpperCaseCharacters | StringDigit>,
-			I
-	  >
-	: K;
+export type TitleCase<K, I extends ApaIgnoreUpperCase = ApaIgnoreUpperCase> = K extends string ? TitleCaseJoin<SplitIncludingDelimiters<CamelCase<K>, UpperCaseCharacters | StringDigit>, I> : K;
--- source/titleize.d.ts
+++ source/titleize.d.ts
@@ -1,40 +1,19 @@
-import { StringDigit, UpperCaseCharacters } from "./utilities";
-import { SplitIncludingDelimiters } from "./delimiter-case";
-import { CamelCase } from "./camel-case";
-import { Join } from "./join";
+import {StringDigit, UpperCaseCharacters} from './utilities';
+import {SplitIncludingDelimiters} from './delimiter-case';
+import {CamelCase} from './camel-case';
+import {Join} from './join';
 
-type TitleizeJoin<Parts extends readonly any[]> = Parts extends [
-	`${infer FirstPart}`,
-	`${infer SecondPart}`,
-	...infer Rest
-]
+type TitleizeJoin<Parts extends readonly any[]> = Parts extends [`${infer FirstPart}`, `${infer SecondPart}`, ...infer Rest]
 	? FirstPart extends StringDigit
-		? `${Capitalize<FirstPart>}${Titleize<
-				Join<
-					[SecondPart, Rest extends string[] | number[] ? Join<Rest, ""> : ""],
-					""
-				>
-		  >}`
-		: `${Capitalize<FirstPart>} ${Titleize<
-				Join<
-					[SecondPart, Rest extends string[] | number[] ? Join<Rest, ""> : ""],
-					""
-				>
-		  >}`
-	: Parts extends [`${infer FirstPart}`, ...infer Rest]
-	? `${Capitalize<FirstPart>}${Rest extends string
-			? Rest
-			: TitleizeJoin<Rest>}`
-	: Parts extends [string | number]
-	? Parts extends [string]
-		? Capitalize<Parts[0]>
-		: `${Parts[0]}`
-	: "";
+		? `${Capitalize<FirstPart>}${Titleize<Join<[SecondPart, Rest extends string[] | number[] ? Join<Rest, ''> : ''], ''>>}`
+		: `${Capitalize<FirstPart>} ${Titleize<Join<[SecondPart, Rest extends string[] | number[] ? Join<Rest, ''> : ''], ''>>}`
+	: Parts extends [`${infer FirstPart}`, ...infer Rest] ? `${Capitalize<FirstPart>}${Rest extends string ? Rest : TitleizeJoin<Rest>}`
+	: Parts extends [string | number] ? Parts extends [string] ? Capitalize<Parts[0]> : `${Parts[0]}` : '';
 
 /**
 Convert a string literal to Titleized Format.
 
-__This is similar to "TitleCase", however does _not_ follow APA guidelines for title casing:__
+__This is similar to 'TitleCase', however does _not_ follow APA guidelines for title casing:__
 
 - _typeOfAnimal -> Type Of Animal (Titleize)_
 - _typeOfAnimal -> Type of Animal (Title Case)_
@@ -44,7 +23,7 @@ This can be useful when converting a camel-cased object property into Titleized 
 It is ideal to use Titleize when you want automated formatting of data for display with a utility function like below:
 
 ```ts
-const titleize = (str: string) => str.replace(/([A-Z])/g, " $1").replace(/^./, (str) => str.toUpperCase());
+const titleize = (str: string) => str.replace(/([A-Z])/g, ' $1').replace(/^./, (str) => str.toUpperCase());
 ```
 
 @example
@@ -71,15 +50,15 @@ interface CustomGridColDef extends GridColDef {
 }
 
 const columns: CustomGridColDef[] = [
-	{ field: "id", headerName: "Id" },
-	{ field: "name", headerName: "Name" },
-	{ field: "prettyPrintMePlease", headerName: "Pretty Print Me Please" }
+	{ field: 'id', headerName: 'Id' },
+	{ field: 'name', headerName: 'Name' },
+	{ field: 'prettyPrintMePlease', headerName: 'Pretty Print Me Please' }
 ]
 
 // Or a more automated approach
 const apiObject: CustomObject = {
 	id: 1,
-	fooBar: "barBaz",
+	fooBar: 'barBaz',
 	prettyPrintMePlease: null
 }
 
