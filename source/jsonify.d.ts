62abbdeaa150162199342e63218449bda9b807bc
William Baker <bbrk24@gmail.com>
2021-11-05 18:48:13 -0400

    Fix treatment of partials in Jsonify

--- source/jsonify.d.ts
+++ source/jsonify.d.ts
@@ -70,6 +70,6 @@ type Jsonify<T> =
 						? (() => J) extends (() => JsonValue) // Is J assignable to JsonValue?
 							? J // Then T is Jsonable and its Jsonable value is J
 							: never // Not Jsonable because its toJSON() method does not return JsonValue
-						: {[P in keyof T]: Jsonify<T[P]>} // It's an object: recursive call for its children
+						: {[P in keyof T]: Jsonify<Required<T>[P]>} // It's an object: recursive call for its children
 					: never // Otherwise any other non-object is removed
 		: never; // Otherwise non-JSONable type union was found not empty
