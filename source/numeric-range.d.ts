0c7d6c80b810e29b5d6e90bc5dcffae5dbede690
Jonah Snider <jonah@jonahsnider.com>
2021-11-14 20:00:22 -0800

    Add explanation about how `Integer` works

--- source/numeric-range.d.ts
+++ source/numeric-range.d.ts
@@ -10,6 +10,8 @@ declare function setYear<T extends number>(length: Integer<T>): void;
 
 @category Utilities
 */
+// `${bigint}` is a type that matches a valid bigint literal without the `n` (ex. 1, 0b1, 0o1, 0x1)
+// Because T is a number and not a string we can effectively use this to filter out any numbers containing decimal points
 export type Integer<T extends number> = `${T}` extends `${bigint}` ? T : never;
 
 type Numeric = number | bigint;
