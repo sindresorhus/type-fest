6a87568e7352a38f2c75a38620d482f628fab93f
Jonah Snider <jonah@jonahsnider.com>
2021-11-14 20:16:12 -0800

    Return `never` from `Integer` when `T` is `Infinity` or `-Infinity`

--- test-d/numeric-range.ts
+++ test-d/numeric-range.ts
@@ -5,10 +5,14 @@ import {Integer, Natural, Negative, Positive} from '../index';
 declare const integer: Integer<1>;
 declare const integerMixed: Integer<1 | 1.5>;
 declare const nonInteger: Integer<1.5>;
+// See https://github.com/microsoft/TypeScript/issues/31752
+// eslint-disable-next-line @typescript-eslint/no-loss-of-precision
+declare const infinity: Integer<1e999 | -1e999>;
 
 expectType<1>(integer);
-expectType<never>(integerMixed);
+expectType<1>(integerMixed);
 expectType<never>(nonInteger);
+expectType<never>(infinity);
 
 // Negative
 declare const negative: Negative<-1 | -1n | 0 | 0n | 1 | 1n>;
