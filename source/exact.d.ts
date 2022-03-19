3568ad763bf02b4a79322b62fd2c30f79e8c6f11
Sindre Sorhus <sindresorhus@gmail.com>
2022-03-19 15:25:59 +0700

    Update exact.d.ts

--- source/exact.d.ts
+++ source/exact.d.ts
@@ -5,22 +5,22 @@ import {KeysOfUnion} from './internal';
 Create a type from type A and B and changes keys exclusive to type B to `never`.
 
 This is useful for function type-guarding to reject arguments with excess
-properties.
-
-Due to the nature of TypeScript, it does not complain if excess properties are
+properties. Due to the nature of TypeScript, it does not complain if excess properties are
 provided unless the provided value is an object literal.
 
 @example
 ```
 type OnlyAcceptName = {name: string};
+
 function onlyAcceptName(args: OnlyAcceptName) {}
 
-// TypeScript complains this because it's an object literal
-onlyAcceptName({ name: "name", id: 1 }); // compilation error, `id` is excess
+// TypeScript complains this because it's an object literal.
+onlyAcceptName({name: 'name', id: 1});
+//=> `id` is excess
 
-// TypeScript does not complain because it's not an object literal
-const invalidInput = { name: "name", id: 1 };
-onlyAcceptName(invalidInput); // no errors
+// TypeScript does not complain because it's not an object literal.
+const invalidInput = {name: 'name', id: 1};
+onlyAcceptName(invalidInput); // No errors
 ```
 
 Having `Exact` allows TypeScript to reject excess properties.
@@ -29,11 +29,13 @@ Having `Exact` allows TypeScript to reject excess properties.
 ```
 import {Exact} from 'type-fest';
 
-type OnlyAcceptName = { name: string };
+type OnlyAcceptName = {name: string};
+
 function onlyAcceptNameImproved<T extends Exact<OnlyAcceptName, T>>(args: T) {}
 
-const invalidInput = { name: "name", id: 1 };
-onlyAcceptNameImproved(invalidInput); // compilation error
+const invalidInput = {name: 'name', id: 1};
+
+onlyAcceptNameImproved(invalidInput); // Compilation error
 ```
 
 The solution of `Exact` is
