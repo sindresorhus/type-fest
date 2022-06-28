cf45daff713cc659b170661a21ac396f54246ac9
Fatih Altinok <photonia@gmail.com>
2022-06-28 21:11:49 +0300

    Fix lint issues

--- test-d/jsonify.ts
+++ test-d/jsonify.ts
@@ -191,15 +191,15 @@ declare const bigInt: Jsonify<BigInt>;
 expectType<never>(bigInt);
 
 declare const int8Array: Int8Array;
-declare const int8ArrayJson: Jsonify<typeof int8Array>
-expectType<{[key: string]: number}>(int8ArrayJson);
+declare const int8ArrayJson: Jsonify<typeof int8Array>;
+expectType<Record<string, number>>(int8ArrayJson);
 
-declare const map: Map<string, number>
-declare const mapJson: Jsonify<typeof map>
+declare const map: Map<string, number>;
+declare const mapJson: Jsonify<typeof map>;
 expectType<{}>(mapJson);
 
-declare const set: Set<string>
-declare const setJson: Jsonify<typeof set>
+declare const set: Set<string>;
+declare const setJson: Jsonify<typeof set>;
 expectType<{}>(setJson);
 
 // Positive and negative Infinity, NaN and null are turned into null
