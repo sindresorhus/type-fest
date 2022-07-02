96fe79598ec20fb9adad26cad95a9daa46718b0d
skarab42 <skarab@bluewin.ch>
2022-07-02 13:49:45 +0200

    Make `Simplify` handle all types (#414)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- index.d.ts
+++ index.d.ts
@@ -38,7 +38,7 @@ export {Entry} from './source/entry';
 export {Entries} from './source/entries';
 export {SetReturnType} from './source/set-return-type';
 export {Asyncify} from './source/asyncify';
-export {Simplify} from './source/simplify';
+export {Simplify, SimplifyOptions} from './source/simplify';
 export {Jsonify} from './source/jsonify';
 export {Schema} from './source/schema';
 export {LiteralToPrimitive} from './source/literal-to-primitive';
