96126c00086371ef4366c589dde52d497033fe70
Sindre Sorhus <sindresorhus@gmail.com>
2022-07-02 18:00:07 +0200

    Update has-optional-keys.d.ts

--- source/has-optional-keys.d.ts
+++ source/has-optional-keys.d.ts
@@ -1,7 +1,7 @@
 import {OptionalKeysOf} from './optional-keys-of';
 
 /**
-Creates a type that represents `true` or `false` depending on the presence or absence of optional fields.
+Creates a type that represents `true` or `false` depending on whether the given type has any optional fields.
 
 This is useful when you want to create an API whose behaviour depends on the presence or absence of optional fields.
 
@@ -17,5 +17,5 @@ type UpdateService<Entity extends object> = {
 ```
 
 @category Utilities
- */
+*/
 export type HasOptionalKeys<BaseType extends object> = OptionalKeysOf<BaseType> extends never ? false : true;
