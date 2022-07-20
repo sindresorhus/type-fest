0faf461ed3d0d05b28300047c073367a2f9992a4
clement <clement.cloux@ynov.com>
2022-07-20 14:38:19 +0200

    Convert indentation to tabs on PartialOnUndefinedDeep type

--- source/partial-on-undefined-deep.d.ts
+++ source/partial-on-undefined-deep.d.ts
@@ -12,36 +12,36 @@ https://github.com/sindresorhus/type-fest/issues/399
 ```
 import type {PartialOnUndefinedDeep} from 'type-fest';
 interface Settings {
-  optionA: string;
-  optionB: number | undefined;
-  subOption: {
-    subOptionA: boolean;
-    subOptionB: boolean | undefined;
-  }
+	optionA: string;
+	optionB: number | undefined;
+	subOption: {
+		subOptionA: boolean;
+		subOptionB: boolean | undefined;
+	}
 };
 const testSettings: PartialOnUndefinedDeep<Settings> = {
-  optionA: "foo",
-  // 👉 optionB is now optional and can be omited
-  subOption: {
-    subOptionA: true,
-    // 👉 subOptionB is now optional aswell and can be omited
-  },
+	optionA: "foo",
+	// 👉 optionB is now optional and can be omited
+	subOption: {
+		subOptionA: true,
+		// 👉 subOptionB is now optional aswell and can be omited
+	},
 };
 ```
 
 @category Object
 */
 export type PartialOnUndefinedDeep<T> = T extends Record<any, any> | undefined
-  ? {[KeyType in keyof T as undefined extends T[KeyType] ? KeyType : never]?: PartialOnUndefinedDeepValue<T[KeyType]>} extends infer U // Make a partial type with all value types accepting undefined (and set them optional)
-    ? Merge<{[KeyType in keyof T as KeyType extends keyof U ? never : KeyType]: PartialOnUndefinedDeepValue<T[KeyType]>}, U> // Join all remaining keys not treated in U
-    : never // Should not happen
-  : T;
+	? {[KeyType in keyof T as undefined extends T[KeyType] ? KeyType : never]?: PartialOnUndefinedDeepValue<T[KeyType]>} extends infer U // Make a partial type with all value types accepting undefined (and set them optional)
+		? Merge<{[KeyType in keyof T as KeyType extends keyof U ? never : KeyType]: PartialOnUndefinedDeepValue<T[KeyType]>}, U> // Join all remaining keys not treated in U
+		: never // Should not happen
+	: T;
 
 /**
  * Utility type to get the value type by key, recursively call PartialOnUndefinedDeep to treat subObjects
  */
 type PartialOnUndefinedDeepValue<T> = T extends BuiltIns | ((...arguments: any[]) => unknown)
-  ? T
-  : T extends Record<any, any> | undefined
-  ? PartialOnUndefinedDeep<T>
-  : unknown;
+	? T
+	: T extends Record<any, any> | undefined
+	? PartialOnUndefinedDeep<T>
+	: unknown;
