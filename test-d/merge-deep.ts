9350e6df451b01ad770b98fa211c737c9f1a31f3
skarab42 <skarab@bluewin.ch>
2022-08-15 07:56:57 +0200

    Rename abbreviated variables

--- test-d/merge-deep.ts
+++ test-d/merge-deep.ts
@@ -3,101 +3,101 @@ import {MergeDeep} from '../index';
 
 // Test MergeDeep inputs
 
-declare const sign1: MergeDeep<{}, {}>; // {}
-declare const sign2: MergeDeep<[], []>; // Never
+declare const signature1: MergeDeep<{}, {}>; // {}
+declare const signature2: MergeDeep<[], []>; // Never
 
-expectNotType<never>(sign1);
-expectType<never>(sign2);
+expectNotType<never>(signature1);
+expectType<never>(signature2);
 
-declare const sign3: MergeDeep<{}, []>; // Never
-declare const sign4: MergeDeep<[], {}>; // Never
+declare const signature3: MergeDeep<{}, []>; // Never
+declare const signature4: MergeDeep<[], {}>; // Never
 
-expectType<never>(sign3);
-expectType<never>(sign4);
+expectType<never>(signature3);
+expectType<never>(signature4);
 
-declare const sign5: MergeDeep<42, {}>; // Never
-declare const sign6: MergeDeep<42, []>; // Never
+declare const signature5: MergeDeep<42, {}>; // Never
+declare const signature6: MergeDeep<42, []>; // Never
 
-expectType<never>(sign5);
-expectType<never>(sign6);
+expectType<never>(signature5);
+expectType<never>(signature6);
 
-declare const sign7: MergeDeep<{}, 42>; // Never
-declare const sign8: MergeDeep<[], 42>; // Never
+declare const signature7: MergeDeep<{}, 42>; // Never
+declare const signature8: MergeDeep<[], 42>; // Never
 
-expectType<never>(sign7);
-expectType<never>(sign8);
+expectType<never>(signature7);
+expectType<never>(signature8);
 
-declare const sign9: MergeDeep<{}, {}, {recurseIntoArrays: true}>; // {}
-declare const sign10: MergeDeep<[], [], {recurseIntoArrays: true}>; // []
+declare const signature9: MergeDeep<{}, {}, {recurseIntoArrays: true}>; // {}
+declare const signature10: MergeDeep<[], [], {recurseIntoArrays: true}>; // []
 
-expectNotType<never>(sign9);
-expectNotType<never>(sign10);
+expectNotType<never>(signature9);
+expectNotType<never>(signature10);
 
-declare const sign11: MergeDeep<{}, [], {recurseIntoArrays: true}>; // Never
-declare const sign12: MergeDeep<[], {}, {recurseIntoArrays: true}>; // Never
+declare const signature11: MergeDeep<{}, [], {recurseIntoArrays: true}>; // Never
+declare const signature12: MergeDeep<[], {}, {recurseIntoArrays: true}>; // Never
 
-expectType<never>(sign11);
-expectType<never>(sign12);
+expectType<never>(signature11);
+expectType<never>(signature12);
 
-declare const sign13: MergeDeep<42, {}, {recurseIntoArrays: true}>; // Never
-declare const sign14: MergeDeep<42, [], {recurseIntoArrays: true}>; // Never
+declare const signature13: MergeDeep<42, {}, {recurseIntoArrays: true}>; // Never
+declare const signature14: MergeDeep<42, [], {recurseIntoArrays: true}>; // Never
 
-expectType<never>(sign13);
-expectType<never>(sign14);
+expectType<never>(signature13);
+expectType<never>(signature14);
 
-declare const sign15: MergeDeep<{}, 42, {recurseIntoArrays: true}>; // Never
-declare const sign16: MergeDeep<[], 42, {recurseIntoArrays: true}>; // Never
+declare const signature15: MergeDeep<{}, 42, {recurseIntoArrays: true}>; // Never
+declare const signature16: MergeDeep<[], 42, {recurseIntoArrays: true}>; // Never
 
-expectType<never>(sign15);
-expectType<never>(sign16);
+expectType<never>(signature15);
+expectType<never>(signature16);
 
 // Test flat plain object
 
-const src1 = {life: 42, name: 'nyan', a: undefined};
-const dest1 = {life: '24', fly: true, b: undefined};
-const merge1 = {...dest1, ...src1};
+const source1 = {life: 42, name: 'nyan', a: undefined};
+const destination1 = {life: '24', fly: true, b: undefined};
+const merge1 = {...destination1, ...source1};
 
-expectType<MergeDeep<typeof dest1, typeof src1>>(merge1);
+expectType<MergeDeep<typeof destination1, typeof source1>>(merge1);
 
 // Test deep plain object
 
-const src2 = {a: 'a', b: src1, c: {d: src1}};
-const dest2 = {a: 'z', b: dest1, c: {d: dest1}};
+const source2 = {a: 'a', b: source1, c: {d: source1}};
+const destination2 = {a: 'z', b: destination1, c: {d: destination1}};
 const merge2 = {a: 'a', b: merge1, c: {d: merge1}};
 
-expectType<MergeDeep<typeof dest2, typeof src2>>(merge2);
+expectType<MergeDeep<typeof destination2, typeof source2>>(merge2);
 
 // Undefied properties must be preserved
 
-const src3 = {a: 'a', c: undefined, e: 42, f: undefined};
-const dest3 = {b: 'b', d: undefined, e: undefined, f: 42};
+const source3 = {a: 'a', c: undefined, e: 42, f: undefined};
+const destination3 = {b: 'b', d: undefined, e: undefined, f: 42};
 
 const merge3 = {a: 'a', b: 'b', c: undefined, d: undefined, e: 42, f: undefined};
-declare const type3: MergeDeep<typeof dest3, typeof src3>;
+declare const type3: MergeDeep<typeof destination3, typeof source3>;
 
 expectType<typeof merge3>(type3);
 
 // Undefied properties must be skipped
 
 const merge4 = {a: 'a', b: 'b', e: 42};
-declare const type4: MergeDeep<typeof dest3, typeof src3, {stripUndefinedValues: true}>;
+declare const type4: MergeDeep<typeof destination3, typeof source3, {stripUndefinedValues: true}>;
 
 expectType<typeof merge4>(type4);
 
 // Should replace arrays
 
-const src5 = {a: 'a', items: ['4', '5', '6']};
-const dest5 = {b: 'b', items: [1, 2, 3]};
+const source5 = {a: 'a', items: ['4', '5', '6']};
+const destination5 = {b: 'b', items: [1, 2, 3]};
 
 const merge5 = {a: 'a', b: 'b', items: ['4', '5', '6']};
-declare const prout5: MergeDeep<typeof dest5, typeof src5>;
+declare const prout5: MergeDeep<typeof destination5, typeof source5>;
 
 expectType<typeof merge5>(prout5);
 
 // Should concat arrays
 
 const merge6 = {a: 'a', b: 'b', items: [1, 2, 3, '4', '5', '6']};
-declare const prout6: MergeDeep<typeof dest5, typeof src5, {recurseIntoArrays: true}>;
+declare const prout6: MergeDeep<typeof destination5, typeof source5, {recurseIntoArrays: true}>;
 
 expectType<typeof merge6>(prout6);
 
