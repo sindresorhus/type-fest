2f22f14a6532aac6d6db651f97a93286f52b5d17
Sindre Sorhus <sindresorhus@gmail.com>
2022-08-31 22:28:13 +0700

    Update pick-index-signature.d.ts

--- source/pick-index-signature.d.ts
+++ source/pick-index-signature.d.ts
@@ -3,9 +3,7 @@ Pick only index signatures from the given object type, leaving out all explicitl
 
 This is the counterpart of `OmitIndexSignature`.
 
-When you use a type that will iterate through an object that has indexed keys and explicitly defined keys you end up with a type where only the indexed keys are kept.
-This is because `keyof` of an indexed type always returns `string | number | symbol`, because every key is possible in that object.
-With `PickIndexSignature` you can save the indexed keys and reinject them later, see the second example below.
+When you use a type that will iterate through an object that has indexed keys and explicitly defined keys you end up with a type where only the indexed keys are kept. This is because `keyof` of an indexed type always returns `string | number | symbol`, because every key is possible in that object. With this type, you can save the indexed keys and reinject them later, like in the second example below.
 
 @example
 ```
