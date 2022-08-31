58c3b16064081e65bf7f9633536bb88bdfb8cf68
skarab42 <skarab@bluewin.ch>
2022-08-31 11:16:20 +0200

    Update test

---
+++ test-d/omit-index-signature.ts
@@ -0,0 +1,39 @@
+import {expectType} from 'tsd';
+import type {OmitIndexSignature} from '../index';
+
+type ExampleInterface = {
+	// These index signatures will be removed.
+	[x: string]: any;
+	[x: number]: any;
+	[x: symbol]: any;
+	[x: `head-${string}`]: string;
+	[x: `${string}-tail`]: string;
+	[x: `head-${string}-tail`]: string;
+	[x: `${bigint}`]: string;
+	[x: `embedded-${number}`]: string;
+
+	// These explicitly defined keys will remain.
+	foo: 'bar';
+	qux?: 'baz';
+};
+
+type MappedType<ObjectType> = {
+	[Key in keyof ObjectType]: {
+		key: Key;
+		value: Exclude<ObjectType[Key], undefined>;
+	};
+};
+
+declare const exampleInterfaceKnownKeys: OmitIndexSignature<ExampleInterface>;
+expectType<{
+	foo: 'bar';
+	qux?: 'baz';
+}>(exampleInterfaceKnownKeys);
+
+declare const exampleMappedTypeKnownKeys: OmitIndexSignature<
+MappedType<ExampleInterface>
+>;
+expectType<{
+	foo: {key: 'foo'; value: 'bar'};
+	qux?: {key: 'qux'; value: 'baz'};
+}>(exampleMappedTypeKnownKeys);
