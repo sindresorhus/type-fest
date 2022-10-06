ca3682b16e677fd1059f60e814f2a09d96b60667
Joe Alden <me@joealden.com>
2022-10-06 15:43:19 +0100

    Update documentation for `SetNonNullable`

--- source/set-non-nullable.d.ts
+++ source/set-non-nullable.d.ts
@@ -2,23 +2,30 @@ import type {Except} from './except';
 import type {Simplify} from './simplify';
 
 /**
-Create a type that makes the given keys non-nullable. The remaining keys are kept as is.
+Create a type that makes the given keys non-nullable, where the remaining keys are kept as is. If no keys are given, all keys will be made non-nullable.
 
-Use-case: You want to define a single model where the only thing that changes is whether or not some of the keys are non-nullable.
+Use-case: You want to define a single model where the only thing that changes is whether or not some or all of the keys are non-nullable.
 
 @example
 ```
 import type {SetNonNullable} from 'type-fest';
 
 type Foo = {
-	a: number;
+	a: number | null;
 	b: string | undefined;
-	c?: boolean | null;
+	d?: boolean | null;
 }
 
 type SomeNonNullable = SetNonNullable<Foo, 'b' | 'c'>;
 // type SomeNonNullable = {
-// 	a: number;
+// 	a: number | null;
+// 	b: string; // Can no longer be undefined.
+// 	c?: boolean; // Can no longer be null, but is still optional.
+// }
+
+type AllNonNullable = SetNonNullable<Foo>;
+// type AllNonNullable = {
+// 	a: number; // Can no longer be null.
 // 	b: string; // Can no longer be undefined.
 // 	c?: boolean; // Can no longer be null, but is still optional.
 // }
