3f399adfba71d1653612c1b1508b1a755cfbfe8a
itsjohncs <johnsullivan.pem@gmail.com>
2022-10-16 00:48:19 -0700

    Add `Jsonifiable` type.
    Closes #356.

---
+++ source/jsonifiable.d.ts
@@ -0,0 +1,31 @@
+import type {JsonPrimitive} from './basic';
+
+type JsonifiablePrimitive = JsonPrimitive | {toJSON: () => Jsonifiable};
+type JsonifiableObject = {[Key in string]?: Jsonifiable};
+type JsonifiableArray = Jsonifiable[];
+
+/**
+Matches a value that can be losslessly converted to JSON.
+
+@example
+```
+import type {Jsonifiable} from 'type-fest';
+
+// Neither a map nor undefined can be serialized by JSON.stringify so
+// it will be lost.
+const error: Jsonifiable = {
+    map: new Map(),
+    missing: undefined,
+};
+
+// Numbers can be directly serialized and Date objects have a toJSON()
+// function.
+const good: Jsonifiable = {
+    number: 3,
+    date: new Date(),
+}
+```
+
+@category JSON
+*/
+export type Jsonifiable = JsonifiablePrimitive | JsonifiableObject | JsonifiableArray;
---
+++ test-d/jsonifiable.ts
@@ -0,0 +1,14 @@
+import {expectAssignable, expectNotAssignable} from 'tsd';
+import type {Jsonifiable} from '..';
+
+expectAssignable<Jsonifiable>(1);
+expectAssignable<Jsonifiable>('');
+expectAssignable<Jsonifiable>(null);
+expectAssignable<Jsonifiable>(new Date());
+expectAssignable<Jsonifiable>({a: new Date()});
+expectAssignable<Jsonifiable>([new Date()]);
+
+expectNotAssignable<Jsonifiable>(undefined);
+expectNotAssignable<Jsonifiable>(new Map());
+expectNotAssignable<Jsonifiable>({a: new Map()});
+expectNotAssignable<Jsonifiable>([new Map()]);
