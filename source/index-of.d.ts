ab84683f5d8f99aed14c5f96c6de6a001464d71a
Jacob Bergholtz <cuppachino.dev@gmail.com>
2022-11-25 08:18:14 -0600

    Change `IndexOf` to `Indices`

--- source/index-of.d.ts
+++ source/index-of.d.ts
@@ -2,18 +2,18 @@
  * Generates a tuple based on the length of the input array
  * and fills each index with the index number.
  *
- * @example type FruitIndices = IndexOf<['apple', 'banana', 'plum']> // [0, 1, 2]
- * @example type FruitIndex = IndexOf<['apple', 'banana', 'plum']>[number] // 0 | 1 | 2
+ * @example type FruitIndices = Indices<['apple', 'banana', 'plum']> // [0, 1, 2]
+ * @example type FruitIndex = Indices<['apple', 'banana', 'plum']>[number] // 0 | 1 | 2
  *
  * @category Array
  * @category Utility
  */
-export type IndexOf<
+export type Indices<
 	Tuple extends any[] | readonly any[],
 	Accumulator extends any[] = [],
 > = LengthOf<Accumulator> extends LengthOf<Tuple>
 	? Accumulator
-	: IndexOf<Tuple, [...Accumulator, LengthOf<Accumulator>]>;
+	: Indices<Tuple, [...Accumulator, LengthOf<Accumulator>]>;
 
 /**
  * Returns the length property of the input array; tuples will return the number of items in the tuple.
