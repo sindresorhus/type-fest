2071f47efbe79cc932ab45137e931a2998901a4e
Sachin Raja <me@s4n.land>
2022-11-28 01:00:03 -0800

    `Jsonify`: Add jump to definition and `any` support (#519)
    Co-authored-by: Michaël De Boey <info@michaeldeboey.be>
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- source/set-return-type.d.ts
+++ source/set-return-type.d.ts
@@ -1,4 +1,5 @@
-type IsAny<T> = 0 extends (1 & T) ? true : false; // https://stackoverflow.com/a/49928360/3406963
+import type {IsAny} from './internal';
+
 type IsNever<T> = [T] extends [never] ? true : false;
 type IsUnknown<T> = IsNever<T> extends false ? T extends unknown ? unknown extends T ? IsAny<T> extends false ? true : false : false : false : false;
 
