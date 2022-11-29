cb885052512718f46e4120a49820f18ae6ed9831
cuppachino <cuppachino.dev@gmail.com>
2022-11-28 20:13:40 -0600

    simplify Indices type and format

--- source/indices.d.ts
+++ source/indices.d.ts
@@ -1,24 +1,21 @@
 /**
- * Generates a tuple based on the length of the input array
- * and fills each index with the index number.
- *
- * @example type FruitIndices = Indices<['apple', 'banana', 'plum']> // [0, 1, 2]
- * @example type FruitIndex = Indices<['apple', 'banana', 'plum']>[number] // 0 | 1 | 2
- *
- * @category Array
- * @category Utility
- */
-export type Indices<
-	Tuple extends any[] | readonly any[],
-	Accumulator extends any[] = [],
-> = LengthOf<Accumulator> extends LengthOf<Tuple>
-	? Accumulator
-	: Indices<Tuple, [...Accumulator, LengthOf<Accumulator>]>;
+Generates a tuple based on the length of the input array and fills each index with the index number.
 
-/**
- * Returns the length property of the input array; tuples will return the number of items in the tuple.
- *
- * @example type TotalFruits = LengthOf<['apple', 'banana', 'plum']> // 3
- * @example type TotalStrings = LengthOf<string[]> // number
- */
-type LengthOf<Tuple extends any[] | readonly any[]> = Tuple['length'];
+@example
+```
+type FruitIndices = Indices<['apple', 'banana', 'plum']> // [0, 1, 2]
+```
+
+Can be used to create an index union (`0 | 1 | 2 | ...`)
+
+@example
+```
+type FruitIndex = Indices<['apple', 'banana', 'plum']>[number] // 0 | 1 | 2
+```
+
+@category Array
+@category Utility
+*/
+export type Indices<T extends unknown[] | readonly unknown[]> = {
+	[K in keyof T]: K extends `${infer N extends number}` ? N : never;
+};
