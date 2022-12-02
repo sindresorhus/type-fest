45c1443740914910e08d60e77421da26e358b9ab
Takahiro Kato <dozo13189@gmail.com>
2022-12-02 20:32:58 +0900

    added sample code

--- source/is-equal.d.ts
+++ source/is-equal.d.ts
@@ -6,16 +6,32 @@ Returns a boolean for whether the two given types are equal.
 
 @example
 ```
+type ExpectTrue = IsEqual<1, 1>
+//=> true
+```
+
+@example
+```
+type ExpectFalse = IsEqual<2, 1>
+//=> false
+```
+
+@example
+```
 import type {IsEqual} from 'type-fest';
 
-type Expect<T extends true> = T
-type Case = Expect<IsEqual<Uppercase<"sample">, "SAMPLE">>
+type Includes<Value extends readonly any[], Item> =
+	Value extends readonly [Value[0], ...infer rest]
+		? IsEqual<Value[0], Item> extends true
+			? true
+			: Includes<rest, Item>
+		: false;
 ```
 
 @category Utilities
 */
-export type IsEqual<X, Y> =
-	(<G>() => G extends X ? 1 : 2) extends
-	(<G>() => G extends Y ? 1 : 2)
+export type IsEqual<A, B> =
+	(<G>() => G extends A ? 1 : 2) extends
+	(<G>() => G extends B ? 1 : 2)
 		? true
 		: false;
