081aad2d5f009760b8ee2981cdbe4739263076d3
Takahiro Kato <dozo13189@gmail.com>
2022-12-14 00:30:50 +0900

    Deleted unnecessary examples

--- source/is-equal.d.ts
+++ source/is-equal.d.ts
@@ -8,24 +8,6 @@ Returns a boolean for whether the two given types are equal.
 ```
 import type {IsEqual} from 'type-fest';
 
-// `IsEqual` compares 1 and 1, then gets true as a result.
-type ExpectTrue = IsEqual<1, 1>;
-//=> true
-```
-
-@example
-```
-import type {IsEqual} from 'type-fest';
-
-// `IsEqual` compares 2 and 1, then gets false as a result.
-type ExpectFalse = IsEqual<2, 1>;
-//=> false
-```
-
-@example
-```
-import type {IsEqual} from 'type-fest';
-
 // `Includes` Returns a boolean for whether the given array includes the given item.
 // `IsEqual` compares the given array of 0th and the given item, then return true if they are equal.
 type Includes<Value extends readonly any[], Item> =
