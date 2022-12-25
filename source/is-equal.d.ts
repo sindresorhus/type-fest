401ad60676f4fb8b3c7770eeaaa7dc6b775ff201
Takahiro Kato <dozo13189@gmail.com>
2022-12-25 19:33:32 +0900

    use cases added as documentation

--- source/is-equal.d.ts
+++ source/is-equal.d.ts
@@ -4,6 +4,9 @@ Returns a boolean for whether the two given types are equal.
 @link https://github.com/microsoft/TypeScript/issues/27024#issuecomment-421529650
 @link https://stackoverflow.com/questions/68961864/how-does-the-equals-work-in-typescript/68963796#68963796
 
+Use-cases:
+- If you want to make a conditional branch based on the result of a comparison of two types.
+
 @example
 ```
 import type {IsEqual} from 'type-fest';
