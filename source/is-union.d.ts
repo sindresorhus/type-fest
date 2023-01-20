b4a56d760929a4ff76e1b9b73fa1bae433565cc2
Sindre Sorhus <sindresorhus@gmail.com>
2023-01-20 14:42:03 +0700

    Update is-union.d.ts

--- source/is-union.d.ts
+++ source/is-union.d.ts
@@ -3,9 +3,9 @@ import type {UnionToIntersection} from './union-to-intersection';
 /**
 Detects whether a given type is a union of types.
 
-While you can default to returning a boolean based on the condition, the resulting typing will be simpler if you instead specify types for True and False.
+While you can default to returning a boolean based on the condition, the resulting typing will be simpler if you instead specify the `True` and `False` type parameters.
 
-There are some types that are themselves unions such as `boolean` and enum types.
+Note that `boolean` and `enum` types are unions.
 
 Use-cases:
 - You want to ensure a given type is not a union but a singular type.
