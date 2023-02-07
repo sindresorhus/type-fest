381a2477bad822a899c558f32e3eefaed2fab1d2
Hiroki Osame <osame@squareup.com>
2023-02-07 09:11:33 +0900

    fix: simplify to actually expand types

--- source/simplify.d.ts
+++ source/simplify.d.ts
@@ -55,4 +55,4 @@ fn(someInterface as Simplify<SomeInterface>); // Good: transform an `interface` 
 
 @category Object
 */
-export type Simplify<T> = {[KeyType in keyof T]: T[KeyType]};
+export type Simplify<T> = {[KeyType in keyof T]: T[KeyType]} & {};
