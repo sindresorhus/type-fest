1d5a0a70bd57320344560717a2de576bc4742e35
Ray Wong <raycnwong@gmail.com>
2023-02-28 12:15:50 +0900

    fix abstract class test

--- test-d/abstract-class.ts
+++ test-d/abstract-class.ts
@@ -13,7 +13,7 @@ function functionRecevingAbsClass<T>(cls: AbstractClass<T>) {
 	return cls;
 }
 
-function withBar<T extends AbstractConstructor<object>>(Ctor: T): AbstractClass<object> {
+function withBar<T extends AbstractConstructor<object>>(Ctor: T) {
 	abstract class ExtendedBar extends Ctor {
 		// eslint-disable-next-line @typescript-eslint/no-empty-function
 		extendedBarMethod() {}
@@ -23,14 +23,18 @@ function withBar<T extends AbstractConstructor<object>>(Ctor: T): AbstractClass<
 
 function assertWithBar() {
 	// This lacks `barMethod`.
-	expectError(class WrongConcreteExtendedBar extends withBar(Bar) {});
+	// @ts-expect-error
+	class WrongConcreteExtendedBar extends withBar(Bar) {}
 
 	// This should be alright since `barMethod` is implemented.
 	class CorrectConcreteExtendedBar extends withBar(Bar) {
 		// eslint-disable-next-line @typescript-eslint/no-empty-function
 		barMethod() {}
 	}
+	expectAssignable(functionRecevingAbsClass<Bar>(withBar(Bar)));
+	expectAssignable(functionRecevingAbsClass<Bar>(CorrectConcreteExtendedBar));
 }
 
 expectAssignable(functionRecevingAbsClass(Foo));
+expectError(functionRecevingAbsClass<Bar>(Foo));
 assertWithBar();
