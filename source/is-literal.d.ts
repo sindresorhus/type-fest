da3c48e5c236bb35eabb66a1a963aeb14662fb45
tommy-mitchell <tmitchell7@uh.edu>
2023-03-06 18:16:28 -0600

    feat: scaffold doc comments

--- source/is-literal.d.ts
+++ source/is-literal.d.ts
@@ -21,18 +21,74 @@ type LiteralChecks<T, LiteralUnionType> = (
 	>
 );
 
+/**
+Returns a boolean for whether the given type is a `string` [literal type](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#literal-types).
+
+@example
+```
+import type {IsStringLiteral} from 'type-fest';
+
+```
+
+@category Utilities
+*/
 export type IsStringLiteral<T> = LiteralCheck<T, string>;
 
+/**
+Returns a boolean for whether the given type is a `number` or `bigint` [literal type](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#literal-types).
+
+@example
+```
+import type {IsNumericLiteral} from 'type-fest';
+
+```
+
+@category Utilities
+*/
 export type IsNumericLiteral<T> = LiteralChecks<T, Numeric>;
 
+/**
+Returns a boolean for whether the given type is a `true` or `false` [literal type](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#literal-types).
+
+@example
+```
+import type {IsBooleanLiteral} from 'type-fest';
+
+```
+
+@category Utilities
+*/
 export type IsBooleanLiteral<T> = LiteralCheck<T, boolean>;
 
+/**
+Returns a boolean for whether the given type is a `symbol` [literal type](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#literal-types).
+
+@example
+```
+import type {IsLiteral} from 'type-fest';
+
+```
+
+@category Utilities
+*/
 export type IsSymbolLiteral<T> = LiteralCheck<T, symbol>;
 
+/** Helper type for `IsLiteral`. */
 type IsLiteralUnion<T> =
 	| IsStringLiteral<T>
 	| IsNumericLiteral<T>
 	| IsBooleanLiteral<T>
 	| IsSymbolLiteral<T>;
 
+/**
+Returns a boolean for whether the given type is a [literal type](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#literal-types).
+
+@example
+```
+import type {IsLiteral} from 'type-fest';
+
+```
+
+@category Utilities
+*/
 export type IsLiteral<T extends Primitive> = IsNotFalse<IsLiteralUnion<T>>;
