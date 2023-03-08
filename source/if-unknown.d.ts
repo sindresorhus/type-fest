1530ef9d1842bf3805b31fd74c1c5d532d6c3337
tommy-mitchell <tmitchell7@uh.edu>
2023-03-07 21:38:01 -0600

    feat(`unknown`): add documentation

--- source/if-unknown.d.ts
+++ source/if-unknown.d.ts
@@ -3,10 +3,41 @@ import type {IsUnknown} from './is-unknown';
 /**
 If the given type `T` is `unknown`, the returned type is `TypeIfUnknown`. Otherwise, the return type is `TypeIfNotUnknown`. If only `T` is specified, `TypeIfUnknown` will be `true` and `TypeIfNotUnknown` will be false.
 
+Useful in type utilities, such as when dealing with unknown data from API calls.
+
 @example
 ```
 import type {IfUnknown} from 'type-fest';
 
+// https://github.com/pajecawav/tiny-global-store/blob/master/src/index.ts
+type Action<TState, TPayload = void> = IfUnknown<
+	TPayload,
+	(state: TState) => TState,
+	(state: TState, payload: TPayload) => TState
+>;
+
+class Store<TState> {
+	constructor(private state: TState) {}
+
+	execute<TPayload = void>(action: Action<TState, TPayload>, payload?: TPayload): TState {
+		this.state = action(this.state, payload);
+		return this.state;
+	}
+
+	// ... other methods
+}
+
+const store = new Store({value: 1});
+declare const someExternalData: unknown;
+
+store.execute(state => ({value: state.value + 1}));
+//=> `TPayload` is `void`
+
+store.execute((state, payload) => ({value: state.value + payload}), 5);
+//=> `TPayload` is `5`
+
+store.execute((state, payload) => ({value: state.value + payload}), someExternalData);
+//=> Errors: `action` is `(state: TState) => TState`
 ```
 
 @category Utilities
--- source/is-unknown.d.ts
+++ source/is-unknown.d.ts
@@ -5,10 +5,40 @@ Returns a boolean for whether the given type is `unknown`.
 
 @link https://github.com/dsherret/conditional-type-checks/pull/16
 
+Useful in type utilities, such as when dealing with unknown data from API calls.
+
 @example
 ```
 import type {IsUnknown} from 'type-fest';
 
+// https://github.com/pajecawav/tiny-global-store/blob/master/src/index.ts
+type Action<TState, TPayload = void> =
+	IsUnknown<TPayload> extends true
+		? (state: TState) => TState,
+		: (state: TState, payload: TPayload) => TState;
+
+class Store<TState> {
+	constructor(private state: TState) {}
+
+	execute<TPayload = void>(action: Action<TState, TPayload>, payload?: TPayload): TState {
+		this.state = action(this.state, payload);
+		return this.state;
+	}
+
+	// ... other methods
+}
+
+const store = new Store({value: 1});
+declare const someExternalData: unknown;
+
+store.execute(state => ({value: state.value + 1}));
+//=> `TPayload` is `void`
+
+store.execute((state, payload) => ({value: state.value + payload}), 5);
+//=> `TPayload` is `5`
+
+store.execute((state, payload) => ({value: state.value + payload}), someExternalData);
+//=> Errors: `action` is `(state: TState) => TState`
 ```
 
 @category Utilities
