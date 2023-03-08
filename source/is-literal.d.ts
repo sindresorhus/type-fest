5f7ca088994443b63d8cfaa05675a8f00ce88ac8
tommy-mitchell <tmitchell7@uh.edu>
2023-03-08 12:52:01 -0600

    feat(`IsBooleanLiteral`): add example

--- source/is-literal.d.ts
+++ source/is-literal.d.ts
@@ -131,12 +131,36 @@ export type IsNumericLiteral<T> = LiteralChecks<T, Numeric>;
 /**
 Returns a boolean for whether the given type is a `true` or `false` [literal type](https://www.typescriptlang.org/docs/handbook/2/everyday-types.html#literal-types).
 
-Useful for type utilities, such as when constructing parsers and ASTs.
+Useful for:
+	- providing strongly-typed functions when given literal arguments
+	- type utilities, such as when constructing parsers and ASTs
 
 @example
 ```
 import type {IsBooleanLiteral} from 'type-fest';
 
+const id = 123;
+
+type GetId<AsString extends boolean> =
+	IsBooleanLiteral<AsString> extends true
+		? AsString extends true
+			? `${typeof id}`
+			: typeof id
+		: number | string;
+
+function getId<AsString extends boolean = false>(options?: {asString: AsString}) {
+	return (options?.asString ? `${id}` : id) as GetId<AsString>;
+}
+
+const numberId = getId();
+//=> 123
+
+const stringId = getId({asString: true});
+//=> '123'
+
+declare const runtimeBoolean: boolean;
+const eitherId = getId({asString: runtimeBoolean});
+//=> number | string
 ```
 
 @category Utilities
