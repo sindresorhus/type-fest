6a39a2988dd4b81348853588fd34e091994d7e18
tommy-mitchell <tmitchell7@uh.edu>
2023-03-07 20:17:43 -0600

    feat(`never`): add documentation

--- source/if-never.d.ts
+++ source/if-never.d.ts
@@ -5,11 +5,43 @@ If the given type `T` is `never`, the returned type is `TypeIfNever`. Otherwise,
 
 @link https://github.com/microsoft/TypeScript/issues/31751#issuecomment-498526919
 @link https://stackoverflow.com/a/53984913/10292952
+@link https://www.zhenghao.io/posts/ts-never
+
+Useful in type utilities, such as checking if something does not occur.
 
 @example
 ```
 import type {IfNever} from 'type-fest';
 
+type And<A, B> =
+	A extends true
+		? B extends true
+			? true
+			: false
+		: false;
+
+type AreStringsEqual<A extends string, B extends string> =
+	And<
+		IfNever<Exclude<A, B>>,
+		IfNever<Exclude<B, A>>
+	>;
+
+type EndIfEqual<I extends string, O extends string> =
+	AreStringsEqual<I, O> extends true
+		? never
+		: void;
+
+function endIfEqual<I extends string, O extends string>(input: I, output: O): EndIfEqual<I, O> {
+	if (input === output) {
+		process.exit(0);
+	}
+}
+
+endIfEqual('abc', 'abc');
+//=> never
+
+endIfEqual('abc', '123');
+//=> void
 ```
 
 @category Utilities
