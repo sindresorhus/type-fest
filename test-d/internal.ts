d7a1d0f33dd12eec77812c97767b00bced9d73dd
Tommy <tmitchell7@uh.edu>
2023-03-10 14:54:24 -0600

    Add `IsLiteral` types (#563)

--- test-d/internal.ts
+++ test-d/internal.ts
@@ -1,5 +1,5 @@
 import {expectType} from 'tsd';
-import type {IsWhitespace, IsNumeric} from '../source/internal';
+import type {IsWhitespace, IsNumeric, IsNotFalse} from '../source/internal';
 
 expectType<IsWhitespace<''>>(false);
 expectType<IsWhitespace<' '>>(true);
@@ -27,3 +27,11 @@ expectType<IsNumeric<' 1.2'>>(false);
 expectType<IsNumeric<'1 2'>>(false);
 expectType<IsNumeric<'1_200'>>(false);
 expectType<IsNumeric<' 1 '>>(false);
+
+expectType<IsNotFalse<true>>(true);
+expectType<IsNotFalse<boolean>>(true);
+expectType<IsNotFalse<true | false>>(true);
+expectType<IsNotFalse<true | false | false | false>>(true);
+expectType<IsNotFalse<false>>(false);
+expectType<IsNotFalse<false | false>>(false);
+expectType<IsNotFalse<false | false | false | false>>(false);
---
+++ test-d/is-literal.ts
@@ -0,0 +1,64 @@
+import {expectError, expectType} from 'tsd';
+import type {
+	IsLiteral,
+	IsStringLiteral,
+	IsNumericLiteral,
+	IsBooleanLiteral,
+	IsSymbolLiteral,
+} from '../index';
+
+const stringLiteral = '';
+const numberLiteral = 1;
+// @ts-expect-error: suppress BigInt literal tsd warning
+const bigintLiteral = 1n;
+const booleanLiteral = true;
+const symbolLiteral = Symbol('');
+
+declare const _string: string;
+declare const _number: number;
+declare const _bigint: bigint;
+declare const _boolean: boolean;
+declare const _symbol: symbol;
+
+// Literals should be true
+expectType<IsLiteral<typeof stringLiteral>>(true);
+expectType<IsLiteral<typeof numberLiteral>>(true);
+expectType<IsLiteral<typeof bigintLiteral>>(true);
+expectType<IsLiteral<typeof booleanLiteral>>(true);
+expectType<IsLiteral<typeof symbolLiteral>>(true);
+
+// Primitives should be false
+expectType<IsLiteral<typeof _string>>(false);
+expectType<IsLiteral<typeof _number>>(false);
+expectType<IsLiteral<typeof _bigint>>(false);
+expectType<IsLiteral<typeof _boolean>>(false);
+expectType<IsLiteral<typeof _symbol>>(false);
+
+// Null, undefined, and non-primitives should fail all literal checks
+expectType<IsLiteral<null>>(false);
+expectType<IsLiteral<undefined>>(false);
+expectType<IsLiteral<any>>(false);
+expectType<IsLiteral<never>>(false);
+
+expectType<IsStringLiteral<typeof stringLiteral>>(true);
+expectType<IsStringLiteral<typeof _string>>(false);
+
+expectType<IsNumericLiteral<typeof numberLiteral>>(true);
+expectType<IsNumericLiteral<typeof bigintLiteral>>(true);
+expectType<IsNumericLiteral<typeof _number>>(false);
+expectType<IsNumericLiteral<typeof _bigint>>(false);
+
+expectType<IsBooleanLiteral<typeof booleanLiteral>>(true);
+expectType<IsBooleanLiteral<typeof _boolean>>(false);
+
+expectType<IsSymbolLiteral<typeof symbolLiteral>>(true);
+expectType<IsSymbolLiteral<typeof _symbol>>(false);
+
+declare const anything: any;
+
+// Missing generic parameter
+expectError<IsLiteral>(anything);
+expectError<IsStringLiteral>(anything);
+expectError<IsNumericLiteral>(anything);
+expectError<IsBooleanLiteral>(anything);
+expectError<IsSymbolLiteral>(anything);
