911352cd8ff82e8bb5e668448aca112b425b04b9
orimiles5 <ori.miles@boyar.org.il>
2023-03-11 23:08:23 +0200

    add test cases for strict except

--- test-d/except.ts
+++ test-d/except.ts
@@ -1,8 +1,22 @@
-import {expectType} from 'tsd';
+import {expectType, expectError} from 'tsd';
 import type {Except} from '../index';
 
 declare const except: Except<{a: number; b: string}, 'b'>;
 expectType<{a: number}>(except);
+expectError(except.b);
+
+const nonStrict = {
+	a: 1,
+	b: '2',
+};
+
+const nonStrictAssignment: typeof except = nonStrict; // No error
+
+declare const strictExcept: Except<{a: number; b: string}, 'b', {strict: true}>;
+
+expectError(() => {
+	const strictAssignment: typeof strictExcept = nonStrict;
+});
 
 // Generic properties
 type Example = {
