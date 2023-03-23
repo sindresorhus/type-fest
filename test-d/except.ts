c5743c9dabdbfcdc58b1f924cd59ac8c3c3deedb
orimiles5 <97595296+orimiles5@users.noreply.github.com>
2023-03-23 11:54:14 +0200

    `Except`: Add `requireExactProps` option (#560)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>
    Co-authored-by: Tommy <tmitchell7@uh.edu>

--- test-d/except.ts
+++ test-d/except.ts
@@ -1,8 +1,22 @@
-import {expectType} from 'tsd';
+import {expectType, expectError} from 'tsd';
 import type {Except} from '../index';
 
 declare const except: Except<{a: number; b: string}, 'b'>;
 expectType<{a: number}>(except);
+expectError(except.b);
+
+const nonStrict = {
+	a: 1,
+	b: '2',
+};
+
+const nonStrictAssignment: typeof except = nonStrict; // No error
+
+declare const strictExcept: Except<{a: number; b: string}, 'b', {requireExactProps: true}>;
+
+expectError(() => {
+	const strictAssignment: typeof strictExcept = nonStrict;
+});
 
 // Generic properties
 type Example = {
@@ -11,7 +25,7 @@ type Example = {
 	bar: string;
 };
 
-const test: Except<Example, 'bar'> = {foo: 123, bar: 'asdf'};
+const test: Except<Example, 'bar', {requireExactProps: false}> = {foo: 123, bar: 'asdf'};
 expectType<number>(test.foo);
 // eslint-disable-next-line @typescript-eslint/dot-notation
 expectType<unknown>(test['bar']);
