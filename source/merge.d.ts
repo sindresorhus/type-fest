cbb7bbe37d62dab116a5fb7189edd211ac295b82
Sébastien Mischler <skarab@bluewin.ch>
2023-03-28 19:57:25 +0200

    fix: remove unused import

--- source/merge.d.ts
+++ source/merge.d.ts
@@ -1,7 +1,6 @@
 import type {OmitIndexSignature} from './omit-index-signature';
 import type {PickIndexSignature} from './pick-index-signature';
 import type {EnforceOptional} from './enforce-optional';
-import type {Simplify} from './simplify';
 
 // Merges two objects without worrying about index signatures.
 type SimpleMerge<Destination, Source> = {
