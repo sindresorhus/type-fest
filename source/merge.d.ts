60a057b891f94df3d6ffe3d10fd6b90d475d9315
Sébastien Mischler <skarab@bluewin.ch>
2023-03-29 14:38:09 +0200

    Fix `Merge` with optional `any` value (#583)

--- source/merge.d.ts
+++ source/merge.d.ts
@@ -2,14 +2,10 @@ import type {OmitIndexSignature} from './omit-index-signature';
 import type {PickIndexSignature} from './pick-index-signature';
 import type {EnforceOptional} from './enforce-optional';
 
-// Merges two objects without worrying about index signatures or optional keys.
+// Merges two objects without worrying about index signatures.
 type SimpleMerge<Destination, Source> = {
-	[Key in keyof Destination | keyof Source]: Key extends keyof Source
-		? Source[Key]
-		: Key extends keyof Destination
-			? Destination[Key]
-			: never;
-};
+	[Key in keyof Destination as Key extends keyof Source ? never : Key]: Destination[Key];
+} & Source;
 
 /**
 Merge two types into a new type. Keys of the second type overrides keys of the first type.
