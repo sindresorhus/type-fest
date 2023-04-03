484d27a613a01673a8e5e2d8b39f1eec8a99cc07
Alvis Tang <alvis@hilbert.space>
2023-04-03 07:10:16 +0000

    doc: improve the documentation of LiteralToPrimitiveDeep in code

--- source/literal-to-primitive-deep.d.ts
+++ source/literal-to-primitive-deep.d.ts
@@ -2,14 +2,15 @@ import type {LiteralToPrimitive} from './literal-to-primitive';
 import type {OmitIndexSignature} from './omit-index-signature';
 
 /**
-Given a constant object, return a new object with the same keys but with all the values converted to primitives.
+Like `LiteralToPrimitive`, but it converts literal types inside an object or array deeply.
+i.e. Given a constant object, return a new object type with the same keys but with all the values converted to primitives.
 @see LiteralToPrimitive
 
 Use-case: Deal with data that is imported from a JSON file.
 
 @example
 ```
-import type { LiteralToPrimitiveDeep, TsConfigJson } from 'type-fest';
+import type {LiteralToPrimitiveDeep, TsConfigJson} from 'type-fest';
 import tsconfig from 'path/to/tsconfig.json';
 
 function doSomethingWithTSConfig(config: LiteralToPrimitiveDeep<TsConfigJson>) { ... }
