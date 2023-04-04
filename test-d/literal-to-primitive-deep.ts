5374588a88ee643893784f66367bc26b8e6509ec
Alvis Tang <alvis@hilbert.space>
2023-04-04 07:53:19 +0100

    Add `LiteralToPrimitiveDeep` type (#584)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

---
+++ test-d/literal-to-primitive-deep.ts
@@ -0,0 +1,54 @@
+import {expectType} from 'tsd';
+import type {IsEqual, LiteralToPrimitiveDeep} from '../index';
+
+type LiteralObject = {
+	a: string;
+	b: number;
+	c: boolean;
+	d: {
+		e: bigint;
+		f: symbol;
+		g: {
+			h: string[];
+			i: {
+				j: boolean;
+				k: {
+					l: 1;
+					m: 'hello';
+					o: [1, 2, 3];
+					p: ['a', 'b', 'c'];
+					q: [1, 'a', true];
+				};
+			};
+		};
+	};
+};
+
+type PrimitiveObject = {
+	a: string;
+	b: number;
+	c: boolean;
+	d: {
+		e: bigint;
+		f: symbol;
+		g: {
+			h: string[];
+			i: {
+				j: boolean;
+				k: {
+					l: number;
+					m: string;
+					o: number[];
+					p: string[];
+					q: Array<number | string | boolean>;
+				};
+			};
+		};
+	};
+};
+
+const typeEqual: IsEqual<
+LiteralToPrimitiveDeep<LiteralObject>,
+PrimitiveObject
+> = true;
+expectType<true>(typeEqual);
