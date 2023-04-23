a96bc1e953cb5a7c9423ddc1406cb89752a781bd
Sindre Sorhus <sindresorhus@gmail.com>
2023-04-23 16:26:24 +0800

    Update override-properties.d.ts

--- source/override-properties.d.ts
+++ source/override-properties.d.ts
@@ -1,7 +1,7 @@
 import type {Merge} from './merge';
 
 /**
-Override existing properties of the given type. Similar to Merge, but enforces that the original type has the properties you want to override.
+Override existing properties of the given type. Similar to `Merge`, but enforces that the original type has the properties you want to override.
 
 This is useful when you want to override existing properties with a different type and make sure that these properties really exist in the original.
 
@@ -17,6 +17,7 @@ type Bar = OverrideProperties<Foo, {b: number}>
 type Baz = OverrideProperties<Foo, {c: number}>
 // error TS2559: Type '{c: number}' has no properties in common with type 'Partial{a: unknown; b: unknown}>'.
 ```
+
 @category Object
 */
 export type OverrideProperties<
