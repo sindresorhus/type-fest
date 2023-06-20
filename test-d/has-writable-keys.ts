ef813ff5635a2e8d0f3337166d37f32d5a43b4ee
Trevor Florence <tflo@google.com>
2023-06-20 13:33:37 -0600

    Add `HasReadonlyKeys` and `HasWritableKeys` types
    These complement the new `ReadonlyKeysOf` and `WritableKeysOf` types and mirror the `HasOptionalKeys` and `HasRequiredKeys` types.

---
+++ test-d/has-writable-keys.ts
@@ -0,0 +1,29 @@
+import {expectType} from 'tsd';
+import type {HasWritableKeys} from '../index';
+
+type TestType1 = {
+	a: string;
+	readonly b: boolean;
+};
+
+type TestType2 = {
+	readonly a: string;
+	readonly b: boolean;
+};
+
+type TestType3 = {
+	a: string;
+	b: boolean;
+};
+
+type HasWritableKeys1 = HasWritableKeys<TestType1>;
+type HasWritableKeys2 = HasWritableKeys<TestType2>;
+type HasWritableKeys3 = HasWritableKeys<TestType3>;
+
+declare const test1: HasWritableKeys1;
+declare const test2: HasWritableKeys2;
+declare const test3: HasWritableKeys3;
+
+expectType<true>(test1);
+expectType<false>(test2);
+expectType<true>(test3);
