e87b0667e1f34a94c0af5d7cde7fde841595915e
Michaël De Boey <info@michaeldeboey.be>
2023-06-20 03:27:46 +0200

    Update jsonify.ts

--- test-d/jsonify.ts
+++ test-d/jsonify.ts
@@ -223,7 +223,7 @@ declare const tupleRestJson: Jsonify<[string, ...Date[]]>;
 expectType<[string, ...string[]]>(tupleRestJson);
 
 declare const tupleStringJson: Jsonify<string[] & ['some value']>;
-expectType<string[] & ['some value']>(tupleStringJson);
+expectType<['some value']>(tupleStringJson);
 
 // BigInt fails JSON.stringify
 declare const bigInt: Jsonify<bigint>;
