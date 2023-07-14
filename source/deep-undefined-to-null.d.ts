83f6ab4a9831a81c89f3de7739ad1d4d06f6cfad
Andrew Jarrett <ahrjarrett@gmail.com>
2023-07-14 18:23:32 -0500

    feat: `DeepUndefinedToNull` supports unions

--- source/deep-undefined-to-null.d.ts
+++ source/deep-undefined-to-null.d.ts
@@ -1,49 +1,102 @@
-import type {OptionalKeysOf} from './optional-keys-of';
-import type {RequiredKeysOf} from './required-keys-of';
+type TempPlaceholder = typeof TempPlaceholder
+declare const TempPlaceholder: unique symbol
 
-/** Any record is assignable to `AnyRecord`, but arrays are not. */
-type AnyRecord = Record<symbol, unknown>;
+export type DeepReplace<
+	BaseType,
+	Find,
+	Replace,
+>
+	= BaseType extends BaseType ? BaseType extends Find ? Replace
+	: BaseType extends Record<symbol, any> ? { [k in keyof BaseType]: DeepReplace<BaseType[k], Find, Replace> }
+	: BaseType extends readonly any[] ? { [ix in keyof BaseType]: DeepReplace<BaseType[ix], Find, Replace> }
+	: BaseType
+	: never
 
-/**
- * Type function that accepts a record, recursively removes all optional property modifiers, and in those properties' values replaces `undefined` with `null`.
- *
- * Optionally, providing the second parameter to `DeepUndefinedToNull` lets you specify the type to replace `undefined` with.
- *
- * Use-case:
- *
- * JSON, for example, does not accept `undefined` as a value. If you need to send an object containing undefined over the wire, without a function
- * like `DeepUndefinedToNull`, you'd need to write that type by hand.
- *
- * Also, since JavaScript runtimes will implicitly insert `undefined` in the absence of a
- * value, using `undefined` can create ambiguity (is this value undefined because someone forgot to add a value, or because `undefined` was used specifically?).
- *
- * @example
- * import type {DeepUndefinedToNull} from 'type-fest';
- * type TypeWithOptionalProps = {a?: 1; b: 2; c: 3 | undefined; d: {e?: 3}};
- * type TypeWithNullReplaced = DeepUndefinedToNull<TypeWithOptionalProps>;
- * //   ^? { a: 1 | null; b: 2; c: 3 | undefined; d: { e: 3 | null }}
- *
- * @category Type
- * @category Object
- */
-export type DeepUndefinedToNull<
-	BaseType extends AnyRecord,
-	Replacement = null,
-> =
-  /**
-	 * Any type unioned with `never` is an identity for that type; the reason we add it
-	 * here is because it "forces" the TypeScript LSP to evaluate it, showing the full type
-	 */
-	| never // eslint-disable-line @typescript-eslint/no-redundant-type-constituents
-	| {
-		[Key in keyof Required<BaseType>]:
-		Required<BaseType>[Key] extends AnyRecord
-			? undefined extends BaseType[Key]
-				? Replacement | DeepUndefinedToNull<Exclude<Required<BaseType>[Key], undefined>>
-				: DeepUndefinedToNull<Exclude<Required<BaseType>[Key], undefined>>
-			: Key extends OptionalKeysOf<BaseType>
-				? Replacement | Exclude<BaseType[Key], undefined>
-				: Key extends RequiredKeysOf<BaseType>
-					? BaseType[Key]
-					: never
-	};
+type ReplaceUnionMember<
+	BaseType,
+	Find,
+	Replace
+>
+	= BaseType extends BaseType
+	? BaseType extends Find
+	? Replace
+	: BaseType
+	: never
+
+type DeepUndefinedToNull_<
+	BaseType,
+	Replace
+>
+	= BaseType extends BaseType ? BaseType extends undefined ? TempPlaceholder
+	: BaseType extends Record<symbol, any>
+	? { [k in keyof BaseType]-?: DeepUndefinedToNull_<{} extends Pick<BaseType, k> ? ReplaceUnionMember<BaseType[k], undefined, Replace> : BaseType[k], Replace> }
+	: BaseType
+	: never
+
+type DeepUndefinedToNull<
+	BaseType,
+	Replace = null
+>
+	= DeepReplace<
+		DeepUndefinedToNull_<
+			BaseType,
+			Replace
+		>,
+		TempPlaceholder,
+		undefined
+	>
+
+
+// /**
+//  * Type function that accepts a record, recursively removes all optional property modifiers, and in those properties' values replaces `undefined` with `null`.
+//  *
+//  * Optionally, providing the second parameter to `DeepUndefinedToNull` lets you specify the type to replace `undefined` with.
+//  *
+//  * Use-case:
+//  *
+//  * JSON, for example, does not accept `undefined` as a value. If you need to send an object containing undefined over the wire, without a function
+//  * like `DeepUndefinedToNull`, you'd need to write that type by hand.
+//  *
+//  * Also, since JavaScript runtimes will implicitly insert `undefined` in the absence of a
+//  * value, using `undefined` can create ambiguity (is this value undefined because someone forgot to add a value, or because `undefined` was used specifically?).
+//  *
+//  * @example
+//  * import type {DeepUndefinedToNull} from 'type-fest';
+//  * type TypeWithOptionalProps = {a?: 1; b: 2; c: 3 | undefined; d: {e?: 3}};
+//  * type TypeWithNullReplaced = DeepUndefinedToNull<TypeWithOptionalProps>;
+//  * //   ^? { a: 1 | null; b: 2; c: 3 | undefined; d: { e: 3 | null }}
+//  *
+//  * @category Type
+//  * @category Object
+//  */
+// export type DeepUndefinedToNull___<
+// 	BaseType extends AnyRecord,
+// 	Replacement = null,
+// > =
+// 	/**
+// 	 * Any type unioned with `never` is an identity for that type; the reason we add it
+// 	 * here is because it "forces" the TypeScript LSP to evaluate it, showing the full type
+// 	 */
+// 	| never // eslint-disable-line @typescript-eslint/no-redundant-type-constituents
+// 	| {
+// 		[Key in keyof Required<BaseType>]:
+// 		/**
+// 		 * Here we check if the original record at this key was optional, so we
+// 		 * don't forget to union the value with `Replacement` when recursing
+// 		 */
+// 		Required<BaseType>[Key] extends AnyRecord
+// 		? undefined extends BaseType[Key]
+// 		? Required<BaseType>[Key] extends any ? Replacement | DeepUndefinedToNull<Required<BaseType>[Key]> : never
+// 		: Required<BaseType>[Key] extends any ? DeepUndefinedToNull<Required<BaseType>[Key]> : never
+// 		: Key extends OptionalKeysOf<BaseType>
+// 		? Exclude<BaseType, undefined>[Key] extends any ? Replacement | Exclude<BaseType[Key], undefined> : never
+// 		: Key extends RequiredKeysOf<BaseType>
+// 		? BaseType[Key]
+// 		: never
+// 	};
+
+// /**
+//  * Here we declare an intermediate type variable called `Value` so we don't perform the
+//  * same lookup again and and (and so we don't have to keep typing the whole thing out).
+//  * Prepending the type variable with `|` calls attention to the new identifier.
+//  */
--- test-d/deep-undefined-to-null.ts
+++ test-d/deep-undefined-to-null.ts
@@ -1,16 +1,16 @@
-import {expectType} from 'tsd';
-import type {DeepUndefinedToNull} from '../index';
+import { expectType } from 'tsd';
+import type { DeepUndefinedToNull } from '../index';
 
-type In1 = {a: string; b?: boolean};
-type Out1 = {a: string; b: boolean | null};
+type In1 = { a: string; b?: boolean };
+type Out1 = { a: string; b: boolean | null };
 
-type In2 = {a?: string; b?: boolean};
-type Out2 = {a: string | null; b: boolean | null};
+type In2 = { a?: string; b?: boolean };
+type Out2 = { a: string | null; b: boolean | null };
 
-type In3 = {a: string; b: boolean};
-type Out3 = {a: string; b: boolean};
+type In3 = { a: string; b: boolean };
+type Out3 = { a: string; b: boolean };
 
-type In4 = {a: undefined};
+type In4 = { a: undefined };
 type Out4 = In4;
 
 type In5 = {
@@ -37,32 +37,52 @@ type In5 = {
 
 type Out5 = {
 	a: 1;
-	b: 2 | null;
-	c: 3 | null;
-	d: 4 | null;
-	e: 5 | null;
-	f: 6 | undefined;
-	g: {} | null;
+	b: | null | 2;
+	c: | null | 3;
+	d: | null | 4;
+	e: | null | 5;
+	f: | undefined | 6;
+	g: | null | {};
 	h: {
 		i: 7;
-		j: 7 | null;
-		k: {
-			l: 8 | null;
+		j: | null | 7;
+		k:
+		| null
+		| {
+			l: | null | 8;
 			m: 9;
-			n: 10 | null;
-			o: 11 | null;
-			p: 12 | null;
-			q: 13 | undefined;
-		} | null;
+			n: | null | 10;
+			o: | null | 11;
+			p: | null | 12;
+			q: | undefined | 13;
+		};
 	};
 };
 
 type In6 = {
-	a?: 1;
+	a?: 1 | { b: 2, c?: 3, d: undefined | readonly undefined[] | { e?: { a?: 1 } | { a?: 2 } } }
 };
 
 type Out6 = {
-	a: 1 | 0;
+	a: 1 | {
+		b: 2;
+		c: 3 | null;
+		d: readonly undefined[] | {
+			e: {
+				a: 1 | null;
+			} | {
+				a: 2 | null;
+			} | null;
+		} | undefined;
+	} | null;
+};
+
+type In7 = {
+	a?: 1;
+};
+
+type Out7 = {
+	a: | 0 | 1;
 };
 
 declare const test1: DeepUndefinedToNull<In1>;
@@ -70,7 +90,8 @@ declare const test2: DeepUndefinedToNull<In2>;
 declare const test3: DeepUndefinedToNull<In3>;
 declare const test4: DeepUndefinedToNull<In4>;
 declare const test5: DeepUndefinedToNull<In5>;
-declare const test6: DeepUndefinedToNull<In6, 0>;
+declare const test6: DeepUndefinedToNull<In6>;
+declare const test7: DeepUndefinedToNull<In7, 0>;
 
 expectType<Out1>(test1);
 expectType<Out2>(test2);
@@ -78,3 +99,4 @@ expectType<Out3>(test3);
 expectType<Out4>(test4);
 expectType<Out5>(test5);
 expectType<Out6>(test6);
+expectType<Out7>(test7);
