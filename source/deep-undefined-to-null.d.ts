7d7a540c465ddce8111a42d444dbcc6bfefd66ed
Andrew Jarrett <ahrjarrett@gmail.com>
2023-07-14 16:19:26 -0500

    chore: fixes formatting

--- source/deep-undefined-to-null.d.ts
+++ source/deep-undefined-to-null.d.ts
@@ -1,16 +1,48 @@
 import type {OptionalKeysOf} from './optional-keys-of';
 import type {RequiredKeysOf} from './required-keys-of';
 
-type AnyRecord = Record<symbol, unknown>
+/** Any record is assignable to `AnyRecord`, but arrays are not. */
+type AnyRecord = Record<symbol, unknown>;
 
-export type DeepUndefinedToNull<BaseType extends AnyRecord> =
+/**
+ * Type function that accepts a record, recursively removes all optional property modifiers, and in those properties' values replaces `undefined` with `null`.
+ *
+ * Optionally, providing the second parameter to `DeepUndefinedToNull` lets you specify the type to replace `undefined` with.
+ *
+ * Use-case:
+ *
+ * JSON, for example, does not accept `undefined` as a value. If you need to send an object containing undefined over the wire, without a function
+ * like `DeepUndefinedToNull`, you'd need to write that type by hand.
+ *
+ * Also, since JavaScript runtimes will implicitly insert `undefined` in the absence of a
+ * value, using `undefined` can create ambiguity (is this value undefined because someone forgot to add a value, or because `undefined` was used specifically?).
+ *
+ * @example
+ * import type {DeepUndefinedToNull} from 'type-fest';
+ * type TypeWithOptionalProps = {a?: 1; b: 2; c: 3 | undefined; d: {e?: 3}};
+ * type TypeWithNullReplaced = DeepUndefinedToNull<TypeWithOptionalProps>;
+ * //   ^? { a: 1 | null; b: 2; c: 3 | undefined; d: { e: 3 | null }}
+ *
+ * @category Type
+ * @category Object
+ */
+export type DeepUndefinedToNull<
+	BaseType extends AnyRecord,
+	Replacement = null,
+> =
+  /**
+	 * Any type unioned with `never` is an identity for that type; the reason we add it
+	 * here is because it "forces" the TypeScript LSP to evaluate it, showing the full type
+	 */
 	| never // eslint-disable-line @typescript-eslint/no-redundant-type-constituents
 	| {
 		[Key in keyof Required<BaseType>]:
 		Required<BaseType>[Key] extends AnyRecord
-			? DeepUndefinedToNull<Exclude<Required<BaseType>[Key], undefined>>
+			? undefined extends BaseType[Key]
+				? Replacement | DeepUndefinedToNull<Exclude<Required<BaseType>[Key], undefined>>
+				: DeepUndefinedToNull<Exclude<Required<BaseType>[Key], undefined>>
 			: Key extends OptionalKeysOf<BaseType>
-				? null | Exclude<BaseType[Key], undefined>
+				? Replacement | Exclude<BaseType[Key], undefined>
 				: Key extends RequiredKeysOf<BaseType>
 					? BaseType[Key]
 					: never
--- test-d/deep-undefined-to-null.ts
+++ test-d/deep-undefined-to-null.ts
@@ -14,35 +14,55 @@ type In4 = {a: undefined};
 type Out4 = In4;
 
 type In5 = {
-	a: void;
-	b?: void;
-	c?: {
-		d?: void;
-		e?: {
-			f?: void;
-			g: void;
+	a: 1;
+	b?: 2;
+	c?: 3 | undefined;
+	d?: 4 | null;
+	e?: 5 | undefined | null;
+	f: 6 | undefined;
+	g?: {};
+	h: {
+		i: 7;
+		j?: 7;
+		k?: {
+			l?: 8;
+			m: 9;
+			n?: 10 | undefined;
+			o?: 11 | null;
+			p?: 12 | undefined | null;
+			q: 13 | undefined;
 		};
 	};
-	d?: void | undefined;
-	e?: void | null;
-	f?: void | undefined | null;
-	g: void | undefined;
 };
 
 type Out5 = {
-	a: void;
-	b: never;
-	c: {
-		d: never;
-		e: {
-			f: never;
-			g: void;
-		};
+	a: 1;
+	b: 2 | null;
+	c: 3 | null;
+	d: 4 | null;
+	e: 5 | null;
+	f: 6 | undefined;
+	g: {} | null;
+	h: {
+		i: 7;
+		j: 7 | null;
+		k: {
+			l: 8 | null;
+			m: 9;
+			n: 10 | null;
+			o: 11 | null;
+			p: 12 | null;
+			q: 13 | undefined;
+		} | null;
 	};
-	d: never;
-	e: void | null;
-	f: void | null;
-	g: void | undefined;
+};
+
+type In6 = {
+	a?: 1;
+};
+
+type Out6 = {
+	a: 1 | 0;
 };
 
 declare const test1: DeepUndefinedToNull<In1>;
@@ -50,9 +70,11 @@ declare const test2: DeepUndefinedToNull<In2>;
 declare const test3: DeepUndefinedToNull<In3>;
 declare const test4: DeepUndefinedToNull<In4>;
 declare const test5: DeepUndefinedToNull<In5>;
+declare const test6: DeepUndefinedToNull<In6, 0>;
 
 expectType<Out1>(test1);
 expectType<Out2>(test2);
 expectType<Out3>(test3);
 expectType<Out4>(test4);
 expectType<Out5>(test5);
+expectType<Out6>(test6);
