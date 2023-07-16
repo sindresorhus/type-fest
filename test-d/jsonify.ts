9fdac37ea5e1ea7fcc444a8df95c8eafe88dca2f
Sindre Sorhus <sindresorhus@gmail.com>
2023-07-16 11:44:27 +0200

    Revert "`Jsonify`: Fix handling of tuples (#638)"
    This reverts commit 605b9011726e72c6ff3735ce6c864263ff5bdcb6.

--- test-d/jsonify.ts
+++ test-d/jsonify.ts
@@ -222,9 +222,6 @@ expectType<[string, string]>(tupleJson);
 declare const tupleRestJson: Jsonify<[string, ...Date[]]>;
 expectType<[string, ...string[]]>(tupleRestJson);
 
-declare const tupleStringJson: Jsonify<string[] & ['some value']>;
-expectType<['some value']>(tupleStringJson);
-
 // BigInt fails JSON.stringify
 declare const bigInt: Jsonify<bigint>;
 expectType<never>(bigInt);
