9e5a3dcae7d93260e9f0a1114e38fa032fa579f8
Matan Yadaev <matan.yed@gmail.com>
2023-08-05 14:57:07 +0300

    add docs

--- source/unknown-record.d.ts
+++ source/unknown-record.d.ts
@@ -1,6 +1,31 @@
 /**
 Matches any unknown record.
 
+Use case: You have an object that you don't know its keys or values.
+
+@example
+```
+import type {UnknownRecord} from 'type-fest';
+
+function toJson(object: UnknownRecord) {
+	return JSON.stringify(object);
+}
+
+toJson({hello: 'world'});
+//=> '{"hello":"world"}'
+
+function isObject(value: unknown): value is UnknownRecord {
+	return typeof value === 'object' && value !== null;
+}
+
+isObject({hello: 'world'});
+//=> true
+
+isObject('hello');
+//=> false
+```
+
+@category Type
 @category Object
 */
 export type UnknownRecord = Record<PropertyKey, unknown>;
