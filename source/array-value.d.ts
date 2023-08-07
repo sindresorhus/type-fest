c43919763293b144e93ecf1c28ce2af253d89694
Michal Struck <michal.struck@lipsia.digital>
2023-08-07 09:47:08 +0200

    docs

--- source/array-value.d.ts
+++ source/array-value.d.ts
@@ -1,7 +1,10 @@
 import type {IterableElement} from './iterable-element';
 /**
  * Get a union of all the possible array/tuple values, with improved readability.
+ *
  * This type is useful when you want to extract all the possible element types of an array.
+ *
+ * This type is a narrowed down use case of {@link IterableElement}.
  *
  * It is advised that one uses other, built in language methods where applicable, e.g:
  * ```ts
@@ -15,11 +18,17 @@ import type {IterableElement} from './iterable-element';
  *
  * @example
  * ```ts
+ * import type {IterableElement} from 'type-fest';
+ *
  * const foo: Array<(string | boolean)> = [];
  * type ValueFoo = ArrayValue<typeof foo>; // => string | boolean
+ * ```
+ * @example
+ * ```ts
+ * import type {IterableElement} from 'type-fest';
  *
  * type Bar = {dataPoint: number, timestamp: number}[];
- * type ValueBar = ArrayValue< Bar>; // => {dataPoint: number, timestamp: number}
+ * type ValueBar = ArrayValue<Bar>; // => {dataPoint: number, timestamp: number}
  *```
  *
  */
