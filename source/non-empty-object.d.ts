7f0ab1ba70e2f9586905a49d9bb3ccab7982cdc9
Sindre Sorhus <sindresorhus@gmail.com>
2023-08-24 12:30:33 +0200

    Update non-empty-object.d.ts

--- source/non-empty-object.d.ts
+++ source/non-empty-object.d.ts
@@ -4,7 +4,7 @@ import type {RequireAtLeastOne} from './require-at-least-one';
 /**
 Represents an object with at least 1 non-optional key.
 
-This is useful where you need an object where all keys are optional, but there must be at least 1 key.
+This is useful when you need an object where all keys are optional, but there must be at least 1 key.
 
 @example
 ```
@@ -23,7 +23,7 @@ const update1: UpdateRequest<User> = {
 	surname: 'Acme',
 };
 
-// at least 1 key is required, therefore this will report a 2322 error
+// At least 1 key is required, therefore this will report a 2322 error:
 // Type '{}' is not assignable to type 'UpdateRequest<User>'
 // you can use "IsEmptyObject" to check if a object is empty
 const update2: UpdateRequest<User> = {};
