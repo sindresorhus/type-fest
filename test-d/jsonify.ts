025f6e9db5db7ae3ee5697595fb7eb0fce1e3998
Emiya <2914034404@qq.com>
2023-09-30 22:32:23 +0800

    `Jsonify`: Fix handling of arrays (#673)

--- test-d/jsonify.ts
+++ test-d/jsonify.ts
@@ -222,6 +222,9 @@ expectType<[string, string]>(tupleJson);
 declare const tupleRestJson: Jsonify<[string, ...Date[]]>;
 expectType<[string, ...string[]]>(tupleRestJson);
 
+declare const mixTupleJson: Jsonify<['1', (_: any) => void, 2]>;
+expectType<['1', null, 2]>(mixTupleJson);
+
 declare const tupleStringJson: Jsonify<string[] & ['some value']>;
 expectType<['some value']>(tupleStringJson);
 
@@ -334,10 +337,14 @@ declare const nestedObjectWithNameProperty: {
 		name: string;
 	};
 };
-declare const jsonifiedNestedObjectWithNameProperty: Jsonify<typeof nestedObjectWithNameProperty>;
+declare const jsonifiedNestedObjectWithNameProperty: Jsonify<
+	typeof nestedObjectWithNameProperty
+>;
 
-expectType<typeof nestedObjectWithNameProperty>(jsonifiedNestedObjectWithNameProperty);
+expectType<typeof nestedObjectWithNameProperty>(
+	jsonifiedNestedObjectWithNameProperty,
+);
 
-/// #629
-// declare const readonlyTuple: Jsonify<readonly [1, 2, 3]>;
-// expectType<readonly [1, 2, 3]>(readonlyTuple);
+// Regression test for https://github.com/sindresorhus/type-fest/issues/629
+declare const readonlyTuple: Jsonify<readonly [1, 2, 3]>;
+expectType<[1, 2, 3]>(readonlyTuple);
