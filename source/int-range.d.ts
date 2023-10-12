659fb360a7319104dafa25d0d63079c7d6e30d31
Emiya <emiya@emiya.com.cn>
2023-10-12 22:19:43 +0800

    feat: add `intRange` type

---
+++ source/int-range.d.ts
@@ -0,0 +1,39 @@
+import {type IsNever} from './is-never';
+import type {BuildTuple, Subtract} from './internal';
+
+/**
+Return the result of `A >= B`
+*/
+type EqualOrMoreThan<A extends number, B extends number> = IsNever<Subtract<A, B>> extends true ? false : true;
+
+/**
+Create a union of numbers from `Start` (inclusive) to `End` (exclusive). Can skip numbers using `Step`.
+
+Use-case: Easier to create union types of consecutive numbers.
+
+@example
+```
+import type {IntRange} from 'type-fest';
+
+// create union type `0 | 1 | ... | 9`
+type ZeroToNine = IntRange<0, 10>;
+
+// create union type `100 | 200 | 300 | ... | 900`
+type Hundreds = IntRange<100, 901, 100>;
+```
+*/
+export type IntRange<Start extends number, End extends number, Step extends number = 1> = PrivateIntRange<Start, End, Step>;
+
+/**
+The actual implementation of `IntRange`. It's private because it has some argument that don't need to be exposed.
+*/
+type PrivateIntRange<
+	Start extends number,
+	End extends number,
+	Step extends number,
+	Gap extends number = Subtract<Step, 1>, // The gap between each number, gap = step - 1
+	List extends unknown[] = BuildTuple<Start, never>, // The final `List` is [...StartLengthTuple, ...[number, ...GapLengthTuple], ...[number, ...GapLengthTuple], ... ...], So can initialize the `List` with [...StartLengthTuple]
+> = EqualOrMoreThan<List['length'], End> extends true
+	? Exclude<List[number], never> // All unused elements are `never`, so exclude them
+	: PrivateIntRange<Start, End, Step, Gap, [...List, List['length'], ...BuildTuple<Gap, never>]>;
+
