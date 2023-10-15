9772af243d385fca80f5b5e3ab8dd8e098a23c22
Henrique Yuji Rossetti Inonhe <henriqueinonhe@gmail.com>
2023-10-15 14:01:46 -0300

    Add DistributedOmit type

---
+++ source/distributed-omit.d.ts
@@ -0,0 +1,80 @@
+import type {AllKeys} from './all-keys';
+
+/**
+Omits keys from a type, distributing the operation over a union.
+
+Typescript's `Omit` doesn't distribute over unions, which causes the following situation:
+
+```ts
+type A = {
+  discriminant: "A";
+  foo: string;
+  a: number;
+};
+
+type B = {
+  discriminant: "B";
+  foo: string;
+  b: string;
+};
+
+type Union = A | B;
+
+type OmittedUnion = Omit<Union, "foo">; //==> { discriminant: "A" | "B" }
+
+const omittedUnion: OmittedUnion = createOmittedUnion();
+
+if(omittedUnion.discriminant === "A") {
+  // We would like to narrow `omittedUnion`'s type
+  // to `A` here, but we can't because `Omit`
+  // doesn't distribute over unions.
+
+  omittedUnion.a; //==> Error, `a` is not a property of `{ discriminant: "A" | "B" }`
+}
+```
+
+While `Except` solves this problem, it restricts the keys you can omit to the ones that are present in **ALL** union members, where `DistributedOmit` allows you to omit keys that are present in **ANY** union member.
+
+@example
+```ts
+type A = {
+  discriminant: "A";
+  foo: string;
+  a: number;
+};
+
+type B = {
+  discriminant: "B";
+  foo: string;
+  bar: string;
+  b: string;
+};
+
+type C = {
+  discriminant: "C";
+  bar: string;
+  c: boolean;
+};
+
+// Notice that `foo` exists in `A` and `B`, but not in `C`, and
+// `bar` exists in `B` and `C`, but not in `A`.
+
+type Union = A | B | C;
+
+type OmittedUnion = DistributedOmit<Union, "foo" | "bar">;
+
+const omittedUnion: OmittedUnion = createOmittedUnion();
+
+if(omittedUnion.discriminant === "A") {
+  omittedUnion.a; //==> OK
+  omittedUnion.foo; //==> Error, `foo` is not a property of `{ discriminant: "A"; a: string; }`
+  omittedUnion.bar; //==> Error, `bar` is not a property of `{ discriminant: "A"; a: string; }`
+}
+```
+
+@category Object
+*/
+export type DistributedOmit<ObjectType, KeyType extends AllKeys<ObjectType>> =
+	ObjectType extends unknown
+		? Omit<ObjectType, KeyType>
+		: never;
