9c73db0929a452539bfa10f483e0bb10b7bfad25
Sindre Sorhus <sindresorhus@gmail.com>
2023-10-18 02:25:07 +0700

    Update int-range.d.ts

--- source/int-range.d.ts
+++ source/int-range.d.ts
@@ -1,20 +1,22 @@
 import type {BuildTuple, Subtract} from './internal';
 
 /**
-Create a union of numbers from `Start` (inclusive) to `End` (exclusive).
+Generate a union of numbers.
 
-Can skip numbers using `Step`, defaulting to `1`. for example, `IntRange<0, 10, 2>` will create a union of `0 | 2 | 4 | 6 | 8`.
+The numbers are created from the given `Start` (inclusive) parameter to the given `End` (exclusive) parameter.
 
-Note: `Start` or `End` must smaller than `1000`
+You skip over numbers using the `Step` parameter (defaults to `1`). For example, `IntRange<0, 10, 2>` will create a union of `0 | 2 | 4 | 6 | 8`.
 
-Use-case:
-	1. This can be used to define a set of valid input/output values. for example:
-		```
-			type Age = IntRange<0, 120>;
-			type FontSize = IntRange<10, 20>;
-			type EvenNumber = IntRange<0, 11, 2>; // 0 | 2 | 4 | 6 | 8 | 10
-		```
-	2. This can be used to define random numbers in a range. for example, `type RandomNumber = IntRange<0, 100>;`
+Note: `Start` or `End` must smaller than `1000`.
+
+Use-cases:
+1. This can be used to define a set of valid input/output values. for example:
+	```
+	type Age = IntRange<0, 120>;
+	type FontSize = IntRange<10, 20>;
+	type EvenNumber = IntRange<0, 11, 2>; //=> 0 | 2 | 4 | 6 | 8 | 10
+	```
+2. This can be used to define random numbers in a range. For example, `type RandomNumber = IntRange<0, 100>;`
 
 @example
 ```
@@ -30,14 +32,14 @@ type Hundreds = IntRange<100, 901, 100>;
 export type IntRange<Start extends number, End extends number, Step extends number = 1> = PrivateIntRange<Start, End, Step>;
 
 /**
-The actual implementation of `IntRange`. It's private because it has some argument that don't need to be exposed.
+The actual implementation of `IntRange`. It's private because it has some arguments that don't need to be exposed.
 */
 type PrivateIntRange<
 	Start extends number,
 	End extends number,
 	Step extends number,
 	Gap extends number = Subtract<Step, 1>, // The gap between each number, gap = step - 1
-	List extends unknown[] = BuildTuple<Start, never>, // The final `List` is [...StartLengthTuple, ...[number, ...GapLengthTuple], ...[number, ...GapLengthTuple], ... ...], So can initialize the `List` with [...StartLengthTuple]
+	List extends unknown[] = BuildTuple<Start, never>, // The final `List` is `[...StartLengthTuple, ...[number, ...GapLengthTuple], ...[number, ...GapLengthTuple], ... ...]`, so can initialize the `List` with `[...StartLengthTuple]`
 	EndLengthTuple extends unknown[] = BuildTuple<End>,
 > = Gap extends 0 ?
 	// Handle the case that without `Step`
