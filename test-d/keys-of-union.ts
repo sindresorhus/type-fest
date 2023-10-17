cc47bbb0a5d1057803fc0c49ac4322ba5be7d161
Sindre Sorhus <sindresorhus@gmail.com>
2023-10-18 02:40:45 +0700

    Update keys-of-union.ts

--- test-d/keys-of-union.ts
+++ test-d/keys-of-union.ts
@@ -1,8 +1,7 @@
 import {expectType} from 'tsd';
 import type {KeysOfUnion} from '../index';
 
-// When passing types that are not unions
-// It behaves exactly as the `keyof` operator
+// When passing types that are not unions, it behaves exactly as the `keyof` operator.
 
 type Example1 = {
 	string: string;
@@ -18,8 +17,7 @@ declare const actual1: KeysOfUnion<Example1>;
 
 expectType<Expected1>(actual1);
 
-// When passing a type that is a union
-// It returns a union of all keys of all union members
+// When passing a type that is a union, it returns a union of all keys of all union members.
 
 type Example2 = {
 	common: string;
