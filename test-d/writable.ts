4dcc2769d3916ada6ef34d2e93b42c095b8dad24
Emiya <emiya@emiya.com.cn>
2023-10-26 15:51:38 +0800

    feat: support map and set

--- test-d/writable.ts
+++ test-d/writable.ts
@@ -40,3 +40,11 @@ declare const variation6: Writable<readonly [...string[], number]>;
 expectType<[...string[], number]>(variation6);
 declare const variation7: Writable<readonly [string, ...number[]]>;
 expectType<[string, ...number[]]>(variation7);
+
+// Test readonly set
+declare const variation9: Writable<ReadonlySet<string>>;
+expectType<Set<string>>(variation9);
+
+// Test readonly map
+declare const variation10: Writable<ReadonlyMap<string, number>>;
+expectType<Map<string, number>>(variation10);
