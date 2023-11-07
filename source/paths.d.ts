e2b8531ea21e93b5ccb0063ada25da037fdef43b
Emiya <emiya@emiya.com.cn>
2023-11-07 19:58:05 +0800

    support spread tuple

--- source/paths.d.ts
+++ source/paths.d.ts
@@ -1,11 +1,45 @@
 import type {EmptyObject} from './empty-object';
+import type {IsAny} from './is-any';
+import type {IsNever} from './is-never';
 import type {UnknownArray} from './unknown-array';
 import type {UnknownRecord} from './unknown-record';
 
-type ToString<T> = T extends string | number ? `${T}` : '';
+type ToString<T> = T extends string | number ? `${T}` : never;
 
 /**
-Generate a union of all the possible paths in given object.
+Return the part of the given array with fixed index
+
+@example
+```
+type A = [string, number, boolean, ...string[]];
+type B = FilterFixedIndexArray<A>;
+//=> [string, number, boolean]
+```
+*/
+type FilterFixedIndexArray<T extends UnknownArray, Result extends UnknownArray = []> =
+	number extends T['length'] ?
+		T extends readonly [infer U, ...infer V]
+			? FilterFixedIndexArray<V, [...Result, U]>
+			: Result
+		: T;
+
+/**
+Return the part of the given array with not fixed index
+
+@example
+```
+type A = [string, number, boolean, ...string[]];
+type B = FilterNotFixedIndexArray<A>;
+//=> string[]
+```
+*/
+type FilterNotFixedIndexArray<T extends UnknownArray> =
+T extends readonly [...FilterFixedIndexArray<T>, ...infer U]
+	? U
+	: [];
+
+/**
+Generate a union of all possible paths to properties in the given object. Also works with arrays.
 
 Use-case: You want a type-safe way to access deeply nested properties in an object.
 
@@ -14,39 +48,58 @@ Use-case: You want a type-safe way to access deeply nested properties in an obje
 import type {Paths} from 'type-fest';
 
 type Project = {
-	fileName: string;
+	filename: string;
+	filelist1: string[];
+	filelist2: [{filename: string}];
 	folder: {
 		subfolder: {
-			fileName: string;
+			filename: string;
 		};
 	};
 };
 
 type ProjectPaths = Paths<Project>;
-//=> "fileName" | "folder" | "folder.subfolder" | "folder.subfolder.fileName"
+//=> "filename" | "filelist" | "filelist2" | "folder" | `filelist1.${number}` | "filelist2.0" | "filelist2.0.filename" | "folder.subfolder" | "folder.subfolder.filename"
 
 declare function open<Path extends ProjectPaths>(path: Path): void;
 
-open('fileName'); // Pass
+open('filename'); // Pass
 open('folder.subfolder'); // Pass
-open('folder.subfolder.fileName'); // Pass
+open('folder.subfolder.filename'); // Pass
 open('foo'); // TypeError
+
+// Also works with arrays
+open('filelist1.1'); // Pass
+open('filelist2.0'); // Pass
+open('filelist2.1'); // TypeError. Because filelist2 only has one element
 ```
 
 @category Object
 */
-export type Paths<T extends UnknownRecord | UnknownArray, RecordType = Required<T>> =
-RecordType extends EmptyObject | []
-	? ''
-	: {
-		[Key in keyof RecordType]: RecordType[Key] extends UnknownRecord | UnknownArray
-			? Key extends string | number // Handle TS Error: Type instantiation is excessively deep and possibly infinite.
-				?
-				(
-					`${Paths<RecordType[Key]>}` extends ''
-						? ToString<Key>
-						: ToString<Key> | `${ToString<Key>}.${Paths<RecordType[Key]>}`
-				)
-				: ToString<Key>
-			: ToString<Key>
-	}[keyof RecordType & (RecordType extends UnknownArray ? number : unknown)];
+export type Paths<T extends UnknownRecord | UnknownArray> =
+	IsAny<T> extends true
+		? never
+		: T extends UnknownArray
+			? number extends T['length']
+				// Need handle the fixed index part of array and not fixed index part of array separately
+				? InternalPaths<FilterFixedIndexArray<T>>
+				| InternalPaths<Array<FilterNotFixedIndexArray<T>[number]>>
+				: InternalPaths<T>
+			: InternalPaths<T>;
+
+export type InternalPaths<_T extends UnknownRecord | UnknownArray, T = Required<_T>> =
+	T extends EmptyObject | readonly []
+		? never
+		: {
+			[Key in keyof T]:
+			Key extends string | number // Limit `Key` to string or number
+				? T[Key] extends UnknownRecord | UnknownArray
+					? (
+						IsNever<Paths<T[Key]>> extends false
+						// If `Key` is number, return `Key | `${Key}``. Because both `array[0]` and `array['0']` are work.
+							? Key | ToString<Key> | `${Key}.${Paths<T[Key]>}`
+							: Key | ToString<Key>
+					)
+					: Key | ToString<Key>
+				: never
+		}[keyof T & (T extends UnknownArray ? number : unknown)];
--- test-d/paths.ts
+++ test-d/paths.ts
@@ -19,13 +19,13 @@ declare const deepObject: Paths<DeepObject>;
 expectType<'a' | 'a.b' | 'a.b2' | 'a.b3' | 'a.b.c' | 'a.b.c.d' | `a.b2.${number}`>(deepObject);
 
 declare const emptyObject: Paths<{}>;
-expectType<''>(emptyObject);
+expectType<never>(emptyObject);
 
 declare const emptyArray: Paths<[]>;
-expectType<''>(emptyArray);
+expectType<never>(emptyArray);
 
 declare const symbol: Paths<{[Symbol.iterator]: string}>;
-expectType<''>(symbol);
+expectType<never>(symbol);
 
 declare const never: Paths<never>;
 expectType<never>(never);
@@ -36,8 +36,8 @@ expectType<'foo'>(date);
 declare const mixed: Paths<{foo: boolean} | {bar: string}>;
 expectType<'foo' | 'bar'>(mixed);
 
-declare const list: Paths<Array<{foo: string}>>;
-expectType<`${number}` | `${number}.foo`>(list);
+declare const array: Paths<Array<{foo: string}>>;
+expectType<number | `${number}` | `${number}.foo`>(array);
 
 declare const tuple: Paths<[{foo: string}]>;
 expectType<'0' | '0.foo'>(tuple);
@@ -56,3 +56,20 @@ expectType<'foo' | 'foo.bar'>(optional);
 
 declare const record: Paths<Record<'a', any>>;
 expectType<'a'>(record);
+
+declare const record2: Paths<Record<1, unknown>>;
+expectType<1 | '1'>(record2);
+
+// Test for unknown length array
+declare const trailingSpreadTuple: Paths<[{a: string}, ...Array<{b: number}>]>;
+expectType<number | `${number}` | '0.a' | `${number}.b`>(trailingSpreadTuple);
+
+declare const trailingSpreadTuple1: Paths<[{a: string}, {b: number}, ...Array<{c: number}>]>;
+expectType<number | `${number}` | '0.a' | `${number}.b`>(trailingSpreadTuple);
+expectType<number | `${number}` | '0.a' | '1.b' | `${number}.c`>(trailingSpreadTuple1);
+
+declare const leadingSpreadTuple: Paths<[...Array<{a: string}>, {b: number}]>;
+expectType<number | `${number}` | `${number}.b` | `${number}.a`>(leadingSpreadTuple);
+
+declare const leadingSpreadTuple1: Paths<[...Array<{a: string}>, {b: number}, {c: number}]>;
+expectType<number | `${number}` | `${number}.b` | `${number}.c` | `${number}.a`>(leadingSpreadTuple1);
