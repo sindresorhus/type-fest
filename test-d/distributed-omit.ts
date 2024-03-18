bc4957763a49212339a6c5bbd13f3115d658fda7
Henrique Inonhe <henriqueinonhe@gmail.com>
2024-03-18 11:31:08 -0300

    Add `DistributedOmit` type (#820)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

---
+++ test-d/distributed-omit.ts
@@ -0,0 +1,77 @@
+import {expectType, expectError} from 'tsd';
+import type {DistributedOmit, Except} from '../index';
+
+// When passing a non-union type, and
+// omitting keys that are present in the type.
+// It behaves exactly like `Except`.
+
+type Example1 = {
+	a: number;
+	b: string;
+};
+
+type Actual1 = DistributedOmit<Example1, 'a'>;
+type Actual2 = DistributedOmit<Example1, 'b'>;
+type Actual3 = DistributedOmit<Example1, 'a' | 'b'>;
+
+type Expected1 = Except<Example1, 'a'>;
+type Expected2 = Except<Example1, 'b'>;
+type Expected3 = Except<Example1, 'a' | 'b'>;
+
+declare const expected1: Expected1;
+declare const expected2: Expected2;
+declare const expected3: Expected3;
+
+expectType<Actual1>(expected1);
+expectType<Actual2>(expected2);
+expectType<Actual3>(expected3);
+
+// When passing a non-union type, and
+// omitting keys that are NOT present in the type.
+// It behaves exactly like `Except`, by not letting you
+// omit keys that are not present in the type.
+
+type Example2 = {
+	a: number;
+	b: string;
+};
+
+expectError(() => {
+	type Actual4 = DistributedOmit<Example2, 'c'>;
+});
+
+// When passing a union type, and
+// omitting keys that are present in some union members.
+// It lets you omit keys that are present in some union members,
+// and distributes over the union.
+
+type A = {
+	discriminant: 'A';
+	foo: string;
+	a: number;
+};
+
+type B = {
+	discriminant: 'B';
+	foo: string;
+	bar: string;
+	b: string;
+};
+
+type C = {
+	discriminant: 'C';
+	bar: string;
+	c: boolean;
+};
+
+type Union = A | B | C;
+
+type OmittedUnion = DistributedOmit<Union, 'foo' | 'bar'>;
+
+declare const omittedUnion: OmittedUnion;
+
+if (omittedUnion.discriminant === 'A') {
+	expectType<{discriminant: 'A'; a: number}>(omittedUnion);
+	expectError(omittedUnion.foo);
+	expectError(omittedUnion.bar);
+}
