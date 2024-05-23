44b5104aea896b1d6e611ad1026d4b27e2e151e1
rory <rory@expensify.com>
2024-05-23 13:09:40 -0700

    Fix indentation

--- source/conditional-keys.d.ts
+++ source/conditional-keys.d.ts
@@ -36,11 +36,11 @@ export type ConditionalKeys<Base, Condition> =
 {
 	// Map through all the keys of the given base type.
 	[Key in keyof Base]-?:
-		// Pick only keys with types extending the given `Condition` type.
-		Base[Key] extends Condition
-			// If the value for the key extends never, only include it if `Condition` also extends never
-			? IfNever<Base[Key], IfNever<Condition, Key, never>, Key>
-			// Discard this key since the condition fails.
-			: never;
+	// Pick only keys with types extending the given `Condition` type.
+	Base[Key] extends Condition
+	// If the value for the key extends never, only include it if `Condition` also extends never
+		? IfNever<Base[Key], IfNever<Condition, Key, never>, Key>
+	// Discard this key since the condition fails.
+		: never;
 	// Convert the produced object into a union type of the keys which passed the conditional test.
 }[keyof Base];
