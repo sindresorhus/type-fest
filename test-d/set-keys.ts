11e4dff983c170de09cef17d01534b35ae4689cb
xeho91 <xeho91@pm.me>
2024-06-20 13:28:11 +0800

    fix tests - use `expectAssignable` instead of `expectType`

--- test-d/set-keys.ts
+++ test-d/set-keys.ts
@@ -1,24 +1,24 @@
-import {expectType} from 'tsd';
+import {expectAssignable} from 'tsd';
 import type {SetKeys} from '../source/set-keys';
 
 const fruits = new Set(['🍎', '🍌', '🍉'] as const);
 
 type Fruit = SetKeys<typeof fruits>;
 
-expectType<Fruit>('🍎');
-expectType<Fruit>('🍌');
-expectType<Fruit>('🍉');
+expectAssignable<Fruit>('🍎');
+expectAssignable<Fruit>('🍌');
+expectAssignable<Fruit>('🍉');
 
 type VegetableSet = Set<'🥦' | '🥕' | '🌶'>;
 type Vegetable = SetKeys<VegetableSet>;
 
-expectType<Vegetable>('🥦');
-expectType<Vegetable>('🥕');
-expectType<Vegetable>('🌶');
+expectAssignable<Vegetable>('🥦');
+expectAssignable<Vegetable>('🥕');
+expectAssignable<Vegetable>('🌶');
 
 type UserRolesSet = ReadonlySet<'regular' | 'contributor' | 'maintainer'>;
 type UserRole = SetKeys<UserRolesSet>;
 
-expectType<UserRole>('regular');
-expectType<UserRole>('contributor');
-expectType<UserRole>('maintainer');
+expectAssignable<UserRole>('regular');
+expectAssignable<UserRole>('contributor');
+expectAssignable<UserRole>('maintainer');
