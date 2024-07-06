24409bae25581df1a0486796153d945eaaf35110
tommy-mitchell <tmitchell7@uh.edu>
2024-07-06 11:53:34 -0500

    update array to tuple

--- test-d/internal/tuple/tuple-max.ts
+++ test-d/internal/tuple/tuple-max.ts
@@ -1,9 +1,9 @@
 import {expectType} from 'tsd';
-import type {ArrayMax} from '../../source/internal';
-import type {PositiveInfinity} from '../../source/numeric';
+import type {TupleMax} from '../../../source/internal';
+import type {PositiveInfinity} from '../../../source/numeric';
 
-expectType<ArrayMax<[1, 2, 5, 3, 7, -9, -5, 0]>>(7);
-expectType<ArrayMax<[1, 2, 5, 3, 7, -9, -5, 0, PositiveInfinity]>>(null! as PositiveInfinity);
-expectType<ArrayMax<[1, 1, 1, 1, 1, 1]>>(1);
-expectType<ArrayMax<[-1, -2, -5]>>(-1);
-expectType<ArrayMax<[10, 2]>>(10);
+expectType<TupleMax<[1, 2, 5, 3, 7, -9, -5, 0]>>(7);
+expectType<TupleMax<[1, 2, 5, 3, 7, -9, -5, 0, PositiveInfinity]>>(null! as PositiveInfinity);
+expectType<TupleMax<[1, 1, 1, 1, 1, 1]>>(1);
+expectType<TupleMax<[-1, -2, -5]>>(-1);
+expectType<TupleMax<[10, 2]>>(10);
--- test-d/internal/tuple/tuple-min.ts
+++ test-d/internal/tuple/tuple-min.ts
@@ -1,11 +1,11 @@
 import {expectType} from 'tsd';
-import type {ArrayMin} from '../../source/internal';
-import type {NegativeInfinity, PositiveInfinity} from '../../source/numeric';
+import type {TupleMin} from '../../../source/internal';
+import type {NegativeInfinity, PositiveInfinity} from '../../../source/numeric';
 
 declare const never: never;
 
-expectType<ArrayMin<[1, 2, 5, 3, 7, -9, -5, 0]>>(-9);
-expectType<ArrayMin<[1, 2, 5, 3, 7, -9, -5, 0, PositiveInfinity, NegativeInfinity]>>(null! as NegativeInfinity);
-expectType<ArrayMin<[1, 1, 1, 1, 1, 1]>>(1);
-expectType<ArrayMin<[-1, -2, -5]>>(-5);
-expectType<ArrayMin<[-1, -2, number, -5]>>(never);
+expectType<TupleMin<[1, 2, 5, 3, 7, -9, -5, 0]>>(-9);
+expectType<TupleMin<[1, 2, 5, 3, 7, -9, -5, 0, PositiveInfinity, NegativeInfinity]>>(null! as NegativeInfinity);
+expectType<TupleMin<[1, 1, 1, 1, 1, 1]>>(1);
+expectType<TupleMin<[-1, -2, -5]>>(-5);
+expectType<TupleMin<[-1, -2, number, -5]>>(never);
