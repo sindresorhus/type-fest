04169910c4316f57237b6466f44036a796f2a21e
tommy-mitchell <tmitchell7@uh.edu>
2024-07-06 11:47:37 -0500

    reorg internal types, remove extraneous imports

--- source/get.d.ts
+++ source/get.d.ts
@@ -1,4 +1,4 @@
-import type {StringDigit} from '../source/internal';
+import type {StringDigit} from './internal';
 import type {Split} from './split';
 import type {StringKeyOf} from './string-key-of';
 
