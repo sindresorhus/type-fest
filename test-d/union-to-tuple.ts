1f4f7a10aef7e3c376da1540e4e59b443f6aea69
Haozheng Li <emiya@emiya.com.cn>
2024-08-29 01:03:49 +0800

    Add `UnionToTuple` type (#945)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

---
+++ test-d/union-to-tuple.ts
@@ -0,0 +1,13 @@
+import {expectAssignable, expectError, expectType} from 'tsd';
+import type {UnionToTuple} from '../index';
+
+type Options = UnionToTuple<'a' | 'b' | 'c'>;
+// Results unordered
+expectAssignable<['a', 'b', 'c'] | ['a', 'c', 'b'] | ['b', 'a', 'c'] | ['b', 'c', 'a'] | ['c', 'a', 'b'] | ['c', 'b', 'a']>({} as Options);
+expectType<Options[number]>({} as ('a' | 'b' | 'c'));
+
+type Options1 = UnionToTuple<1 | 2 | 3>;
+expectType<Options1[number]>({} as (1 | 2 | 3));
+
+type Options2 = UnionToTuple<boolean | 1>;
+expectType<Options2[number]>({} as (1 | false | true));
