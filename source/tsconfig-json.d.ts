aa70cfdfc06447349ddef069eeb0ac836ef97a4d
ntnyq <ntnyq13@gmail.com>
2024-11-24 16:10:12 +0800

    Update source/tsconfig-json.d.ts
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>

--- source/tsconfig-json.d.ts
+++ source/tsconfig-json.d.ts
@@ -940,7 +940,7 @@ declare namespace TsConfigJson {
 		checkJs?: boolean;
 
 		/**
-		Built-in iterators are instantiated with a `TReturn` type of undefined instead of `any`
+		Built-in iterators are instantiated with a `TReturn` type of undefined instead of `any`.
 
 		@default false
 		*/
