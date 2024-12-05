d21c5dbd61349a1634d20547913f5894732f534d
Sindre Sorhus <sindresorhus@gmail.com>
2024-12-05 17:13:31 +0100

    Update union-to-tuple.d.ts

--- source/union-to-tuple.d.ts
+++ source/union-to-tuple.d.ts
@@ -18,8 +18,7 @@ UnionToIntersection<T extends any ? () => T : never> extends () => (infer R)
 /**
 Convert a union type into an unordered tuple type of its elements.
 
-"Unordered" means that the elements of the tuple aren't guaranteed to be in the order as in the union type, and the array will
-be arranged randomly, and this order could change any time.
+"Unordered" means the elements of the tuple are not guaranteed to be in the same order as in the union type. The arrangement can appear random and may change at any time.
 
 This can be useful when you have objects with a finite set of keys and want a type defining only the allowed keys, but do not want to repeat yourself.
 
