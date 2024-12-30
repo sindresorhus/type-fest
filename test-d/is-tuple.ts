fc451bfe0dae2e3e131a695c9e469b2641626ad8
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2024-12-30 21:42:31 +0530

    test: replace literals

--- test-d/is-tuple.ts
+++ test-d/is-tuple.ts
@@ -3,28 +3,28 @@ import type {IsTuple} from '../index';
 
 // Tuples
 expectType<IsTuple<[]>>(true);
-expectType<IsTuple<[1]>>(true);
-expectType<IsTuple<[1, 2]>>(true);
-expectType<IsTuple<[1, 2, ...number[]]>>(true);
-expectType<IsTuple<[1?]>>(true);
-expectType<IsTuple<[1?, 2?]>>(true);
-expectType<IsTuple<[1?, 2?, ...number[]]>>(true);
+expectType<IsTuple<[number]>>(true);
+expectType<IsTuple<[number, string]>>(true);
+expectType<IsTuple<[number, string, ...number[]]>>(true);
+expectType<IsTuple<[number?]>>(true);
+expectType<IsTuple<[number?, string?]>>(true);
+expectType<IsTuple<[number?, string?, ...number[]]>>(true);
 expectType<IsTuple<[never]>>(true);
 
 // Readonly tuples
 expectType<IsTuple<readonly []>>(true);
-expectType<IsTuple<readonly [1]>>(true);
-expectType<IsTuple<readonly [1, 2, ...number[]]>>(true);
-expectType<IsTuple<readonly [1?, 2?, ...number[]]>>(true);
-expectType<IsTuple<readonly [1?]>>(true);
+expectType<IsTuple<readonly [number]>>(true);
+expectType<IsTuple<readonly [number, string, ...number[]]>>(true);
+expectType<IsTuple<readonly [number?, string?, ...number[]]>>(true);
+expectType<IsTuple<readonly [number?]>>(true);
 expectType<IsTuple<readonly [never]>>(true);
 
 // Non-tuples
 expectType<IsTuple<number[]>>(false);
 expectType<IsTuple<readonly number[]>>(false);
 expectType<IsTuple<[...number[]]>>(false);
-expectType<IsTuple<[1, 2, ...number[]], {fixedLengthOnly: true}>>(false);
-expectType<IsTuple<readonly [1?, 2?, ...number[]], {fixedLengthOnly: true}>>(false);
+expectType<IsTuple<[number, string, ...number[]], {fixedLengthOnly: true}>>(false);
+expectType<IsTuple<readonly [number?, string?, ...number[]], {fixedLengthOnly: true}>>(false);
 expectType<IsTuple<never[]>>(false);
 expectType<IsTuple<any[]>>(false);
 
@@ -34,11 +34,11 @@ expectType<IsTuple<any, {fixedLengthOnly: true}>>({} as boolean);
 expectType<IsTuple<never>>(false);
 
 // Unions
-expectType<IsTuple<[1] | [1, 2, 3]>>(true);
-expectType<IsTuple<[1?, 2?] | [] | [1, 2, ...number[]]>>(true);
-expectType<IsTuple<[1?, 2?] | [] | [1, 2, ...number[]], {fixedLengthOnly: true}>>({} as boolean);
+expectType<IsTuple<[number] | [number, string, boolean]>>(true);
+expectType<IsTuple<[number?, string?] | [] | [number, string, ...number[]]>>(true);
+expectType<IsTuple<[number?, string?] | [] | [number, string, ...number[]], {fixedLengthOnly: true}>>({} as boolean);
 expectType<IsTuple<number[] | string[]>>(false);
-expectType<IsTuple<[1, 2] | string[]>>({} as boolean);
+expectType<IsTuple<[number, string] | string[]>>({} as boolean);
 expectType<IsTuple<[string] | [number] | number[]>>({} as boolean);
 expectType<IsTuple<[string, ...number[]] | [number?, string?]>>(true);
 expectType<IsTuple<[] | [number] | readonly [string]>>(true);
@@ -53,7 +53,7 @@ expectType<IsTuple<[string, number?]>>(true);
 expectType<IsTuple<readonly [string, number?, ...boolean[]]>>(true);
 
 // Setting `fixedLengthOnly` to `boolean` falls back to it's default value of `false`
-expectType<IsTuple<[1, 2, ...number[]], {fixedLengthOnly: boolean}>>(true);
+expectType<IsTuple<[number, string, ...number[]], {fixedLengthOnly: boolean}>>(true);
 
 // @ts-expect-error only works with arrays
 type T = IsTuple<{}>;
