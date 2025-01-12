1217970c71f185c82f9639912f9ed94e2573db58
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-01-12 13:44:39 +0530

    refactor: set-required-deep implementation

--- test-d/set-required-deep.ts
+++ test-d/set-required-deep.ts
@@ -45,18 +45,66 @@ expectType<{a: 1; b: {c: 1}}>(variation10);
 declare const variation11: SetRequiredDeep<{a: 1; b: {c?: 1} | number}, 'b.c'>;
 expectType<{a: 1; b: {c: 1} | number}>(variation11);
 
-// Preserves readonly in root keys
+// Preserves readonly
 declare const variation12: SetRequiredDeep<{a: 1; readonly b: {c?: 1}}, 'b.c'>;
 expectType<{a: 1; readonly b: {c: 1}}>(variation12);
 
+declare const variation13: SetRequiredDeep<{readonly a?: 1; readonly b?: {readonly c?: 1}}, 'a' | 'b'>;
+expectType<{readonly a: 1; readonly b: {readonly c?: 1}}>(variation13);
+
+declare const variation14: SetRequiredDeep<{readonly a?: 1; readonly b?: {readonly c?: 1}}, 'a' | 'b' | 'b.c'>;
+expectType<{readonly a: 1; readonly b: {readonly c: 1}}>(variation14);
+
 // Works with number keys
-declare const variation13: SetRequiredDeep<{0: 1; 1: {2?: string}}, '1.2'>;
-expectType<{0: 1; 1: {2: string}}>(variation13);
+declare const variation15: SetRequiredDeep<{0: 1; 1: {2?: string}}, '1.2'>;
+expectType<{0: 1; 1: {2: string}}>(variation15);
+
+declare const variation16: SetRequiredDeep<{0?: 1; 1?: {2?: string}}, 0 | 1>;
+expectType<{0: 1; 1: {2?: string}}>(variation16);
 
 // Multiple keys
-declare const variation14: SetRequiredDeep<{a?: 1; b?: {c?: 2}; d?: {e?: {f?: 2}; g?: 3}}, 'a' | 'b' | 'b.c' | 'd.e.f' | 'd.g'>;
-expectType<{a: 1; b: {c: 2}; d?: {e?: {f: 2}; g: 3}}>(variation14);
+declare const variation17: SetRequiredDeep<{a?: 1; b?: {c?: 2}; d?: {e?: {f?: 2}; g?: 3}}, 'a' | 'b' | 'b.c' | 'd.e.f' | 'd.g'>;
+expectType<{a: 1; b: {c: 2}; d?: {e?: {f: 2}; g: 3}}>(variation17);
 
 // Index signatures
-declare const variation15: SetRequiredDeep<{[x: string]: any; a?: number; b?: {c?: number}}, 'a' | 'b.c'>;
-expectType<{[x: string]: any; a: number; b?: {c: number}}>(variation15);
+declare const variation18: SetRequiredDeep<{[x: string]: any; a?: number; b?: {c?: number}}, 'a' | 'b.c'>;
+expectType<{[x: string]: any; a: number; b?: {c: number}}>(variation18);
+
+// Preserves union in nested keys
+declare const variation19: SetRequiredDeep<{a: 1; b?: {c?: 1} | number}, 'b'>;
+expectType<{a: 1; b: {c?: 1} | number}>(variation19);
+
+declare const variation20: SetRequiredDeep<{a?: number; b?: {c?: number} | {d?: string}}, 'b' | 'b.d'>;
+expectType<{a?: number; b: {c?: number} | {d: string}}>(variation20);
+
+// Works with number keys containing dots
+// NOTE: Passing "1.2" instead of 1.2 will treat it as a path instead of a key
+declare const variation21: SetRequiredDeep<{1.2?: string; 1?: {2?: string}}, 1.2>;
+expectType<{1.2: string; 1?: {2?: string}}>(variation21);
+
+declare const variation22: SetRequiredDeep<{1.2?: string; 1?: {2?: string}}, '1.2'>;
+expectType<{1.2?: string; 1?: {2: string}}>(variation22);
+
+declare const variation23: SetRequiredDeep<{1.2?: string; 1?: {2?: string}}, 1.2 | '1.2'>;
+expectType<{1.2: string; 1?: {2: string}}>(variation23);
+
+// Works with unions
+declare const variation24: SetRequiredDeep<{a?: {readonly b?: number}} | {readonly b?: {c?: number[]}}, 'a.b' | 'b' | 'b.c'>;
+expectType<{a?: {readonly b: number}} | {readonly b: {c: number[]}}>(variation24);
+
+// Works with `KeyPaths` containing template literals
+declare const variation25: SetRequiredDeep<{a?: number; b?: {c?: number} | {d?: number}}, `b.${'c' | 'd'}`>;
+expectType<{a?: number; b?: {c: number} | {d: number}}>(variation25);
+
+declare const variation26: SetRequiredDeep<
+{a?: number; b?: {readonly c?: {1?: number}} | {d?: {1?: number}}}, 'a' | `b.${'c' | 'd'}.1`
+>;
+expectType<{a: number; b?: {readonly c?: {1: number}} | {d?: {1: number}}}>(variation26);
+
+// Calls `RequiredDeep` when `KeyPaths` is `any`
+declare const variation27: SetRequiredDeep<{a?: number; readonly b?: {c?: string}}, any>;
+expectType<{a: number; readonly b: {c: string}}>(variation27);
+
+// Does nothing when `KeyPaths` is `never`
+declare const variation28: SetRequiredDeep<{a?: number; readonly b?: {c?: string}}, never>;
+expectType<{a?: number; readonly b?: {c?: string}}>(variation28);
