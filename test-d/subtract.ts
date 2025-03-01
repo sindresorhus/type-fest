e7cfe262602f8b8e1a24e56621587e05fbc7b1e2
Laurynas Grigutis <lgrigutis@perthera.com>
2025-03-01 19:16:47 +0200

    Added extra Subtract test cases requested in CR

--- test-d/subtract.ts
+++ test-d/subtract.ts
@@ -4,15 +4,22 @@ import type {NegativeInfinity, PositiveInfinity} from '../source/numeric';
 
 // Positive result
 expectType<12>({} as Subtract<10, -2>);
+expectType<0>({} as Subtract<0, 0>);
 expectType<0>({} as Subtract<2, 2>);
 expectType<2>({} as Subtract<-1, -3>);
+expectType<7>({} as Subtract<12, 5>);
 expectType<69>({} as Subtract<69, 0>);
+expectType<1000>({} as Subtract<999, -1>);
+expectType<1998>({} as Subtract<999, -999>);
 
 // Negative result
 expectType<-1>({} as Subtract<1, 2>);
 expectType<-6>({} as Subtract<-10, -4>);
+expectType<-14>({} as Subtract<-10, 4>);
 expectType<-333>({} as Subtract<-111, 222>);
 expectType<-420>({} as Subtract<0, 420>);
+expectType<-1000>({} as Subtract<-999, 1>);
+expectType<-1998>({} as Subtract<-999, 999>);
 
 // Infinity
 expectType<PositiveInfinity>({} as Subtract<PositiveInfinity, 999>);
@@ -34,3 +41,4 @@ expectType<number>({} as Subtract<PositiveInfinity, number>);
 expectType<9 | 8>({} as Subtract<10, 1 | 2>);
 expectType<9 | 4>({} as Subtract<10 | 5, 1>);
 expectType<9 | 8 | 4 | 3>({} as Subtract<10 | 5, 1 | 2>);
+expectType<11 | 8 | -4 | -7>({} as Subtract<10 | -5, -1 | 2>);
