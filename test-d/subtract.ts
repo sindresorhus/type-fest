42c4668c1ef4ba36285e7f5c3ae79f6c13d8ca5c
Laurynas Grigutis <lgrigutis@perthera.com>
2025-03-01 19:05:59 +0200

    Removed declare objects from Subtract tests - use '{} as X' logic for consistency with other test files

--- test-d/subtract.ts
+++ test-d/subtract.ts
@@ -2,68 +2,35 @@ import {expectType} from 'tsd';
 import type {Subtract} from '../index';
 import type {NegativeInfinity, PositiveInfinity} from '../source/numeric';
 
-declare const positive: {
-	12: Subtract<10, -2>;
-	0: Subtract<2, 2>;
-	2: Subtract<-1, -3>;
-	69: Subtract<69, 0>;
-};
 // Positive result
-expectType<12>(positive[12]);
-expectType<0>(positive[0]);
-expectType<2>(positive[2]);
-expectType<69>(positive[69]);
+expectType<12>({} as Subtract<10, -2>);
+expectType<0>({} as Subtract<2, 2>);
+expectType<2>({} as Subtract<-1, -3>);
+expectType<69>({} as Subtract<69, 0>);
 
 // Negative result
-declare const negative: {
-	1: Subtract<1, 2>;
-	6: Subtract<-10, -4>;
-	333: Subtract<-111, 222>;
-	420: Subtract<0, 420>;
-};
-expectType<-1>(negative[1]);
-expectType<-6>(negative[6]);
-expectType<-333>(negative[333]);
-expectType<-420>(negative[420]);
+expectType<-1>({} as Subtract<1, 2>);
+expectType<-6>({} as Subtract<-10, -4>);
+expectType<-333>({} as Subtract<-111, 222>);
+expectType<-420>({} as Subtract<0, 420>);
 
 // Infinity
-declare const infinity: {
-	positive1: Subtract<PositiveInfinity, 999>;
-	positive2: Subtract<-999, PositiveInfinity>;
-	negative1: Subtract<NegativeInfinity, 999>;
-	negative2: Subtract<999, NegativeInfinity>;
-	negative3: Subtract<NegativeInfinity, PositiveInfinity>;
-};
-expectType<PositiveInfinity>(infinity.positive1);
-expectType<NegativeInfinity>(infinity.positive2);
-expectType<NegativeInfinity>(infinity.negative1);
-expectType<PositiveInfinity>(infinity.negative2);
-expectType<NegativeInfinity>(infinity.negative3);
+expectType<PositiveInfinity>({} as Subtract<PositiveInfinity, 999>);
+expectType<NegativeInfinity>({} as Subtract<-999, PositiveInfinity>);
+expectType<NegativeInfinity>({} as Subtract<NegativeInfinity, 999>);
+expectType<PositiveInfinity>({} as Subtract<999, NegativeInfinity>);
+expectType<NegativeInfinity>({} as Subtract<NegativeInfinity, PositiveInfinity>);
 
 // Number
-declare const number: {
-	0: Subtract<number, 2>;
-	1: Subtract<2, number>;
-	2: Subtract<number, number>;
-	infinity1: Subtract<NegativeInfinity, NegativeInfinity>;
-	infinity2: Subtract<PositiveInfinity, PositiveInfinity>;
-	infinity3: Subtract<number, PositiveInfinity>;
-	infinity4: Subtract<PositiveInfinity, number>;
-};
-expectType<number>(number[0]);
-expectType<number>(number[1]);
-expectType<number>(number[2]);
-expectType<number>(number.infinity1);
-expectType<number>(number.infinity2);
-expectType<number>(number.infinity3);
-expectType<number>(number.infinity4);
+expectType<number>({} as Subtract<number, 2>);
+expectType<number>({} as Subtract<2, number>);
+expectType<number>({} as Subtract<number, number>);
+expectType<number>({} as Subtract<NegativeInfinity, NegativeInfinity>);
+expectType<number>({} as Subtract<PositiveInfinity, PositiveInfinity>);
+expectType<number>({} as Subtract<number, PositiveInfinity>);
+expectType<number>({} as Subtract<PositiveInfinity, number>);
 
 // Union
-declare const union: {
-	1: Subtract<10, 1 | 2>;
-	2: Subtract<10 | 5, 1>;
-	3: Subtract<10 | 5, 1 | 2>;
-};
-expectType<9 | 8>(union[1]);
-expectType<9 | 4>(union[2]);
-expectType<9 | 8 | 4 | 3>(union[3]);
+expectType<9 | 8>({} as Subtract<10, 1 | 2>);
+expectType<9 | 4>({} as Subtract<10 | 5, 1>);
+expectType<9 | 8 | 4 | 3>({} as Subtract<10 | 5, 1 | 2>);
