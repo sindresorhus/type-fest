38425db260e3bfa2d7452e3e583cd21d8bb6dbe6
Laurynas Grigutis <laurynas.grigutis@barbora.lt>
2025-03-02 16:26:56 +0200

    Added a few more Sum tests to cover negative results

--- test-d/sum.ts
+++ test-d/sum.ts
@@ -2,24 +2,46 @@ import {expectType} from 'tsd';
 import type {Sum} from '../index';
 import type {NegativeInfinity, PositiveInfinity} from '../source/numeric';
 
+// Positive result
+expectType<0>({} as Sum<-999, 999>);
+expectType<0>({} as Sum<999, -999>);
+expectType<0>({} as Sum<0, 0>);
+expectType<0>({} as Sum<2, -2>);
+expectType<0>({} as Sum<-2, 2>);
 expectType<3>({} as Sum<1, 2>);
 expectType<8>({} as Sum<10, -2>);
-expectType<0>({} as Sum<2, -2>);
+expectType<8>({} as Sum<-2, 10>);
+expectType<57>({} as Sum<-20, 77>);
+expectType<998>({} as Sum<-1, 999>);
+expectType<1000>({} as Sum<999, 1>);
+expectType<1998>({} as Sum<999, 999>);
 
+// Negative result
 expectType<-3>({} as Sum<-1, -2>);
+expectType<-8>({} as Sum<-10, 2>);
+expectType<-8>({} as Sum<2, -10>);
+expectType<-57>({} as Sum<20, -77>);
+expectType<-998>({} as Sum<-999, 1>);
+expectType<-998>({} as Sum<1, -999>);
+expectType<-1998>({} as Sum<-999, -999>);
 
+// Infinity
 expectType<PositiveInfinity>({} as Sum<PositiveInfinity, -999>);
 expectType<PositiveInfinity>({} as Sum<-999, PositiveInfinity>);
 expectType<NegativeInfinity>({} as Sum<NegativeInfinity, 999>);
 expectType<NegativeInfinity>({} as Sum<999, NegativeInfinity>);
-expectType<number>({} as Sum<NegativeInfinity, PositiveInfinity>);
 
+// Number
 expectType<number>({} as Sum<number, 1>);
 expectType<number>({} as Sum<1, number>);
 expectType<number>({} as Sum<number, number>);
 expectType<number>({} as Sum<number, PositiveInfinity>);
+expectType<number>({} as Sum<NegativeInfinity, number>);
+expectType<number>({} as Sum<NegativeInfinity, PositiveInfinity>);
+expectType<number>({} as Sum<PositiveInfinity, NegativeInfinity>);
 
 // Union
 expectType<3 | 4>({} as Sum<1, 2 | 3>);
 expectType<4 | 5>({} as Sum<1 | 2, 3>);
 expectType<5 | 6 | 7 | 8>({} as Sum<1 | 2 | 3, 4 | 5>);
+expectType<2 | -3 | 6 | -1 | -6 | 3 | 4 | 8>({} as Sum<1 | -2 | 3, 1 | -4 | 5>);
