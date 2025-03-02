bcb60efce1364b17bfd2ed6247d7a0a8fd238920
Laurynas Grigutis <lgrigutis@perthera.com>
2025-03-02 17:16:01 +0200

    Updated comments & example in Sum

--- source/sum.d.ts
+++ source/sum.d.ts
@@ -7,8 +7,6 @@ Returns the sum of two numbers.
 
 Note:
 - A or B can only support `-999` ~ `999`.
-- A and B can only be small integers, less than 1000.
-- If the result is negative, you can only get `number`.
 
 @example
 ```
@@ -21,7 +19,7 @@ Sum<-111, 222>;
 //=> 111
 
 Sum<111, -222>;
-//=> number
+//=> -111
 
 Sum<PositiveInfinity, -9999>;
 //=> PositiveInfinity
@@ -32,7 +30,7 @@ Sum<PositiveInfinity, NegativeInfinity>;
 
 @category Numeric
 */
-// TODO: Support big integer and negative number.
+// TODO: Support big integer.
 export type Sum<A extends number, B extends number> =
 	// Handle cases when A or B is the actual "number" type
 	number extends A | B ? number
@@ -44,22 +42,23 @@ export type Sum<A extends number, B extends number> =
 				: A extends PositiveInfinity ? B extends NegativeInfinity ? number : PositiveInfinity
 					// Handle cases when B is +/- infinity (we know A is not +/- infinity at this point)
 					: B extends PositiveInfinity | NegativeInfinity ? B
-						// Handle cases when A or B is 0 or numbers have different signs
+						// Handle cases when A or B is 0 or it's the same number with different signs
 						: A extends 0 ? B : B extends 0 ? A : A extends ReverseSign<B> ? 0
 							// Handle remaining regular cases
 							: SumPostChecks<A, B>;
 
 /**
-Adds two numbers A and B, such that they are not equal and neither of them are 0, +/- infinity or the `number` type
+Adds two numbers A and B, such that they are not equal with different signs and neither of them are 0, +/- infinity or the `number` type
 */
 type SumPostChecks<A extends number, B extends number, AreNegative = [IsNegative<A>, IsNegative<B>]> =
 	AreNegative extends [false, false]
+		// When both numbers are positive we can add them together
 		? SumPositives<A, B>
 		: AreNegative extends [true, true]
 			// When both numbers are negative we add the absolute values and then reverse the sign
 			? ReverseSign<SumPositives<NumberAbsolute<A>, NumberAbsolute<B>>>
 			// When the signs are different we can subtract the absolute values, remove the sign
-			// and then reverse the sign if larger absolute value is negative
+			// and then reverse the sign if the larger absolute value is negative
 			: NumberAbsolute<Subtract<NumberAbsolute<A>, NumberAbsolute<B>>> extends infer Result extends number
 				? TupleMax<[NumberAbsolute<A>, NumberAbsolute<B>]> extends infer Max_ extends number
 					? Max_ extends A | B
