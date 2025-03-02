cc93f85e1cabf66db266e3aa38348d07076f0044
Som Shekhar Mukherjee <49264891+som-sm@users.noreply.github.com>
2025-03-02 20:27:01 +0530

    `Split`: Fix instantiations with unions and add `strictLiteralChecks` option (#1067)

--- test-d/split.ts
+++ test-d/split.ts
@@ -30,6 +30,29 @@ expectType<[]>(split('', ''));
 // Split empty string by any string.
 expectType<['']>(split('', ' '));
 
+// Unions
+expectType<['a', 'b', 'c'] | ['x', 'y', 'z']>({} as Split<'a,b,c' | 'x,y,z', ','>);
+expectType<['a', 'b', 'c'] | ['a,', ',c']>({} as Split<'a,b,c', ',' | 'b'>);
+expectType<['a', 'b', 'c'] | ['a,b,c'] | ['x', 'y', 'z'] | ['x:y:z']>({} as Split<'a,b,c' | 'x:y:z', ',' | ':'>);
+
+// -- strictLiteralChecks: false --
+// NOTE: The behaviour covered in the test below is not acurate because the `a.b.${string}` type might hold a value like `a.b.c.d`,
+// which, when split by `.`, would result in `['a', 'b', 'c', 'd']`, which wouldn't conform to the output type of `['a', 'b', string]`.
+expectType<['a', 'b', string]>(split('a.b.c.d' as `a.b.${string}`, '.'));
+
+// -- strictLiteralChecks: true --
+expectType<string[]>({} as Split<string, '', {strictLiteralChecks: true}>);
+expectType<string[]>({} as Split<Uppercase<string>, '-', {strictLiteralChecks: true}>);
+expectType<string[]>({} as Split<`on${string}`, 'n', {strictLiteralChecks: true}>);
+expectType<string[]>({} as Split<'a,b,c', string, {strictLiteralChecks: true}>);
+expectType<string[]>({} as Split<'a,b,c', Lowercase<string>, {strictLiteralChecks: true}>);
+expectType<string[]>({} as Split<'a,b,c', `,${string}`, {strictLiteralChecks: true}>);
+expectType<string[]>({} as Split<`on${string}`, Lowercase<string>, {strictLiteralChecks: true}>);
+
+expectType<['a', 'b', 'c'] | string[]>({} as Split<'a,b,c' | `bye, ${string}`, ',', {strictLiteralChecks: true}>);
+expectType<['a', 'b', 'c'] | string[]>({} as Split<'a,b,c', ',' | `b${string}`, {strictLiteralChecks: true}>);
+expectType<['a', 'b', 'c'] | string[]>({} as Split<'a,b,c' | `bye, ${string}`, ',' | `b${string}`, {strictLiteralChecks: true}>);
+
 // Recursion depth at which a non-tail recursive implementation starts to fail.
 type FiftyZeroes = StringRepeat<'0', 50>;
 expectType<BuildTuple<50, '0'>>({} as Split<FiftyZeroes, ''>);
