ab52babdf686d0339a502d1a0cefb81c3237a4dc
Christophe Hurpeau <302891+christophehurpeau@users.noreply.github.com>
2025-03-21 10:53:44 +0100

    Update async-return-type.ts

--- test-d/async-return-type.ts
+++ test-d/async-return-type.ts
@@ -11,3 +11,20 @@ asyncFunction().then(value => { // eslint-disable-line unicorn/prefer-top-level-
 	expectType<Value>(value);
 	expectNotAssignable<string>(value);
 });
+
+
+
+function asyncPromiseLike(): PromiseLike<number> {
+	return {
+		then(onfulfilled) {
+			return Promise.resolve(2).then(onfulfilled);
+		}
+	};
+}
+
+type ValuePromiseLike = AsyncReturnType<typeof asyncPromiseLike>;
+
+asyncPromiseLike().then(value => { // eslint-disable-line unicorn/prefer-top-level-await, @typescript-eslint/no-floating-promises
+	expectType<ValuePromiseLike>(value);
+	expectNotAssignable<string>(value);
+});
