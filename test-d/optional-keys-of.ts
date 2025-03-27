735342e1243a33f4f0de79307f4f474ce9e245df
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-03-27 16:42:57 +0530

    fix: instantiations with unions and arrays

--- test-d/optional-keys-of.ts
+++ test-d/optional-keys-of.ts
@@ -27,3 +27,15 @@ declare const test3: OptionalKeysOf3;
 expectType<'b'>(test1);
 expectType<'a' | 'b'>(test2);
 expectType<never>(test3);
+
+expectType<'a' | 'c'>({} as OptionalKeysOf<{readonly a?: string; readonly b: number; c?: boolean; d: string}>);
+
+// Unions
+expectType<'b' | 'c'>({} as OptionalKeysOf<{a: string; b?: number} | {readonly c?: string; readonly d: number}>);
+expectType<'a' | 'b'>({} as OptionalKeysOf<{a: string; b: number} | {a?: string; b?: number}>);
+
+// Arrays
+expectType<never>({} as OptionalKeysOf<[]>);
+expectType<never>({} as OptionalKeysOf<readonly [string, number, boolean]>);
+expectType<'1' | '2'>({} as OptionalKeysOf<[string, number?, boolean?]>);
+expectType<'0' | '1' | '2'>({} as OptionalKeysOf<[string?] | readonly [string, number?] | [string, number, boolean?]>);
