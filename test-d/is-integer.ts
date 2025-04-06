390d1b0be3b49ad9d7ae8ddc48562ae11a517637
Sindre Sorhus <sindresorhus@gmail.com>
2025-04-06 13:13:48 +0700

    Update is-integer.ts

--- test-d/is-integer.ts
+++ test-d/is-integer.ts
@@ -3,13 +3,6 @@ import type {IsInteger, PositiveInfinity} from '../index';
 
 declare const x: unknown;
 
-expectType<false>(x as IsInteger<1.5>);
-expectType<false>(x as IsInteger<-1.5>);
-expectType<false>(x as IsInteger<number>);
-expectType<false>(x as IsInteger<PositiveInfinity>);
-expectType<false>(x as IsInteger<0.000_000_1>);
-expectType<false>(x as IsInteger<-1e-7>);
-
 expectType<true>(x as IsInteger<0>);
 expectType<true>(x as IsInteger<1>);
 expectType<true>(x as IsInteger<1.0>); // eslint-disable-line unicorn/no-zero-fractions
@@ -22,3 +15,10 @@ expectType<true>(x as IsInteger<0x10>);
 expectType<true>(x as IsInteger<1e+100>);
 expectType<true>(x as IsInteger<1.23e+21>);
 expectType<true>(x as IsInteger<-1.23e+21>);
+
+expectType<false>(x as IsInteger<1.5>);
+expectType<false>(x as IsInteger<-1.5>);
+expectType<false>(x as IsInteger<number>);
+expectType<false>(x as IsInteger<PositiveInfinity>);
+expectType<false>(x as IsInteger<0.000_000_1>);
+expectType<false>(x as IsInteger<-1e-7>);
