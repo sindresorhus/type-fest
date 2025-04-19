7532095ff0a8a49c6d966f358cc32242bcdb0898
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-04-19 14:00:07 +0530

    test: add narrowing cases

--- test-d/require-all-or-none.ts
+++ test-d/require-all-or-none.ts
@@ -1,4 +1,5 @@
-import type {RequireAllOrNone} from '../index';
+import {expectAssignable, expectNotAssignable} from 'tsd';
+import type {RequireAllOrNone, Simplify} from '../index';
 
 type SystemMessages = {
 	default: string;
@@ -31,3 +32,51 @@ testWithoutKeys({a: 1, b: 2});
 testWithoutKeys({a: 1});
 // @ts-expect-error
 testWithoutKeys({b: 2});
+
+function narrowingTest(foo: Simplify<RequireAllOrNone<{a: string; b: string}>>): string { // `Simplify` just makes it easier to visualize the narrowing
+	if (typeof foo.a === 'string') {
+		expectAssignable<string>(foo.a);
+		expectAssignable<string>(foo.b);
+
+		return foo.a;
+	}
+
+	expectNotAssignable<string>(foo.a);
+	expectNotAssignable<string>(foo.b);
+
+	return '';
+}
+
+function narrowingTest2(foo: Simplify<RequireAllOrNone<{a: string; b: string; c: string}>>): string { // `Simplify` just makes it easier to visualize the narrowing
+	if (typeof foo.a === 'string') {
+		expectAssignable<string>(foo.a);
+		expectAssignable<string>(foo.b);
+		expectAssignable<string>(foo.c);
+
+		return foo.a;
+	}
+
+	expectNotAssignable<string>(foo.a);
+	expectNotAssignable<string>(foo.b);
+	expectNotAssignable<string>(foo.c);
+
+	return '';
+}
+
+function narrowingTest3(foo: Simplify<RequireAllOrNone<{a: string; b: string; c: string}, 'a' | 'b'>>): string { // `Simplify` just makes it easier to visualize the narrowing
+	if (typeof foo.a === 'string') {
+		expectAssignable<string>(foo.c);
+
+		expectAssignable<string>(foo.a);
+		expectAssignable<string>(foo.b);
+
+		return foo.a;
+	}
+
+	expectAssignable<string>(foo.c);
+
+	expectNotAssignable<string>(foo.a);
+	expectNotAssignable<string>(foo.b);
+
+	return '';
+}
--- test-d/require-one-or-none.ts
+++ test-d/require-one-or-none.ts
@@ -1,5 +1,5 @@
 import {expectAssignable, expectNotAssignable} from 'tsd';
-import type {RequireOneOrNone} from '../index';
+import type {RequireOneOrNone, Simplify} from '../index';
 
 type OneAtMost = RequireOneOrNone<Record<'foo' | 'bar' | 'baz', true>>;
 
@@ -24,3 +24,83 @@ expectNotAssignable<OneOrTwo>({});
 expectNotAssignable<OneOrTwo>({bar: true});
 expectNotAssignable<OneOrTwo>({baz: true});
 expectNotAssignable<OneOrTwo>({foo: true, bar: true, baz: true});
+
+function narrowingTest(foo: Simplify<RequireOneOrNone<{a: string; b: string}>>): string { // `Simplify` just makes it easier to visualize the narrowing
+	if (typeof foo.a === 'string') {
+		expectAssignable<string>(foo.a);
+		expectNotAssignable<string>(foo.b);
+
+		return foo.a;
+	}
+
+	if (typeof foo.b === 'string') {
+		expectAssignable<string>(foo.b);
+		expectNotAssignable<string>(foo.a);
+
+		return foo.b;
+	}
+
+	expectNotAssignable<string>(foo.a);
+	expectNotAssignable<string>(foo.b);
+
+	return '';
+}
+
+function narrowingTest2(foo: Simplify<RequireOneOrNone<{a: string; b: string; c: string}>>): string { // `Simplify` just makes it easier to visualize the narrowing
+	if (typeof foo.a === 'string') {
+		expectAssignable<string>(foo.a);
+		expectNotAssignable<string>(foo.b);
+		expectNotAssignable<string>(foo.c);
+
+		return foo.a;
+	}
+
+	if (typeof foo.b === 'string') {
+		expectAssignable<string>(foo.b);
+		expectNotAssignable<string>(foo.a);
+		expectNotAssignable<string>(foo.c);
+
+		return foo.b;
+	}
+
+	if (typeof foo.c === 'string') {
+		expectAssignable<string>(foo.c);
+		expectNotAssignable<string>(foo.a);
+		expectNotAssignable<string>(foo.b);
+
+		return foo.c;
+	}
+
+	expectNotAssignable<string>(foo.a);
+	expectNotAssignable<string>(foo.b);
+	expectNotAssignable<string>(foo.c);
+
+	return '';
+}
+
+function narrowingTest3(foo: Simplify<RequireOneOrNone<{a: string; b: string; c: string}, 'a' | 'b'>>): string { // `Simplify` just makes it easier to visualize the narrowing
+	if (typeof foo.a === 'string') {
+		expectAssignable<string>(foo.c);
+
+		expectAssignable<string>(foo.a);
+		expectNotAssignable<string>(foo.b);
+
+		return foo.a;
+	}
+
+	if (typeof foo.b === 'string') {
+		expectAssignable<string>(foo.c);
+
+		expectAssignable<string>(foo.b);
+		expectNotAssignable<string>(foo.a);
+
+		return foo.b;
+	}
+
+	expectAssignable<string>(foo.c);
+
+	expectNotAssignable<string>(foo.a);
+	expectNotAssignable<string>(foo.b);
+
+	return '';
+}
