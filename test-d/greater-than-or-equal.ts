ff6530c2a8094123cb67499f92363cc16fb85557
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-04-29 10:38:11 +0530

    fix: number comparision types with unions

--- test-d/greater-than-or-equal.ts
+++ test-d/greater-than-or-equal.ts
@@ -13,6 +13,21 @@ expectType<GreaterThanOrEqual<-2, -2>>(true);
 expectType<GreaterThanOrEqual<-2, -3>>(true);
 expectType<GreaterThanOrEqual<-2, number>>(never);
 
+// === unions ===
+expectType<GreaterThanOrEqual<100 | 200, 50>>(true);
+expectType<GreaterThanOrEqual<25, -100 | -15 | 2 | 21>>(true);
+expectType<GreaterThanOrEqual<10 | 15, -5 | 10>>(true);
+
+expectType<GreaterThanOrEqual<10, 50 | 100>>(false);
+expectType<GreaterThanOrEqual<50 | 25 | 0 | -16, 100>>(false);
+expectType<GreaterThanOrEqual<1 | 2, 3 | 4>>(false);
+
+expectType<GreaterThanOrEqual<-10, -90 | 90>>({} as boolean);
+expectType<GreaterThanOrEqual<-16 | 16, 0>>({} as boolean);
+expectType<GreaterThanOrEqual<-4 | 45, 20 | 30>>({} as boolean);
+expectType<GreaterThanOrEqual<1 | -1 | 3, 0 | 2>>({} as boolean);
+expectType<GreaterThanOrEqual<1 | 2 | 3, 3 | 4>>({} as boolean);
+
 expectType<GreaterThanOrEqual<PositiveInfinity, -999>>(true);
 expectType<GreaterThanOrEqual<PositiveInfinity, 999>>(true);
 expectType<GreaterThanOrEqual<999, PositiveInfinity>>(false);
--- test-d/less-than.ts
+++ test-d/less-than.ts
@@ -12,6 +12,21 @@ expectType<LessThan<2, 2>>(false);
 expectType<LessThan<-2, -2>>(false);
 expectType<LessThan<-2, -3>>(false);
 expectType<LessThan<-2, number>>(never);
+
+// === unions ===
+expectType<LessThan<10, 50 | 100>>(true);
+expectType<LessThan<50 | 25 | 0 | -16, 100>>(true);
+expectType<LessThan<1 | 2, 3 | 4>>(true);
+
+expectType<LessThan<100 | 200, 50>>(false);
+expectType<LessThan<25, -100 | -15 | 2 | 21>>(false);
+expectType<LessThan<10 | 15, -5 | 10>>(false);
+
+expectType<LessThan<-10, -90 | 90>>({} as boolean);
+expectType<LessThan<-16 | 16, 0>>({} as boolean);
+expectType<LessThan<-4 | 45, 20 | 30>>({} as boolean);
+expectType<LessThan<1 | -1 | 3, 0 | 2>>({} as boolean);
+
 expectType<LessThan<PositiveInfinity, -999>>(false);
 expectType<LessThan<PositiveInfinity, 999>>(false);
 expectType<LessThan<999, PositiveInfinity>>(true);
