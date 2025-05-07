67f5c723c6cfcb25b0dfa3aa4b1038927e897907
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-05-07 22:13:13 +0530

    test: add more cases

--- test-d/extract-strict.ts
+++ test-d/extract-strict.ts
@@ -59,3 +59,43 @@ declare const invalidMixedFields: ExtractStrict<Foobar, {kind: 'foo'; d: string}
 
 // @ts-expect-error
 declare const undefinedField: ExtractStrict<Foobar, undefined>;
+
+// Primitives
+expectType<number>({} as ExtractStrict<string | number, number>);
+expectType<number | bigint>({} as ExtractStrict<string | number | bigint, number | bigint>);
+expectType<'bar' | 'baz'>({} as ExtractStrict<'foo' | 'bar' | 'baz', `b${string}`>);
+
+// @ts-expect-error
+type invalid1 = ExtractStrict<string | number | boolean, number | bigint>;
+// @ts-expect-error
+type invalid2 = ExtractStrict<string, Uppercase<string>>;
+
+// Optional and readonly modifiers
+expectType<{a: string; b: number}>({} as ExtractStrict<{a: string; b: number}, {a?: string}>);
+expectType<string[]>({} as ExtractStrict<string[], readonly string[]>);
+
+// @ts-expect-error
+type invalid3 = ExtractStrict<{a?: string; b: number}, {a: string}>;
+// @ts-expect-error
+type invalid4 = ExtractStrict<readonly string[], string[]>;
+
+// Index signatures
+expectType<{c: true; d: false}>(
+	{} as ExtractStrict<{a: string; b: number} | {c: true; d: false}, Record<string, boolean>>,
+);
+
+// @ts-expect-error
+type invalid5 = ExtractStrict<{a: string; b: number} | {c: true; d: false}, Record<string, string>>;
+
+// `any` and `never`
+expectType<string | {a: string; b: number} | string[]>(
+	{} as ExtractStrict<string | {a: string; b: number} | string[], any>,
+);
+expectType<never>(
+	{} as ExtractStrict<string | {a: string; b: number} | string[], never>,
+);
+
+// Miscellaneous
+expectType<[number, number]>({} as ExtractStrict<[number, number] | {x: number; y: number}, unknown[]>);
+expectType<[number, number]>({} as ExtractStrict<[number, number] | [number, number, number], {length: 2}>);
+expectType<{data: string | string[]}>({} as ExtractStrict<string | string[] | {data: string | string[]}, {data: unknown}>);
