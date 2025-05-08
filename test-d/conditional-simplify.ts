be8adf03cd0a1fe4292aaa25e2efdbf072a6357a
Roman Wu <me@romanwu.com>
2025-05-08 17:36:50 +0800

    Update conditional-simplify.ts

--- test-d/conditional-simplify.ts
+++ test-d/conditional-simplify.ts
@@ -6,15 +6,19 @@ type Size = {width: number; height: number};
 // In your editor, hovering over `PositionAndSizeSimplified` will show a simplified object with all the properties.
 type PositionAndSizeIntersection = Position & Size;
 type PositionAndSizeSimplified = ConditionalSimplify<PositionAndSizeIntersection>;
+
 const position = {top: 120, left: 240};
 const size = {width: 480, height: 600};
 const positionAndSize = {...position, ...size};
 expectType<PositionAndSizeSimplified>(positionAndSize);
+
 // Exclude function type to be simplified.
 type SomeFunction = (type: string) => string;
 type SimplifiedFunctionFail = ConditionalSimplify<SomeFunction>; // Return '{}'
 type SimplifiedFunctionPass = ConditionalSimplify<SomeFunction, Function>; // Return '(type: string) => string'
+
 declare const simplifiedFunctionFail: SimplifiedFunctionFail;
 declare const simplifiedFunctionPass: SimplifiedFunctionPass;
+
 expectNotAssignable<SomeFunction>(simplifiedFunctionFail);
 expectType<SomeFunction>(simplifiedFunctionPass);
