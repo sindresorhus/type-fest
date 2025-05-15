29c0ad85ef1381b534c33181aed6c9568b8aabc7
Haozheng Li <emiya@emiya.com.cn>
2025-05-15 11:04:56 +0800

    Update type.d.ts

--- source/internal/type.d.ts
+++ source/internal/type.d.ts
@@ -98,7 +98,7 @@ type C = IfNotAnyOrNever<never, 'VALID', 'IS_ANY', 'IS_NEVER'>;
 ```
 */
 export type IfNotAnyOrNever<T, IfNotAnyOrNever, IfAny = any, IfNever = never> =
-If<IsAny<T>, IfAny, If<IsNever<T>, IfNever, IfNotAnyOrNever>>;
+	If<IsAny<T>, IfAny, If<IsNever<T>, IfNever, IfNotAnyOrNever>>;
 
 /**
 Return the value of exactOptionalPropertyTypes option in tsconfig
