ba30540b7126acc419ed9be8f3bd314063f7f4be
benzaria <benzaria@users.noreply.github.com>
2025-05-26 23:41:33 +0100

    Add `Xor` and `XorAll` types
     **External**
     - `Xor` Exclusive Or
     - `XorAll` Return true when the specified number of trues present, default 1 <= x <= length - 1,
     	can be specified using `{strict: number}` option

---
+++ source/xor.d.ts
@@ -0,0 +1,34 @@
+import type {IsAnyOrNever, IsFalse, IsTrue} from './internal/type.d.ts'
+import type {ApplyDefaultOptions} from './internal/object.d.ts'
+import type {Includes} from './includes.d.ts'
+import type {CountOf} from './count-of.d.ts'
+import type {IsEqual} from './is-equal.d.ts'
+
+type XorOptions = {
+	/**
+	Only match a specific number of trues.
+
+	@default 1 <= x <= length - 1
+	*/
+	strict?: number
+}
+
+type DefaultXorOptions = {
+	strict: -1
+}
+
+type Xor<A extends boolean, B extends boolean> = XorAll<[A, B]>
+
+type XorAll<T extends boolean[], Options extends XorOptions = {}> = (
+	ApplyDefaultOptions<XorOptions, DefaultXorOptions, Options> extends infer ResolvedOptions extends Required<XorOptions>
+		? IsAnyOrNever<T[number]> extends false
+			? Includes<T, boolean> extends false
+				? [IsFalse<T[number]> | IsTrue<T[number]>] extends [false]
+					? ResolvedOptions['strict'] extends -1 
+						? true
+						: IsEqual<CountOf<T, true>, ResolvedOptions['strict']>
+					: false
+				: never
+			: never
+		: never
+)
