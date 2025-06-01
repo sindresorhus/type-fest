385db9b03a7952d591cc2baf497bffd32cad967e
benzaria <benzaria@users.noreply.github.com>
2025-06-01 17:59:06 +0100

    Fix: Linting errors

--- source/xor.d.ts
+++ source/xor.d.ts
@@ -1,8 +1,8 @@
-import type {IsAnyOrNever, IsFalse, IsTrue} from './internal/type.d.ts'
-import type {ApplyDefaultOptions} from './internal/object.d.ts'
-import type {Includes} from './includes.d.ts'
-import type {CountOf} from './count-of.d.ts'
-import type {IsEqual} from './is-equal.d.ts'
+import type {IsAnyOrNever, IsFalse, IsTrue} from './internal/type.d.ts';
+import type {ApplyDefaultOptions} from './internal/object.d.ts';
+import type {Includes} from './includes.d.ts';
+import type {CountOf} from './count-of.d.ts';
+import type {IsEqual} from './is-equal.d.ts';
 
 type XorOptions = {
 	/**
@@ -10,25 +10,26 @@ type XorOptions = {
 
 	@default 1 <= x <= length - 1
 	*/
-	strict?: number
-}
+	strict?: number;
+};
 
 type DefaultXorOptions = {
-	strict: -1
-}
+	strict: -1;
+};
 
-type Xor<A extends boolean, B extends boolean> = XorAll<[A, B]>
+type Xor<A extends boolean, B extends boolean> = XorAll<[A, B]>;
 
 type XorAll<T extends boolean[], Options extends XorOptions = {}> = (
 	ApplyDefaultOptions<XorOptions, DefaultXorOptions, Options> extends infer ResolvedOptions extends Required<XorOptions>
 		? IsAnyOrNever<T[number]> extends false
 			? Includes<T, boolean> extends false
 				? [IsFalse<T[number]> | IsTrue<T[number]>] extends [false]
-					? ResolvedOptions['strict'] extends -1 
-						? true
-						: IsEqual<CountOf<T, true>, ResolvedOptions['strict']>
+					? ResolvedOptions['strict'] extends infer TrueCount extends number
+						? TrueCount extends -1 ? true
+							: IsEqual<CountOf<T, true>, TrueCount>
+						: never
 					: false
 				: never
 			: never
 		: never
-)
+);
--- test-d/xor.ts
+++ test-d/xor.ts
@@ -1,7 +1,7 @@
-import { expectType } from 'tsd';
-import type { Xor, XorAll } from '../source/xor.d.ts';
+import {expectType} from 'tsd';
+import type {Xor, XorAll} from '../source/xor.d.ts';
 
-declare const never: never
+declare const never: never;
 
 // Xor
 expectType<Xor<true, true>>(false);
@@ -21,34 +21,40 @@ expectType<Xor<true, never>>(false);
 expectType<Xor<never, never>>(never);
 
 // XorAll
-expectType<XorAll<[true, true, true]>>(false)
-expectType<XorAll<[false, true, true]>>(true)
-expectType<XorAll<[false, false, true]>>(true)
-expectType<XorAll<[false, false, false]>>(false)
-expectType<XorAll<[true, false, false]>>(true)
-expectType<XorAll<[true, true, false]>>(true)
+expectType<XorAll<[true, true, true]>>(false);
+expectType<XorAll<[false, true, true]>>(true);
+expectType<XorAll<[false, false, true]>>(true);
+expectType<XorAll<[false, false, false]>>(false);
+expectType<XorAll<[true, false, false]>>(true);
+expectType<XorAll<[true, true, false]>>(true);
 
-expectType<XorAll<[true, true, true], { strict: 1 }>>(false)
-expectType<XorAll<[false, true, true], { strict: 1 }>>(false)
-expectType<XorAll<[false, false, true], { strict: 1 }>>(true)
-expectType<XorAll<[false, false, false], { strict: 1 }>>(false)
-expectType<XorAll<[true, false, false], { strict: 1 }>>(true)
-expectType<XorAll<[true, true, false], { strict: 1 }>>(false)
+expectType<XorAll<[true, true, true], {strict: 1}>>(false);
+expectType<XorAll<[false, true, true], {strict: 1}>>(false);
+expectType<XorAll<[false, false, true], {strict: 1}>>(true);
+expectType<XorAll<[false, false, false], {strict: 1}>>(false);
+expectType<XorAll<[true, false, false], {strict: 1}>>(true);
+expectType<XorAll<[true, true, false], {strict: 1}>>(false);
 
 // @ts-expect-error
-expectType<Xor<>>(never)
-expectType<XorAll<[]>>(never)
-expectType<Xor<never, never>>(never)
-expectType<Xor<never, any>>(never)
-expectType<Xor<any, any>>(never)
+expectType<Xor>(never);
+expectType<XorAll<[]>>(never);
+expectType<Xor<never, any>>(never);
+expectType<Xor<any, any>>(never);
+
+// Single value
+expectType<XorAll<[true]>>(false);
+expectType<XorAll<[false]>>(false);
+expectType<XorAll<[boolean]>>(never);
+expectType<XorAll<[never]>>(never);
+expectType<XorAll<[any]>>(never);
 
 // Test if boolean is position dependent
-expectType<XorAll<[boolean, true, true, true]>>(never)
-expectType<XorAll<[true, boolean, true, true]>>(never)
-expectType<XorAll<[true, true, boolean, true]>>(never)
-expectType<XorAll<[true, true, true, boolean]>>(never)
+expectType<XorAll<[boolean, true, true, true]>>(never);
+expectType<XorAll<[true, boolean, true, true]>>(never);
+expectType<XorAll<[true, true, boolean, true]>>(never);
+expectType<XorAll<[true, true, true, boolean]>>(never);
 
-expectType<XorAll<[boolean, false, false, false]>>(never)
-expectType<XorAll<[false, boolean, false, false]>>(never)
-expectType<XorAll<[false, false, boolean, false]>>(never)
-expectType<XorAll<[false, false, false, boolean]>>(never)
+expectType<XorAll<[boolean, false, false, false]>>(never);
+expectType<XorAll<[false, boolean, false, false]>>(never);
+expectType<XorAll<[false, false, boolean, false]>>(never);
+expectType<XorAll<[false, false, false, boolean]>>(never);
