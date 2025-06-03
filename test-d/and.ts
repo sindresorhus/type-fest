37c919a950665994969711decd05439802893189
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-06-03 14:35:47 +0530

    chore: remove unused variable

--- test-d/and.ts
+++ test-d/and.ts
@@ -1,8 +1,6 @@
 import {expectType} from 'tsd';
 import type {And} from '../source/and.d.ts';
 
-declare const never: never;
-
 expectType<And<true, true>>(true);
 expectType<And<true, false>>(false);
 expectType<And<false, true>>(false);
