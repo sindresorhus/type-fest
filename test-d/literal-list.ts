ea13d94e5e618920a44b53a7886c538c66df04a1
benzaria <benzaria@users.noreply.github.com>
2025-06-09 18:47:44 +0100

    feat: add tests covering array `Shape` union

--- test-d/literal-list.ts
+++ test-d/literal-list.ts
@@ -1,9 +1,10 @@
-import {expectType} from 'tsd';
+import {expectAssignable, expectType} from 'tsd';
 import type {LiteralList} from '../source/literal-list.d.ts';
 import type {UnknownArray} from '../source/unknown-array.d.ts';
 
 type U1 = 'a' | 'b' | 'c' | 'd';
 type U2 = 1 | 2 | 3 | 4 | 5 | 6 | 7 | 8 | 9;
+type U3 = ['a'] | ['b', 'c'] | ['a', 'b'];
 
 // ? Should we add this type
 type IsLiteralList<T extends UnknownArray, U> =
@@ -61,12 +62,40 @@ expectType<IsLiteralList<[1, 2, 3, 4, 5, 6, 7, 8, 0], U2>>(false); // Extra
 expectType<IsLiteralList<[1, 2, 3, 4, 5, 6, 7, 8, 8], U2>>(false); // Missing
 expectType<IsLiteralList<[1, 2, 3, 4, 5, 6, 7, 8, 9, 0], U2>>(false); // Longer
 
-// Errors
-expectType<IsLiteralList<['a', 'b', 'c'], U1>>(false);
-expectType<IsLiteralList<['b', 'c', 'd'], U1>>(false);
-expectType<IsLiteralList<['c', 'a', 'd', 'b', 'f'], U1>>(false);
-expectType<IsLiteralList<['c', 'd', 'e', 'b', 'a'], U1>>(false);
-expectType<IsLiteralList<['a', 'd', 'b', 'b'], U1>>(false);
-expectType<IsLiteralList<['a', 'a', 'c', 'b'], U1>>(false);
-expectType<IsLiteralList<['b', 'a', 'c', 'm'], U1>>(false);
-expectType<IsLiteralList<['b', 'c', 'e', 'd'], U1>>(false);
+// Errors for `JoinableItem` (hover to see errors)
+type I1 = LiteralList<['a', 'b', 'c'], U1>;
+type I2 = LiteralList<['b', 'c', 'd'], U1>;
+type I3 = LiteralList<['c', 'a', 'd', 'b', 'f'], U1>;
+type I4 = LiteralList<['c', 'd', 'e', 'b', 'a'], U1>;
+type I5 = LiteralList<['a', 'd', 'b', 'b'], U1>;
+type I6 = LiteralList<['a', 'a', 'b', 'b'], U1>;
+type I7 = LiteralList<['b', 'a', 'c', 'm'], U1>;
+type I8 = LiteralList<['b', 'c', 'e', 'm'], U1>;
+
+expectAssignable<string>({} as I1);
+expectAssignable<string>({} as I2);
+expectAssignable<string>({} as I3);
+expectAssignable<string>({} as I4);
+expectAssignable<string>({} as I5);
+expectAssignable<string>({} as I6);
+expectAssignable<string>({} as I7);
+expectAssignable<string>({} as I8);
+
+// Errors for `JoinableItem[]` (hover to see errors)
+type A1 = LiteralList<[['a'], ['b', 'c']], U3>;
+type A2 = LiteralList<[['b', 'c'], ['a', 'b']], U3>;
+type A3 = LiteralList<[['a'], ['b', 'c'], ['a', 'b'], ['f']], U3>;
+type A4 = LiteralList<[['a'], ['b', 'c'], ['a', 'b'], 'd'], U3>;
+type A5 = LiteralList<[['b', 'c'], ['a'], ['a']], U3>;
+type A6 = LiteralList<[['a'], ['b', 'c'], ['b', 'c']], U3>;
+type A7 = LiteralList<[['a'], ['b', 'c'], ['f']], U3>;
+type A8 = LiteralList<[['b'], ['e'], ['a', 'b']], U3>;
+
+expectAssignable<string>({} as A1);
+expectAssignable<string>({} as A2);
+expectAssignable<string>({} as A3);
+expectAssignable<string>({} as A4);
+expectAssignable<string>({} as A5);
+expectAssignable<string>({} as A6);
+expectAssignable<string>({} as A7);
+expectAssignable<string>({} as A8);
