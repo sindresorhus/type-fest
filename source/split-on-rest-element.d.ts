4be6a5241276d672ee3f2e026f883e0c2453fd59
benzaria <benzaria@users.noreply.github.com>
2025-06-11 13:41:08 +0100

    feat: rename `keepOptionals` -> `preserveOptionalModifier`, change `SplitOnRestElement` docs & brought back `Rest[]`

--- source/split-on-rest-element.d.ts
+++ source/split-on-rest-element.d.ts
@@ -11,31 +11,35 @@ type SplitOnRestElementOptions = {
 	/**
 	Whether to preserve the optional modifier (`?`).
 
-	- When set to `true` (default), the optional modifiers are preserved as-is. For example, `SplitOnRestElement<[number, string?, ...boolean[]], {keepOptionals: true}>` returns `[[number, string?], boolean[], []]`.
+	- When set to `true`, the optional modifiers are preserved as-is. For example,
+		`SplitOnRestElement<[number, string?, ...boolean[]], {preserveOptionalModifier: true}>` returns `[[number, string?], boolean[], []]`.
 
 	- When set to `false`, optional elements like `T?` are transformed to `T | undefined` or simply `T` depending on the `exactOptionalPropertyTypes` compiler option. For example:
-		- With `exactOptionalPropertyTypes` enabled, `SplitOnRestElement<[number, string?, ...boolean[]], {keepOptionals: false}>` returns `[[number, string], boolean[], []]`.
+		- With `exactOptionalPropertyTypes` enabled,
+			`SplitOnRestElement<[number, string?, ...boolean[]], {preserveOptionalModifier: false}>` returns `[[number, string], boolean[], []]`.
 		- And, with it disabled, the result is `[[number, string | undefined], boolean[], []]`.
 
 	@default true
 	*/
-	keepOptionals?: boolean;
+	preserveOptionalModifier?: boolean;
 };
 
 type DefaultSplitOnRestElementOptions = {
-	keepOptionals: true;
+	preserveOptionalModifier: true;
 };
 
 /**
-Splits an array on its [`Rest`](https://www.typescriptlang.org/docs/handbook/2/objects.html#tuple-types) element portion into three parts:
+Splits an array into three parts,
+where the first part contains all the elements before the rest element,
+the second part is the rest element itself,
+and the third part contains all the elements after the rest element.
 
-1. The static prefix before any `Rest` element element (if any).
-2. The inferred `Rest` element type (e.g., `string` from `...string[]`) as a single-element tuple.
-3. The static suffix after the `Rest` element (if any).
+Note: If any of the parts are missing, then they will be represented as empty arrays.
+For example, `SplitOnRestElement<[string, number]>` returns `[[string, number], [], []]`,
+where parts corresponding to the rest element and elements after it are empty.
 
-If no `Rest` element exists, the entire array is treated as a static prefix with empty `Rest` element and suffix parts.
-
-By default, The optional modifier (`?`) is preserved. See {@link SplitOnRestElementOptions.keepOptionals keepOptionals} option to change this behavior.
+By default, The optional modifier (`?`) is preserved.
+See {@link SplitOnRestElementOptions.preserveOptionalModifier `preserveOptionalModifier`} option to change this behavior.
 
 @example
 ```ts
@@ -50,7 +54,7 @@ type T2 = SplitOnRestElement<[...boolean[], string]>;
 type T3 = SplitOnRestElement<[number, string?]>;
 //=> [[number, string?], [], []]
 
-type T4 = SplitOnRestElement<[number, string?], {keepOptionals: false}>;
+type T4 = SplitOnRestElement<[number, string?], {preserveOptionalModifier: false}>;
 //=> [[number, string], [], []] Or [[number, string | undefined], [], []]
 
 type T5 = SplitOnRestElement<[...number[]]>;
@@ -61,15 +65,13 @@ type T5 = SplitOnRestElement<[...number[]]>;
 @category Array
 */
 export type SplitOnRestElement<Array_ extends UnknownArray, Options extends SplitOnRestElementOptions = {}> = IfNotAnyOrNever<Array_,
-	DeconstructRestArray<Array_, ApplyDefaultOptions<SplitOnRestElementOptions, DefaultSplitOnRestElementOptions, Options>>
+	_SplitOnRestElement<Array_, ApplyDefaultOptions<SplitOnRestElementOptions, DefaultSplitOnRestElementOptions, Options>>
 >;
 
 /**
 Deconstructs an array on its rest element and returns the split portions.
-
-See {@link SplitOnRestElement SplitOnRestElement} for details.
 */
-export type DeconstructRestArray<
+export type _SplitOnRestElement<
 	Array_ extends UnknownArray,
 	Options extends Required<SplitOnRestElementOptions>,
 	HeadAcc extends UnknownArray = [],
@@ -80,15 +82,15 @@ export type DeconstructRestArray<
 			// Enters this branch, if `Array_` is empty (e.g., []),
 			// or `Array_` contains no non-rest elements preceding the rest element (e.g., `[...string[]]` or `[...string[], string]`).
 			? Array_ extends readonly [...infer Rest, infer Last]
-				? DeconstructRestArray<Rest, Options, HeadAcc, [Last, ...TailAcc]> // Accumulate elements that are present after the rest element.
-				: [HeadAcc, Array_ extends readonly [] ? [] : [Array_[number]], TailAcc] // Add the rest element between the accumulated elements.
+				? _SplitOnRestElement<Rest, Options, HeadAcc, [Last, ...TailAcc]> // Accumulate elements that are present after the rest element.
+				: [HeadAcc, Array_, TailAcc] // Add the rest element between the accumulated elements.
 			: Array_ extends readonly [(infer First)?, ...infer Rest]
-				? DeconstructRestArray<
+				? _SplitOnRestElement<
 					Rest, Options,
 					[
 						...HeadAcc,
 						...'0' extends OptionalKeysOf<Array_>
-							? Options['keepOptionals'] extends false
+							? Options['preserveOptionalModifier'] extends false
 								? [If<IsExactOptionalPropertyTypesEnabled, First, First | undefined>] // Add `| undefined` for optional elements, if `exactOptionalPropertyTypes` is disabled.
 								: [First?]
 							: [First],
--- test-d/split-on-rest-element.ts
+++ test-d/split-on-rest-element.ts
@@ -10,52 +10,52 @@ expectType<SplitOnRestElement<readonly ['a', 'b', 'c']>>({} as [['a', 'b', 'c'],
 // Finite Rest elements (flattened manually or fixed-length rest element)
 expectType<SplitOnRestElement<[...[1, 2], 3]>>({} as [[1, 2, 3], [], []]);
 expectType<SplitOnRestElement<[...[string?, number?]]>>({} as [[string?, number?], [], []]);
-expectType<SplitOnRestElement<[...[string?, number?]], {keepOptionals: false}>>({} as [[string, number], [], []]);
+expectType<SplitOnRestElement<[...[string?, number?]], {preserveOptionalModifier: false}>>({} as [[string, number], [], []]);
 expectType<SplitOnRestElement<[...[never], 1]>>({} as [[never, 1], [], []]);
 expectType<SplitOnRestElement<[...[], 1]>>({} as [[1], [], []]);
 
 // Infinite Rest elements (true variadic)
-expectType<SplitOnRestElement<[1, ...number[]]>>({} as [[1], [number], []]);
-expectType<SplitOnRestElement<[1, ...number[], 2]>>({} as [[1], [number], [2]]);
-expectType<SplitOnRestElement<['a', ...string[], 'b']>>({} as [['a'], [string], ['b']]);
-expectType<SplitOnRestElement<[...boolean[], string]>>({} as [[], [boolean], [string]]);
-expectType<SplitOnRestElement<[...string[], 'x', 'y']>>({} as [[], [string], ['x', 'y']]);
-expectType<SplitOnRestElement<[...string[]]>>({} as [[], [string], []]);
-expectType<SplitOnRestElement<[...never[]]>>({} as [[], [never], []]);
-expectType<SplitOnRestElement<[...never[], 1]>>({} as [[], [never], [1]]);
-expectType<SplitOnRestElement<[undefined, ...boolean[], null]>>({} as [[undefined], [boolean], [null]]);
-expectType<SplitOnRestElement<[void, ...never[], 1]>>({} as [[void], [never], [1]]);
-expectType<SplitOnRestElement<[null, ...any[], null]>>({} as [[null], [any], [null]]);
+expectType<SplitOnRestElement<[1, ...number[]]>>({} as [[1], number[], []]);
+expectType<SplitOnRestElement<[1, ...number[], 2]>>({} as [[1], number[], [2]]);
+expectType<SplitOnRestElement<['a', ...string[], 'b']>>({} as [['a'], string[], ['b']]);
+expectType<SplitOnRestElement<[...boolean[], string]>>({} as [[], boolean[], [string]]);
+expectType<SplitOnRestElement<[...string[], 'x', 'y']>>({} as [[], string[], ['x', 'y']]);
+expectType<SplitOnRestElement<[...string[]]>>({} as [[], string[], []]);
+expectType<SplitOnRestElement<[...never[]]>>({} as [[], never[], []]);
+expectType<SplitOnRestElement<[...never[], 1]>>({} as [[], never[], [1]]);
+expectType<SplitOnRestElement<[undefined, ...boolean[], null]>>({} as [[undefined], boolean[], [null]]);
+expectType<SplitOnRestElement<[void, ...never[], 1]>>({} as [[void], never[], [1]]);
+expectType<SplitOnRestElement<[null, ...any[], null]>>({} as [[null], any[], [null]]);
 
 // Generic Arrays
-expectType<SplitOnRestElement<string[]>>({} as [[], [string], []]);
-expectType<SplitOnRestElement<number[]>>({} as [[], [number], []]);
-expectType<SplitOnRestElement<unknown[]>>({} as [[], [unknown], []]);
-expectType<SplitOnRestElement<any[]>>({} as [[], [any], []]);
+expectType<SplitOnRestElement<string[]>>({} as [[], string[], []]);
+expectType<SplitOnRestElement<number[]>>({} as [[], number[], []]);
+expectType<SplitOnRestElement<unknown[]>>({} as [[], unknown[], []]);
+expectType<SplitOnRestElement<any[]>>({} as [[], any[], []]);
 
 // Readonly
 expectType<SplitOnRestElement<readonly []>>({} as [[], [], []]);
 expectType<SplitOnRestElement<readonly [number]>>({} as [[number], [], []]);
-expectType<SplitOnRestElement<readonly [...number[], 2]>>({} as [[], [number], [2]]);
-expectType<SplitOnRestElement<readonly [1, ...string[], 2]>>({} as [[1], [string], [2]]);
+expectType<SplitOnRestElement<readonly [...number[], 2]>>({} as [[], number[], [2]]);
+expectType<SplitOnRestElement<readonly [1, ...string[], 2]>>({} as [[1], string[], [2]]);
 expectType<SplitOnRestElement<readonly [1, 2, 3]>>({} as [[1, 2, 3], [], []]);
 
 // Unions
-expectType<SplitOnRestElement<[...Array<string | number>, boolean]>>({} as [[], [string | number], [boolean]]);
-expectType<SplitOnRestElement<[...Array<{id: string}>, number]>>({} as [[], [{id: string}], [number]]);
-expectType<SplitOnRestElement<[1, ...Array<[string, number]>, 2]>>({} as [[1], [[string, number]], [2]]);
+expectType<SplitOnRestElement<[...Array<string | number>, boolean]>>({} as [[], Array<string | number>, [boolean]]);
+expectType<SplitOnRestElement<[...Array<{id: string}>, number]>>({} as [[], Array<{id: string}>, [number]]);
+expectType<SplitOnRestElement<[1, ...Array<[string, number]>, 2]>>({} as [[1], Array<[string, number]>, [2]]);
 expectType<SplitOnRestElement<[1, 2] | [3, 4]>>({} as [[1, 2], [], []] | [[3, 4], [], []]);
-expectType<SplitOnRestElement<[1, ...number[]] | ['foo', ...string[]]>>({} as [[1], [number], []] | [['foo'], [string], []]);
+expectType<SplitOnRestElement<[1, ...number[]] | ['foo', ...string[]]>>({} as [[1], number[], []] | [['foo'], string[], []]);
 
 // Exotic Types
 expectType<SplitOnRestElement<[...[], ...[], ...[], 42]>>({} as [[42], [], []]);
-expectType<SplitOnRestElement<[...unknown[], true]>>({} as [[], [unknown], [true]]);
+expectType<SplitOnRestElement<[...unknown[], true]>>({} as [[], unknown[], [true]]);
 
 // Optional/Undefined/Falsy
-expectType<SplitOnRestElement<[0, 1?, 2?, ...never[]]>>({} as [[0, 1?, 2?], [never], []]);
-expectType<SplitOnRestElement<[0, 1?, 2?, ...never[]], {keepOptionals: false}>>({} as [[0, 1, 2], [never], []]);
-expectType<SplitOnRestElement<[number?, ...string[]]>>({} as [[number?], [string], []]);
-expectType<SplitOnRestElement<[number?, ...string[]], {keepOptionals: false}>>({} as [[number], [string], []]);
+expectType<SplitOnRestElement<[0, 1?, 2?, ...never[]]>>({} as [[0, 1?, 2?], never[], []]);
+expectType<SplitOnRestElement<[0, 1?, 2?, ...never[]], {preserveOptionalModifier: false}>>({} as [[0, 1, 2], never[], []]);
+expectType<SplitOnRestElement<[number?, ...string[]]>>({} as [[number?], string[], []]);
+expectType<SplitOnRestElement<[number?, ...string[]], {preserveOptionalModifier: false}>>({} as [[number], string[], []]);
 
 // Edge: Never / Any
 expectType<SplitOnRestElement<any>>({} as any);
