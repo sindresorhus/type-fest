91a23781ad8b149cecb34d482bc35bbdc7e216b3
benzaria <benzaria@users.noreply.github.com>
2025-06-18 12:47:04 +0100

    test: add test for `NumericLike` that cover all cases

--- test-d/internal/is-number-like.ts
+++ test-d/internal/is-number-like.ts
@@ -1,8 +1,35 @@
 import {expectType} from 'tsd';
 import type {IsNumberLike} from '../../source/internal/numeric.d.ts';
 
+// Integer
+expectType<IsNumberLike<'-1'>>(true);
+expectType<IsNumberLike<-1>>(true);
+expectType<IsNumberLike<'+1'>>(true);
 expectType<IsNumberLike<'1'>>(true);
 expectType<IsNumberLike<1>>(true);
+
+// Float
 expectType<IsNumberLike<'-1.1'>>(true);
 expectType<IsNumberLike<-1.1>>(true);
+expectType<IsNumberLike<'+1.1'>>(true);
+expectType<IsNumberLike<'1.1'>>(true);
+expectType<IsNumberLike<1.1>>(true);
+
+// Sientific
+expectType<IsNumberLike<'+1.2e+3'>>(true);
+expectType<IsNumberLike<'1.2e+3'>>(true);
+expectType<IsNumberLike<1.2e+3>>(true);
+expectType<IsNumberLike<'+5e-3'>>(true);
+expectType<IsNumberLike<'5e-3'>>(true);
+expectType<IsNumberLike<5e-3>>(true);
+
+expectType<IsNumberLike<'-1.2e+3'>>(true);
+expectType<IsNumberLike<-1.2e+3>>(true);
+expectType<IsNumberLike<'-5e-3'>>(true);
+expectType<IsNumberLike<-5e-3>>(true);
+
+// Non valid numeric
 expectType<IsNumberLike<'foo'>>(false);
+expectType<IsNumberLike<'1.2.3'>>(false);
+expectType<IsNumberLike<'5+1.2'>>(false);
+expectType<IsNumberLike<'5e-3.1'>>(false);
