0cc6754d8a346a48f34e5472ac47d2b290a3b277
Andrew Kazakov <andreww_dev@proton.me>
2025-10-13 12:24:50 +0300

    chore: add examples to `FunctionWithMaybeThisParameter` utility usage

--- source/internal/function.d.ts
+++ source/internal/function.d.ts
@@ -4,6 +4,18 @@ import type {IsUnknown} from '../is-unknown.d.ts';
 Constructs a function type with an optional `this` parameter.
 
 If `this` parameter is not needed, pass `unknown`.
+
+@example
+```ts
+type Fun = FunctionWithMaybeThisParameter<unknown, [string], void>;
+//=> (args_0: string) => void;
+```
+
+@example
+```ts
+type Fun = FunctionWithMaybeThisParameter<object, [foo: number, ...bar: string[]], boolean>;
+//=> (this: object, foo: number, ...bar: string[]) => boolean;
+```
 */
 export type FunctionWithMaybeThisParameter<
 	ThisParameter,
