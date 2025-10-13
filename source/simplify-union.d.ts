e90c3fa30913a5b5a40f0e751af3ef46c00f7abf
Sindre Sorhus <sindresorhus@gmail.com>
2025-10-13 13:19:32 +0900

    Update simplify-union.d.ts

--- source/simplify-union.d.ts
+++ source/simplify-union.d.ts
@@ -3,9 +3,7 @@ import type tag from 'tagged-tag';
 /**
 Flattens a complex union type to make it more readable and improve editor IntelliSense.
 
-This utility expands nested or intersected unions into a clearer, simplified form.
-It’s especially helpful when dealing with inferred types or unions that include
-functions, objects, or other complex members.
+This utility expands nested or intersected unions into a clearer, simplified form. It’s especially helpful when dealing with inferred types or unions that include functions, objects, or other complex members.
 
 @example
 ```
@@ -15,7 +13,7 @@ type U1 = 'a' | 0 | null | ['b', 1n];
 type U2 = (() => void) | {[x: string]: number; b: number};
 
 type UnSimplifiedUnion = U1 | U2;
-//=> U1 | U2 <- obfuscated, hovering over the type doesn't show the underline types in the union.
+//=> U1 | U2 <- obfuscated, hovering over the type doesn't show the underlying types in the union.
 
 type SimplifiedUnion = SimplifyUnion<UnSimplifiedUnion>;
 //=> 0 | "a" | null | ['b', 1n] | (() => void) | {[x: string]: number; b: number}
