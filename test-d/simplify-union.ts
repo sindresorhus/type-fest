407cf5c895c30468a3d0d515e96d81543235fff1
Sindre Sorhus <sindresorhus@gmail.com>
2025-10-13 13:18:47 +0900

    Update simplify-union.ts

--- test-d/simplify-union.ts
+++ test-d/simplify-union.ts
@@ -5,7 +5,7 @@ type U1 = 'a' | 0 | null | ['b', 1n];
 type U2 = (() => void) | {[x: string]: number; b: number};
 
 type UnSimplifiedUnion = U1 | U2;
-//=> U1 | U2 <- obfuscated, hovering over the type doesn't show the underline types in the union.
+//=> U1 | U2 <- obfuscated, hovering over the type doesn't show the underlying types in the union.
 
 type SimplifiedUnion = SimplifyUnion<UnSimplifiedUnion>;
 //=> 0 | "a" | null | ['b', 1n] | (() => void) | {[x: string]: number; b: number}
