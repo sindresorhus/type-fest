625f6958c9ef29593197e5ad28cde43277be7ce8
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-10-18 19:48:39 +0530

    doc: add JSDoc

--- source/exclusify-union.d.ts
+++ source/exclusify-union.d.ts
@@ -3,6 +3,61 @@ import type {KeysOfUnion} from './keys-of-union.d.ts';
 import type {Simplify} from './simplify.d.ts';
 import type {UnknownArray} from './unknown-array.d.ts';
 
+/**
+Adds all missing properties from other union members as optional `never`, making each member mutually exclusive.
+
+@example
+```
+import type {ExclusifyUnion} from 'type-fest';
+
+type FileConfig = {
+	filePath: string;
+};
+
+type InlineConfig = {
+	content: string;
+};
+
+declare function loadConfig1(options: FileConfig | InlineConfig): void;
+
+// Someone could mistakenly provide both `filePath` and `content`.
+loadConfig1({filePath: './config.json', content: '{ "name": "app" }'}); // No errors
+
+// Use `ExclusifyUnion` to prevent that mistake.
+type Config = ExclusifyUnion<FileConfig | InlineConfig>;
+//=> {filePath: string; content?: never} | {content: string; filePath?: never}
+
+declare function loadConfig2(options: Config): void;
+
+// @ts-expect-error
+loadConfig2({filePath: './config.json', content: '{ "name": "app" }'});
+//=> Error: Argument of type '{ filePath: string; content: string; }' is not assignable to parameter of type '{ filePath: string; content?: never; } | { content: string; filePath?: never; }'.
+
+loadConfig2({filePath: './config.json'}); // Ok
+
+loadConfig2({content: '{ "name": "app" }'}); // Ok
+```
+
+@example
+```
+import type {ExclusifyUnion} from 'type-fest';
+
+type A = ExclusifyUnion<{a: string} | {b: number}>;
+//=> {a: string; b?: never} | {a?: never; b: number}
+
+type B = ExclusifyUnion<{a: string} | {b: number} | {c: boolean}>;
+//=> {a: string; b?: never; c?: never} | {a?: never; b: number; c?: never} | {a?: never; b?: never; c: boolean}
+
+type C = ExclusifyUnion<{a: string; b: number} | {b: string; c: number}>;
+//=> {a: string; b: number; c?: never} | {a?: never; b: string; c: number}
+
+type D = ExclusifyUnion<{a?: 1; readonly b: 2} | {d: 4}>;
+//=> {a?: 1; readonly b: 2; d?: never} | {a?: never; b?: never; d: 4}
+```
+
+@category Object
+@category Union
+*/
 export type ExclusifyUnion<Union> =
 	Extract<Union, NonRecursiveType | ReadonlyMap<unknown, unknown> | ReadonlySet<unknown> | UnknownArray> extends infer SkippedMembers
 		? SkippedMembers | _ExclusifyUnion<Exclude<Union, SkippedMembers>>
