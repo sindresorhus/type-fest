ad62d2cc8b6ce8b26e0048dea3707ae647cd4ce7
benz <benz.github@gmail.com>
2025-10-20 07:12:46 +0100

    `Or`/`And`: Fix documentation errors and add link to `Xor` (#1261)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>
    Co-authored-by: Som Shekhar Mukherjee <iamssmkhrj@gmail.com>

--- test-d/and.ts
+++ test-d/and.ts
@@ -1,25 +1,27 @@
 import {expectType} from 'tsd';
 import type {And} from '../source/and.d.ts';
 
+declare const boolean: boolean;
+
 expectType<And<true, true>>(true);
 expectType<And<true, false>>(false);
 expectType<And<false, true>>(false);
 expectType<And<false, false>>(false);
 
-expectType<And<true, boolean>>({} as boolean);
-expectType<And<boolean, true>>({} as boolean);
+expectType<And<true, boolean>>(boolean);
+expectType<And<boolean, true>>(boolean);
 expectType<And<false, boolean>>(false);
 expectType<And<boolean, false>>(false);
-expectType<And<boolean, boolean>>({} as boolean);
+expectType<And<boolean, boolean>>(boolean);
 
 // Boundary cases
-expectType<And<any, true>>({} as boolean);
-expectType<And<true, any>>({} as boolean);
+expectType<And<any, true>>(boolean);
+expectType<And<true, any>>(boolean);
 expectType<And<any, false>>(false);
 expectType<And<false, any>>(false);
-expectType<And<any, boolean>>({} as boolean);
-expectType<And<boolean, any>>({} as boolean);
-expectType<And<any, any>>({} as boolean);
+expectType<And<any, boolean>>(boolean);
+expectType<And<boolean, any>>(boolean);
+expectType<And<any, any>>(boolean);
 
 expectType<And<never, true>>(false);
 expectType<And<true, never>>(false);
