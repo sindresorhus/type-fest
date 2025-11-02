d891e127c5555b7fd845c4994d45d544e5d00582
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-11-03 00:22:27 +0530

    fix: `StringSlice` with unions

--- test-d/string-slice.ts
+++ test-d/string-slice.ts
@@ -15,3 +15,29 @@ expectType<StringSlice<'abcde', 100, 1>>('');
 expectType<StringSlice<string>>(null! as string);
 expectType<StringSlice<string, 1>>(null! as string);
 expectType<StringSlice<string, 1, 2>>(null! as string);
+
+// Unions
+// String is union
+expectType<StringSlice<'abcd' | '0123456789'>>({} as 'abcd' | '0123456789');
+expectType<StringSlice<'abcd' | '0123456789', 0, 2>>({} as 'ab' | '01');
+
+// Start is union
+expectType<StringSlice<'abcd', 1 | 3>>({} as 'bcd' | 'd');
+expectType<StringSlice<'abcd', 0 | 2, 3>>({} as 'abc' | 'c');
+
+// End is union
+expectType<StringSlice<'abcd', 1, 2 | 3>>({} as 'b' | 'bc');
+
+// String and start are unions
+expectType<StringSlice<'abcd' | '0123456789', 1 | 2>>({} as 'bcd' | 'cd' | '123456789' | '23456789');
+
+// String and end are unions
+expectType<StringSlice<'abcd' | '0123456789', 1, 2 | 3>>({} as 'b' | 'bc' | '1' | '12');
+
+// Start and end are unions
+expectType<StringSlice<'abcd', 0 | 1, 2 | 3>>({} as 'ab' | 'abc' | 'b' | 'bc');
+
+// String, start and end are unions
+expectType<StringSlice<'abcd' | '0123456789', 0 | 1, 2 | 3>>(
+	{} as 'ab' | 'abc' | 'b' | 'bc' | '01' | '012' | '1' | '12',
+);
