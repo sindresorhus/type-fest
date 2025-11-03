223759d7b861c496c065be304a0e3640027e6804
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2025-11-03 13:15:49 +0530

    test: improve `StringSlice` cases

--- test-d/string-slice.ts
+++ test-d/string-slice.ts
@@ -18,26 +18,37 @@ expectType<StringSlice<string, 1, 2>>(null! as string);
 
 // Unions
 // String is union
-expectType<StringSlice<'abcd' | '0123456789'>>({} as 'abcd' | '0123456789');
-expectType<StringSlice<'abcd' | '0123456789', 0, 2>>({} as 'ab' | '01');
+expectType<StringSlice<'012' | 'abcd', 0>>({} as '012' | 'abcd'); // Positive start, no end
+expectType<StringSlice<'012' | 'abcd', -2>>({} as '12' | 'cd'); // Negative start, no end
+expectType<StringSlice<'012' | 'abcd', 0, 2>>({} as '01' | 'ab'); // Positive start, positive end
+expectType<StringSlice<'012' | 'abcd', -2, -1>>({} as '1' | 'c'); // Negative start, negative end
+expectType<StringSlice<'012' | 'abcd', -3, 2>>({} as '01' | 'b'); // Negative start, positive end
+expectType<StringSlice<'012' | 'abcd', 1, -1>>({} as '1' | 'bc'); // Positive start, negative end
 
 // Start is union
-expectType<StringSlice<'abcd', 1 | 3>>({} as 'bcd' | 'd');
-expectType<StringSlice<'abcd', 0 | 2, 3>>({} as 'abc' | 'c');
+expectType<StringSlice<'0123', 1 | -2>>({} as '123' | '23'); // Positive/Negative start, no end
+expectType<StringSlice<'0123', 2 | -3, 3>>({} as '2' | '12'); // Positive/Negative start, positive end
+expectType<StringSlice<'0123', 0 | -2, -1>>({} as '2' | '012'); // Positive/Negative start, negative end
 
 // End is union
-expectType<StringSlice<'abcd', 1, 2 | 3>>({} as 'b' | 'bc');
+expectType<StringSlice<'0123', 0, 1 | -2>>({} as '0' | '01'); // Positive start, positive/negative end
+expectType<StringSlice<'0123', -2, 2 | -1>>({} as '' | '2'); // Negative start, positive/negative end
 
-// String and start are unions
-expectType<StringSlice<'abcd' | '0123456789', 1 | 2>>({} as 'bcd' | 'cd' | '123456789' | '23456789');
+// Array and start are unions
+expectType<StringSlice<'012' | 'abcd', 1 | -1>>({} as '12' | '2' | 'bcd' | 'd'); // Positive/Negative start, no end
+expectType<StringSlice<'012' | 'abcd', 1 | -2, 2>>({} as '1' | 'b' | ''); // Positive/Negative start, positive end
+expectType<StringSlice<'012' | 'abcd', 0 | -2, -1>>({} as '01' | '1' | 'abc' | 'c'); // Positive/Negative start, negative end
 
-// String and end are unions
-expectType<StringSlice<'abcd' | '0123456789', 1, 2 | 3>>({} as 'b' | 'bc' | '1' | '12');
+// Array and end are unions
+expectType<StringSlice<'012' | 'abcd', 2, 3 | -1>>({} as '2' | '' | 'c'); // Positive start, positive/negative end
+expectType<StringSlice<'012' | 'abcd', -3, 3 | -2>>({} as '012' | '0' | 'bc' | 'b'); // Negative start, positive/negative end
 
 // Start and end are unions
-expectType<StringSlice<'abcd', 0 | 1, 2 | 3>>({} as 'ab' | 'abc' | 'b' | 'bc');
+expectType<StringSlice<'0123', -5 | 0 | 1, -2 | 0 | 3>>( // Positive/Negative start, positive/negative end
+	{} as '01' | '012' | '' | '1' | '12',
+);
 
-// String, start and end are unions
-expectType<StringSlice<'abcd' | '0123456789', 0 | 1, 2 | 3>>(
-	{} as 'ab' | 'abc' | 'b' | 'bc' | '01' | '012' | '1' | '12',
+// Array, start and end are unions
+expectType<StringSlice<'012' | 'abcd', 1 | -4, 4 | -1>>( // Positive/Negative start, positive/negative end
+	{} as '1' | '12' | '01' | '012' | 'abcd' | 'abc' | 'bc' | 'bcd',
 );
