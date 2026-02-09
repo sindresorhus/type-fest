cfea505acc7291fddf564d10d1b87e0a5284277a
Som Shekhar Mukherjee <49264891+som-sm@users.noreply.github.com>
2026-02-10 01:24:53 +0530

    `GreaterThan` / `LessThan` / `GreaterThanOrEqual` / `LessThanOrEqual`: Fix behavior with the `number` type (#1363)

--- test-d/greater-than-or-equal.ts
+++ test-d/greater-than-or-equal.ts
@@ -11,7 +11,6 @@ expectType<GreaterThanOrEqual<10, -2>>(true);
 expectType<GreaterThanOrEqual<2, 2>>(true);
 expectType<GreaterThanOrEqual<-2, -2>>(true);
 expectType<GreaterThanOrEqual<-2, -3>>(true);
-expectType<GreaterThanOrEqual<-2, number>>(never);
 
 // === unions ===
 expectType<GreaterThanOrEqual<100 | 200, 50>>(true);
@@ -37,3 +36,8 @@ expectType<GreaterThanOrEqual<-999, NegativeInfinity>>(true);
 expectType<GreaterThanOrEqual<PositiveInfinity, PositiveInfinity>>(true);
 expectType<GreaterThanOrEqual<NegativeInfinity, NegativeInfinity>>(true);
 expectType<GreaterThanOrEqual<PositiveInfinity, NegativeInfinity>>(true);
+
+// Non-literal `number`
+expectType<GreaterThanOrEqual<number, number>>({} as boolean);
+expectType<GreaterThanOrEqual<number, 1>>({} as boolean);
+expectType<GreaterThanOrEqual<1, number>>({} as boolean);
