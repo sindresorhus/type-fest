fe03131d8c4fe3b93ad9a0b03386286d129805d1
taiyakihitotsu <taiyakihitotsu@proton.me>
2026-03-03 11:05:47 +0900

    doc: rewrite `UnionMember` example
    Co-authored-by: Som Shekhar Mukherjee <49264891+som-sm@users.noreply.github.com>

--- source/union-member.d.ts
+++ source/union-member.d.ts
@@ -4,27 +4,53 @@ import type {IsNever} from './is-never.d.ts';
 /**
 Returns an arbitrary member of a union type.
 
+Use-cases:
+- Implementing recursive type functions that accept a union type.
+
+@example
+```
+import type {UnionMember, IsNever} from 'type-fest';
+
+type UnionLength<T, Acc extends any[] = []> =
+	UnionMember<T> extends infer Member
+		? IsNever<Member> extends false
+			? UnionLength<Exclude<T, Member>, [...Acc, Member]>
+			: Acc['length']
+		: never;
+
+type T1 = UnionLength<'foo' | 'bar' | 'baz'>;
+//=> 3
+
+type T2 = UnionLength<{a: string}>;
+//=> 1
+```
+
+- Picking an arbitrary member from a union
+
+@example
+```
+import type {UnionMember, Primitive, LiteralToPrimitive} from 'type-fest';
+
+type IsHomogenous<T extends Primitive> = [T] extends [LiteralToPrimitive<UnionMember<T>>] ? true : false;
+
+type T1 = IsHomogenous<1 | 2 | 3 | 4>;
+//=> true
+
+type T2 = IsHomogenous<'foo' | 'bar'>;
+//=> true
+
+type T3 = IsHomogenous<'foo' | 'bar' | 1>;
+//=> false
+```
+
 Returns `never` when the input is `never`.
 
 @example
 ```
 import type {UnionMember} from 'type-fest';
 
-type Last = UnionMember<1 | 2 | 3>;
-//=> 3
-
 type LastNever = UnionMember<never>;
 //=> never
-```
-
-@example
-```
-import type {UnionMember, IsNever} from 'type-fest';
-
-type UnionToTuple<T, L = UnionMember<T>> =
-	IsNever<T> extends false
-		? [...UnionToTuple<Exclude<T, L>>, L]
-		: [];
 ```
 
 @category Type
