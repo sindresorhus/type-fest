59bd0566f4731e1b856120e8f527021f094bf725
jericirenej <63595376+jericirenej@users.noreply.github.com>
2026-03-03 15:29:23 +0100

    Add `ArrayLength` type (#1344)
    Co-authored-by: Sindre Sorhus <sindresorhus@gmail.com>
    Co-authored-by: Som Shekhar Mukherjee <iamssmkhrj@gmail.com>

---
+++ source/array-length.d.ts
@@ -0,0 +1,36 @@
+/**
+Return the length of an array. Equivalent to `T['length']` where `T` extends any array.
+
+Tuples resolve to numeric literals, while non-tuples resolve to the `number` type.
+
+@example
+```
+import type {ArrayLength} from 'type-fest';
+
+type TupleLength = ArrayLength<[1, 2, 3]>;
+//=> 3
+
+type TupleWithOptionalMembersLength = ArrayLength<[1, 2, number?]>;
+//=> 2 | 3
+
+type NonTupleArrayLength = ArrayLength<string[]>;
+//=> number
+
+type TupleWithRestElementLength = ArrayLength<[1, 2, ...string[]]>;
+//=> number
+
+// Distinguish between arrays with fixed and non-fixed lengths
+type IsFixedLengthArray<T extends readonly unknown[]> = number extends ArrayLength<T> ? false : true;
+
+type A = IsFixedLengthArray<number[]>;
+//=> false
+
+type B = IsFixedLengthArray<[1, 2, 3]>;
+//=> true
+```
+
+@category Array
+*/
+export type ArrayLength<T extends readonly unknown[]> = T['length'];
+
+export {};
