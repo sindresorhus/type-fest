523841d4dc1d8c0101a5484cc6c3df8b00ac724e
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2026-03-18 00:43:56 +0530

    doc: align JSDoc with `Or`'s JSDoc

--- source/or-multiple.d.ts
+++ source/or-multiple.d.ts
@@ -1,7 +1,7 @@
 import type {SomeExtend} from './some-extend.d.ts';
 
 /**
-Returns a boolean for whether any of the given elements are `true`.
+Returns a boolean for whether any of the given elements is `true`.
 
 Use-cases:
 - Check if at least one condition in a list of booleans is met.
@@ -10,17 +10,52 @@ Use-cases:
 ```
 import type {OrMultiple} from 'type-fest';
 
-type IsValid = OrMultiple<[false, true, false]>;
+type FFT = OrMultiple<[false, false, true]>;
 //=> true
 
-type IsInvalid = OrMultiple<[false, false]>;
+type FFF = OrMultiple<[false, false, false]>;
+//=> false
+```
+
+Note: When `boolean` is passed as an element, it is distributed into separate cases, and the final result is a union of those cases.
+For example, `OrMultiple<[false, boolean]>` expands to `OrMultiple<[false, true]> | OrMultiple<[false, false]>`, which simplifies to `true | false` (i.e., `boolean`).
+
+```
+import type {OrMultiple} from 'type-fest';
+
+type A = OrMultiple<[false, boolean]>;
+//=> boolean
+
+type B = OrMultiple<[true, boolean]>;
+//=> true
+```
+
+Note: If `never` is passed as an element, it is treated as `false` and the result is computed accordingly.
+```
+import type {OrMultiple} from 'type-fest';
+
+type A = OrMultiple<[never, never, true]>;
+//=> true
+
+type B = OrMultiple<[never, never, false]>;
 //=> false
 
-type OnBooleanFirstCase = OrMultiple<[true, boolean]>;
-//=> true
+type C = OrMultiple<[never, never, never]>;
+//=> false
 
-type OnBooleanSecondCase = OrMultiple<[false, boolean]>;
+type D = OrMultiple<[never, never, boolean]>;
 //=> boolean
+```
+
+Note: If `any` is passed as an element, it is treated as `boolean` and the result is computed accordingly.
+```
+import type {OrMultiple} from 'type-fest';
+
+type A = OrMultiple<[false, any]>;
+//=> boolean
+
+type B = OrMultiple<[true, any]>;
+//=> true
 ```
 */
 export type OrMultiple<T extends readonly boolean[]> = SomeExtend<T, true>;
