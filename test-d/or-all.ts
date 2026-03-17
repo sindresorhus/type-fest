b6d323cec8dcdbe51984821d216144f79e25dca0
Salisbury <salisbury.espinosa@gmail.com>
2026-03-17 23:55:21 +0300

    Update and rename or-multiple.ts to or-all.ts

---
+++ test-d/or-all.ts
@@ -0,0 +1,30 @@
+import {expectType} from 'tsd';
+import type {OrAll} from '../source/or-multiple.d.ts';
+
+declare const boolean: boolean;
+
+// Basic boolean combinations
+expectType<OrAll<[true, false]>>(true);
+expectType<OrAll<[false, true]>>(true);
+expectType<OrAll<[true, true]>>(true);
+expectType<OrAll<[false, false]>>(false);
+
+// Multiple elements in a tuple
+expectType<OrAll<[false, false, false, true]>>(true);
+expectType<OrAll<[false, false, false, false]>>(false);
+
+// `boolean` element
+expectType<OrAll<[false, false, boolean]>>(boolean);
+expectType<OrAll<[false, boolean, true]>>(true);
+expectType<OrAll<[boolean, boolean, boolean]>>(boolean);
+
+// Boundary cases
+expectType<OrAll<[]>>(false);
+
+expectType<OrAll<[any, any, false]>>(boolean);
+expectType<OrAll<[any, any, true]>>(true);
+expectType<OrAll<[any, any, any]>>(boolean);
+
+expectType<OrAll<[never, never, false]>>(false);
+expectType<OrAll<[never, never, true]>>(true);
+expectType<OrAll<[never, never, never]>>(false);
