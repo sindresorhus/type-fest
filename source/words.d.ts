9fb092eb87fa1791f5c148d755aab466c730018f
jericirenej <63595376+jericirenej@users.noreply.github.com>
2026-03-29 11:51:06 +0200

    fix/1229: since splintOnPunctuation si false by default, first example should be with the false setting.

--- source/words.d.ts
+++ source/words.d.ts
@@ -42,11 +42,11 @@ export type WordsOptions = {
 	/**
 	 Split on ASCII punctuation mark. Set to `false` by default for now..
 
-	 type Example1 = Words<'hello:world', {splitOnPunctuation: true}>
-	 //=> ['hello', 'world']
-
-	 type Example2 = Words<'hello:world', {splitOnPunctuation: false}>
+	 type Example1 = Words<'hello:world', {splitOnPunctuation: false}>
 	 //=> ['hello', ':world']
+
+	 type Example2 = Words<'hello:world', {splitOnPunctuation: true}>
+	 //=> ['hello', 'world']
 	 */
 	splitOnPunctuation?: boolean;
 };
