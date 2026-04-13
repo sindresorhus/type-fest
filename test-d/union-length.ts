7fbbf7c0dd4b45f0e2a0388c12b61d79ca7a00df
Som Shekhar Mukherjee <iamssmkhrj@gmail.com>
2026-04-14 02:02:29 +0530

    test: add long union tests

--- test-d/union-length.ts
+++ test-d/union-length.ts
@@ -1,5 +1,6 @@
 import {expectType} from 'tsd';
 import type {UnionLength} from '../source/union-length.d.ts';
+import type {IntRange} from '../source/int-range.d.ts';
 
 expectType<1>({} as UnionLength<string>);
 expectType<2>({} as UnionLength<boolean>);
@@ -10,6 +11,9 @@ expectType<3>({} as UnionLength<{a: string} | [string] | Set<string>>);
 
 // Both union members extend each other
 expectType<2>({} as UnionLength<{a: string} | {readonly a: string}>);
+
+// Long unions
+expectType<100>({} as UnionLength<IntRange<0, 100>>);
 
 // Boundary cases
 expectType<0>({} as UnionLength<never>);
