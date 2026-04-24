4d2a5adf958842a828dbc7cc869887f3780727e9
Dom Porada <dom@dom.engineering>
2026-04-24 16:44:19 +0200

    Add documentation

--- source/unwrap-required.d.ts
+++ source/unwrap-required.d.ts
@@ -1,6 +1,23 @@
 /**
 Revert the `Required` modifier on an object type.
 
+Use-case: Infer the underlying type `T` when only `Required<T>` is available or the original type may not be directly accessible.
+
+@example
+```
+import type {UnwrapRequired} from 'type-fest';
+
+type ContactFormData = Required<{
+	email: string;
+	message?: string;
+}>;
+
+type DraftContactFormData = UnwrapRequired<ContactFormData>;
+//=> {email: string; message?: string}
+```
+
+Note: If the provided type isn’t of `Required<T>`, `UnwrapRequired` has no effect on the original type.
+
 @category Object
 */
 export type UnwrapRequired<RequiredObjectType> =
