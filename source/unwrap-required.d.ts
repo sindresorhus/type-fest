35489507c7dfd60862019b3362578dc500b96376
Dom Porada <dom@dom.engineering>
2026-05-02 14:19:10 +0200

    Refactor type

--- source/unwrap-required.d.ts
+++ source/unwrap-required.d.ts
@@ -1,3 +1,5 @@
+import type {MapsSetsOrArrays, NonRecursiveType} from './internal/type.d.ts';
+
 /**
 Revert the `Required` modifier on an object type.
 
@@ -18,17 +20,20 @@ type DraftContactFormData = UnwrapRequired<ContactFormData>;
 
 Note 1: If the provided type isn’t of `Required<T>`, `UnwrapRequired` has no effect on the original type.
 
-Note 2: `Required<T>` is lossy for tuples with optional elements. `UnwrapRequired` cannot recover the original tuple type in these cases. Object types with optional properties are unaffected by this limitation (and work as expected with `UnwrapRequired`).
+Note 2: `Required<T>` is lossy for arrays and tuples with optional elements, so `UnwrapRequired` has no effect on array and tuple types. Object types with optional properties are unaffected by this limitation (and work as expected with `UnwrapRequired`).
 
 @category Object
 */
 export type UnwrapRequired<RequiredObjectType> =
+	// Isolate members where `Required<T>` produces unwanted results (accounting for unions)
+	Extract<RequiredObjectType, MapsSetsOrArrays | NonRecursiveType> extends infer PreservedType
+		// Recombine the preserved types with the unwrapped members
+		? PreservedType | _UnwrapRequired<Exclude<RequiredObjectType, PreservedType>>
+		: RequiredObjectType;
+
+type _UnwrapRequired<RequiredObjectType> =
 	RequiredObjectType extends Required<infer ObjectType>
-		? (
-			Required<ObjectType> extends RequiredObjectType
-				? ObjectType
-				: RequiredObjectType
-		)
+		? ObjectType
 		: RequiredObjectType;
 
 export {};
--- test-d/unwrap-required.ts
+++ test-d/unwrap-required.ts
@@ -47,6 +47,9 @@ type RequiredUnionType = Required<TestType> | Required<AnotherTestType>;
 
 expectType<TestType | AnotherTestType>({} as UnwrapRequired<RequiredUnionType>);
 expectType<TestType | AnotherTestType>({} as UnwrapRequired<Required<TestType> | AnotherTestType>);
+expectType<TestType | string>({} as UnwrapRequired<Required<TestType> | string>);
+expectType<TestType | Map<string, string>>({} as UnwrapRequired<Required<TestType> | Map<string, string>>);
+expectType<TestType | Map<string, string> | string>({} as UnwrapRequired<Required<TestType> | Map<string, string> | string>);
 
 // `UnwrapRequired` works with unknown types
 expectType<unknown>({} as UnwrapRequired<Required<unknown>>);
@@ -56,7 +59,15 @@ expectType<any>({} as UnwrapRequired<Required<any>>);
 expectType<TestType>({} as UnwrapRequired<TestType>);
 expectType<{a: string; b: number}>({} as UnwrapRequired<{a: string; b: number}>);
 expectType<readonly string[]>({} as UnwrapRequired<readonly string[]>);
+expectType<readonly [string, number?]>({} as UnwrapRequired<readonly [string, number?]>);
 expectType<Set<string>>({} as UnwrapRequired<Set<string>>);
+expectType<ReadonlySet<string>>({} as UnwrapRequired<ReadonlySet<string>>);
+expectType<WeakSet<{a: string}>>({} as UnwrapRequired<WeakSet<{a: string}>>);
 expectType<Map<string, string>>({} as UnwrapRequired<Map<string, string>>);
+expectType<ReadonlyMap<string, string>>({} as UnwrapRequired<ReadonlyMap<string, string>>);
+expectType<WeakMap<{a: string}, string>>({} as UnwrapRequired<WeakMap<{a: string}, string>>);
+expectType<Date>({} as UnwrapRequired<Date>);
+expectType<RegExp>({} as UnwrapRequired<RegExp>);
+expectType<Promise<string>>({} as UnwrapRequired<Promise<string>>);
 expectType<string>({} as UnwrapRequired<string>);
 expectType<() => string>({} as UnwrapRequired<() => string>);
