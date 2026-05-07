52c6c4794d5cb9bd2581c29e7850faa0b982e2a8
razhayat <razh.graffiti@gmail.com>
2026-05-07 16:23:09 +0300

    Implemented `GroupBy`

---
+++ source/group-by.d.ts
@@ -0,0 +1,59 @@
+import type { ConditionalKeys } from "./conditional-keys.d.ts";
+
+/**
+Groups items by the value of a specified key.
+
+This type creates an object where each key is a possible value of the specified property `GroupingKey`, and the corresponding value is the union of all items from the input type `Union` that have that value for property `GroupingKey`.
+
+Use-cases:
+- Grouping unions of objects by a discriminant property, such as categorizing different types of items by their type or color.
+- Creating type-safe grouped data structures from discriminated unions.
+
+@example
+```
+import type {GroupBy} from 'type-fest';
+
+type Apple = {
+	color: 'green';
+	kind: 'fruit';
+};
+
+type Banana = {
+    color: 'yellow';
+    kind: 'fruit';
+};
+
+type Tomato = {
+    color: 'red';
+    kind: 'vegetable';
+};
+
+type GroupedByColor = GroupBy<Apple | Banana | Tomato, 'color'>;
+// {
+//   green: Apple;
+//   yellow: Banana;
+//   red: Tomato;
+// }
+
+type GroupedByKind = GroupBy<Apple | Banana | Tomato, 'kind'>;
+// {
+//   fruit: Apple | Banana;
+//   vegetable: Tomato;
+// }
+```
+
+@category Object
+@category Union
+*/
+export type GroupBy<
+	Union,
+	GroupingKey extends ConditionalKeys<Union, PropertyKey>,
+> = {
+	[Key in Extract<Union[GroupingKey], PropertyKey>]: Union extends Union
+		? Key extends Union[GroupingKey]
+			? Union
+			: never
+		: never;
+};
+
+export {};
