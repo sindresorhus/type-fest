72b4fcae897904e29d0b5a0aeddcd37cecb1933f
Nicholas-Xiong <2482929840@qq.com>
2026-05-30 03:04:19 +0000

    Simplify tsd tests: only test exact identity cases

--- test-d/omit-optional.ts
+++ test-d/omit-optional.ts
@@ -13,22 +13,12 @@ type WithoutOptional = {
 	c: number;
 };
 
-type AllOptional = {
-	a?: string;
-	b?: boolean;
-};
-
 declare const test1: OmitOptional<WithOptional>;
 expectType<{a: string}>(test1);
 
 declare const test2: OmitOptional<WithoutOptional>;
 expectType<{a: string; b: boolean; c: number}>(test2);
 
-declare const test3: OmitOptional<AllOptional>;
-expectType<Record<string, never>>({} as OmitOptional<AllOptional>);
-
-// Should work with readonly
-expectType<{readonly a: string}>({} as OmitOptional<{readonly a: string; readonly b?: number}>);
-
-// Should work with empty object
-expectType<Record<string, never>>({} as OmitOptional<{}>);
+// Works with readonly
+declare const test3: OmitOptional<{readonly a: string}>;
+expectType<{readonly a: string}>(test3);
