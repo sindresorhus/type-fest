6ca1f67bc25f96b291e008331b23e3dac4851de4
Nicholas-Xiong <2482929840@qq.com>
2026-05-30 02:50:45 +0000

    Add test-d/omit-optional.ts

---
+++ test-d/omit-optional.ts
@@ -0,0 +1,40 @@
+import {expectType} from 'tsd';
+import type {OmitOptional} from '../index.d.ts';
+
+type WithOptional = {
+	a: string;
+	b?: boolean;
+	c?: number;
+};
+
+type WithoutOptional = {
+	a: string;
+	b: boolean;
+	c: number;
+};
+
+type AllOptional = {
+	a?: string;
+	b?: boolean;
+};
+
+declare const test1: OmitOptional<WithOptional>;
+expectType<{a: string}>(test1);
+
+declare const test2: OmitOptional<WithoutOptional>;
+expectType<{a: string; b: boolean; c: number}>(test2);
+
+declare const test3: OmitOptional<AllOptional>;
+expectType<Record<string, never>>(test3);
+
+// Should work with readonly
+declare const test4: OmitOptional<{readonly a: string; readonly b?: number}>;
+expectType<{readonly a: string}>(test4);
+
+// Should work with empty object
+declare const test5: OmitOptional<{}>;
+expectType<Record<string, never>>(test5);
+
+// OmitOptional<T> should be assignable to T
+type Assignability1<T extends object, _K extends T> = unknown;
+type Test1<T extends object> = Assignability1<T, OmitOptional<T>>;
