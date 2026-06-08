434285109ac68753736e69588f4e083e06435695
Som Shekhar Mukherjee <49264891+som-sm@users.noreply.github.com>
2026-06-08 13:24:25 +0530

    fix: revert incorrect handling of non-numeric strings

--- source/string-to-number.d.ts
+++ source/string-to-number.d.ts
@@ -1,6 +1,5 @@
 import type {IfNotAnyOrNever} from './internal/type.d.ts';
 import type {NegativeInfinity, PositiveInfinity} from './numeric.d.ts';
-import type {Replace} from './replace.d.ts';
 
 /**
 Converts a numeric string to a number.
@@ -20,39 +19,43 @@ type PositiveFloat = StringToNumber<'1234.56'>;
 
 type NegativeFloat = StringToNumber<'-1234.56'>;
 //=> -1234.56
-
-type WithSeparators = StringToNumber<'1_234_567.123_456_7'>;
-//=> 1234567.1234567
 ```
 
-Note: Due to some quirks in the type system, you may occasionally encounter unexpected results (see [microsoft/TypeScript#57404](https://github.com/microsoft/TypeScript/issues/57404)). For example, `StringToNumber<'-0.0000001'>` returns `number`. However, this type does handle fractional numbers ending in zero (such as `1.50`) and numbers with separators (such as `1_000`).
+Note: Some strings, such as `'1.50'`, `'0b10'`, or `'12_345'`, may look like they can be converted to numbers, but actually they don't have any corresponding number literals. So, in such cases, this type produces `never`. See [type-fest#1446](https://github.com/sindresorhus/type-fest/pull/1446) for more details.
+
+@example
+```
+import type {StringToNumber} from 'type-fest';
+
+type FractionalsEndingInZero = StringToNumber<'1.50'>;
+//=> never
+
+type NonDecimalBases = StringToNumber<'0b10' | '0o10' | '0x10'>;
+//=> never
+
+type NumericSeparators = StringToNumber<'12_345'>;
+//=> never
+```
 
 @category String
 @category Numeric
 @category Template literal
 */
-export type StringToNumber<S extends string> =
-	IfNotAnyOrNever<S,
-		S extends `_${string}` | `${string}_` | `${string}__${string}` | `${string}._${string}` | `${string}_.${string}`
-			? never
-			: _StringToNumber<Replace<S, '_', '', {all: true}>>,
-		number>;
+export type StringToNumber<S extends string> = IfNotAnyOrNever<S, _StringToNumber<S>, number>;
 
-type _StringToNumber<S extends string> =
-	S extends '-0'
-		? -0
-		: S extends `-0.${infer _Fraction extends 0}` // Matches `-0.0`, `-0.00`, etc.
-			? -0
-			: S extends `${infer N extends number}.0` // Matches `1.0`, `1234.0`, etc.
+export type _StringToNumber<S extends string> =
+	S extends `${infer N extends number}`
+		? number extends N
+			? `${number}` extends S
 				? N
-				: S extends `${infer N}.${infer Fraction}0` // Matches `1.10`, `1234.5600`, etc.
-					? _StringToNumber<`${N}.${Fraction}`>
-					: S extends `${infer N extends number}`
-						? N
-						: S extends 'Infinity'
-							? PositiveInfinity
-							: S extends '-Infinity'
-								? NegativeInfinity
-								: never;
+				: never
+			: N
+		: string extends S
+			? number
+			: S extends 'Infinity'
+				? PositiveInfinity
+				: S extends '-Infinity'
+					? NegativeInfinity
+					: never;
 
 export {};
--- test-d/string-to-number.ts
+++ test-d/string-to-number.ts
@@ -4,11 +4,6 @@ import type {NegativeInfinity, PositiveInfinity} from '../source/numeric.d.ts';
 
 // Zero
 expectType<0>({} as StringToNumber<'0'>);
-expectType<-0>({} as StringToNumber<'-0'>);
-expectType<0>({} as StringToNumber<'0.0'>);
-expectType<-0>({} as StringToNumber<'-0.0'>);
-expectType<0>({} as StringToNumber<'0.0000'>);
-expectType<-0>({} as StringToNumber<'-0.0000'>);
 
 // Positive integers
 expectType<1>({} as StringToNumber<'1'>);
@@ -32,59 +27,30 @@ expectType<-0.9>({} as StringToNumber<'-0.9'>);
 expectType<-3.14>({} as StringToNumber<'-3.14'>);
 expectType<-56.123>({} as StringToNumber<'-56.123'>);
 
-// Floats with trailing zeroes
-expectType<1>({} as StringToNumber<'1.0'>);
-expectType<-0.01>({} as StringToNumber<'-0.010'>);
-expectType<10.5>({} as StringToNumber<'10.500'>);
-expectType<32.0203>({} as StringToNumber<'32.02030'>);
-expectType<-100>({} as StringToNumber<'-100.00'>);
-expectType<-1234.056>({} as StringToNumber<'-1234.05600'>);
+// Infinity
+expectType<PositiveInfinity>({} as StringToNumber<'Infinity'>);
+expectType<NegativeInfinity>({} as StringToNumber<'-Infinity'>);
 
-// Starting with `-0.` and ending with zeroes
-expectType<-0.025>({} as StringToNumber<'-0.0250'>);
-expectType<-0.203>({} as StringToNumber<'-0.2030'>);
-expectType<-0.1>({} as StringToNumber<'-0.100'>);
-
-// Separators
-expectType<12_345>({} as StringToNumber<'12_345'>);
-expectType<1_234_567_890>({} as StringToNumber<'1_234_567_890'>);
-expectType<-3_456_789>({} as StringToNumber<'-3_456_789'>);
-expectType<12_345.123_45>({} as StringToNumber<'12_345.123_45'>);
-expectType<-12.345_678_9>({} as StringToNumber<'-12.345_678_9'>);
-expectType<98_765.4321>({} as StringToNumber<'98_765.432_10'>);
-expectType<-12.3>({} as StringToNumber<'-12.300_000'>);
-expectType<0.123_456_789>({} as StringToNumber<'0.123_456_789'>);
-expectType<-0.001>({} as StringToNumber<'-0.001_000_00'>);
-
-// Invalid separators
-expectType<never>({} as StringToNumber<'_15'>);
-expectType<never>({} as StringToNumber<'15_'>);
-expectType<never>({} as StringToNumber<'_15_'>);
-expectType<never>({} as StringToNumber<'_0.125'>);
-expectType<never>({} as StringToNumber<'0.125_'>);
-expectType<never>({} as StringToNumber<'6__6'>);
-expectType<never>({} as StringToNumber<'123_45__67'>);
-expectType<never>({} as StringToNumber<'3._14'>);
-expectType<never>({} as StringToNumber<'3_.14'>);
-
-// Special cases
-expectType<number>({} as StringToNumber<`${number}`>);
+// Unions
+expectType<1 | -1>({} as StringToNumber<'1' | '-1' | 'one'>);
+expectType<1 | 2 | -3 | 1.25>({} as StringToNumber<'1' | '2' | '-3' | '1.25' | '-0.650' | '10_000_000'>);
 
 // Non numeric strings
 expectType<never>({} as StringToNumber<'two'>);
 expectType<never>({} as StringToNumber<'1.2.30'>);
 expectType<never>({} as StringToNumber<'..5'>);
-expectType<never>({} as StringToNumber<string>);
 expectType<never>({} as StringToNumber<Uppercase<string>>);
 expectType<never>({} as StringToNumber<`${number}.${number}`>);
 
-// Unions
-expectType<1 | -1>({} as StringToNumber<'1' | '-1' | 'one'>);
-expectType<1 | 2 | -3 | 1.25 | -0.65 | 10_000_000>({} as StringToNumber<'1' | '2' | '-3' | '1.25' | '-0.650' | '10_000_000'>);
+expectType<never>({} as StringToNumber<'-0'>);
+expectType<never>({} as StringToNumber<'0.0'>);
+expectType<never>({} as StringToNumber<'1.0'>);
+expectType<never>({} as StringToNumber<'-3.50'>);
+expectType<never>({} as StringToNumber<'12_345'>);
 
-// Infinity
-expectType<PositiveInfinity>({} as StringToNumber<'Infinity'>);
-expectType<NegativeInfinity>({} as StringToNumber<'-Infinity'>);
+// Special cases
+expectType<number>({} as StringToNumber<`${number}`>);
+expectType<number>({} as StringToNumber<string>);
 
 // Boundary cases
 expectType<never>({} as StringToNumber<never>);
