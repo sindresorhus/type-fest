fd70f0be1ce492853a27e406484b007fa2d9dc24
Som Shekhar Mukherjee <49264891+som-sm@users.noreply.github.com>
2026-06-13 13:53:48 +0530

    test: fix case

--- test-d/string-repeat.ts
+++ test-d/string-repeat.ts
@@ -31,7 +31,7 @@ expectType<StringRepeat<'pi', 3.14>>(unknown as 'pipipi');
 expectType<StringRepeat<'0', 1e2>>(unknown as '0000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000000');
 expectType<StringRepeat<'0', 3e1>>(unknown as '000000000000000000000000000000');
 expectType<StringRepeat<'0', 1e-5>>(unknown as '');
-expectType<StringRepeat<'0', 5e-7>>(unknown as '');
+expectType<StringRepeat<'0', 2e-6>>(unknown as '');
 
 // Counts whose string representation is in scientific notation
 expectType<StringRepeat<'0', 1e-7>>(unknown as string);
