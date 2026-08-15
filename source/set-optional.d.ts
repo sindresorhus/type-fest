b80c7f0778160a4304f9eaefd6b35fdccf0e49ea
Som Shekhar Mukherjee <49264891+som-sm@users.noreply.github.com>
2026-08-15 11:49:50 +0530

    feat: improve behavior with arrays

--- source/set-optional.d.ts
+++ source/set-optional.d.ts
@@ -1,6 +1,10 @@
 import type {Except} from './except.d.ts';
-import type {HomomorphicPick} from './internal/index.d.ts';
+import type {If} from './if.d.ts';
+import type {HomomorphicPick, IsArrayReadonly} from './internal/index.d.ts';
 import type {Simplify} from './simplify.d.ts';
+import type {SplitOnRestElement} from './split-on-rest-element.d.ts';
+import type {Subtract} from './subtract.d.ts';
+import type {UnknownArray} from './unknown-array.d.ts';
 
 /**
 Create a type that makes the given keys optional, while keeping the remaining keys as is.
@@ -19,6 +23,10 @@ type Foo = {
 
 type SomeOptional = SetOptional<Foo, 'b' | 'c'>;
 //=> {a: number; b?: string; c?: boolean}
+
+// Set specific indices in an array to be optional.
+type ArrayExample = SetOptional<[number, number, number], 1 | 2>;
+//=> [number, number?, number?]
 ```
 
 @category Object
@@ -30,13 +38,48 @@ export type SetOptional<BaseType, Keys extends keyof BaseType> =
 	& _SetOptional<BaseType, Keys>;
 
 type _SetOptional<BaseType, Keys extends keyof BaseType> =
-	BaseType extends unknown // To distribute `BaseType` when it's a union type.
-		? Simplify<
+	BaseType extends UnknownArray
+		? SetArrayOptional<BaseType, Keys> extends infer ResultantArray
+			? If<IsArrayReadonly<BaseType>, Readonly<ResultantArray>, ResultantArray>
+			: never
+		: Simplify<
 			// Pick just the keys that are readonly from the base type.
 			Except<BaseType, Keys>
 			// Pick the keys that should be mutable from the base type and make them mutable.
 			& Partial<HomomorphicPick<BaseType, Keys>>
-		>
-		: never;
+		>;
+
+/**
+Add the optional modifier to the specified keys in an array.
+*/
+type SetArrayOptional<
+	TArray extends UnknownArray,
+	Keys,
+> = TArray extends unknown // For distributing `TArray` when it's a union
+	? SplitOnRestElement<TArray> extends readonly [infer BeforeRest extends UnknownArray, infer Rest extends UnknownArray, infer AfterRest extends UnknownArray]
+		? AfterRest extends readonly []
+			? SetArrayBeforeRestOptional<BeforeRest, Keys> extends infer ResultantArray extends UnknownArray
+				? [...ResultantArray, ...Rest]
+				: never
+			: TArray // If there are elements after the rest element, then we can't make any elements optional.
+		: never
+	: never;
+
+type SetArrayBeforeRestOptional<
+	TArray extends UnknownArray,
+	Keys,
+	ReverseCounter extends number = Subtract<Required<TArray>['length'], 1>,
+	Accumulator extends UnknownArray = [],
+> = TArray extends readonly []
+	? Accumulator
+	: TArray extends readonly [...infer Rest, (infer Last)?]
+		? TArray extends readonly [...any[], any] // If last element is required
+			? `${ReverseCounter}` extends `${Keys & (string | number)}` // If the current index needs to be optional
+				? SetArrayBeforeRestOptional<Rest, Keys, Subtract<ReverseCounter, 1>, [Last?, ...Accumulator]>
+				// If the current element is required, but it doesn't need to be optional,
+				// then we can exit early, since no further elements can now be made optional.
+				: [...TArray, ...Accumulator]
+			: SetArrayBeforeRestOptional<Rest, Keys, Subtract<ReverseCounter, 1>, [Last?, ...Accumulator]>
+		: never; // Should never happen, since `[...infer Rest, (infer Last)?]` is a top-type for arrays.
 
 export {};
